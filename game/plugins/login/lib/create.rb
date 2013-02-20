21a6250d06bbe2239b6612324331175147f0ccab
lynnfaraday <lomeara@gmail.com>
2013-02-20 01:22:40 -0500

    Finish first pass at create command.

--- game/plugins/login/lib/create.rb
+++ game/plugins/login/lib/create.rb
@@ -8,7 +8,7 @@ module AresMUSH
       end
 
       def on_command(client, cmd)      
-        args = cmd.crack_args!(/(?<name>\S+) (?<password>\S+)/)
+        args = cmd.crack_args!(/(?<name>\S+) (?<password>.+)/)
         
         if (args.nil?)
           client.emit_failure(t('login.invalid_create_syntax'))
@@ -18,16 +18,21 @@ module AresMUSH
         name = args[:name]
         password = args[:password]
         
+        if (password.length < 5)
+          client.emit_failure(t('login.password_too_short'))
+          return
+        end
+        
         existing_player = Player.find_by_name(name)
         if (!existing_player.empty?)
           client.emit_failure(t('login.player_name_taken'))
           return
         end
           
-          player = Player.create("name" => name, "password" => password)
-          client.emit_success(t('login.player_created', :name => name))
-          client.player = player
-          container.dispatcher.on_event(:player_created, :client => client)
+        player = Player.create("name" => name, "password" => password)
+        client.emit_success(t('login.player_created', :name => name))
+        client.player = player
+        container.dispatcher.on_event(:player_created, :client => client)
       end
     end
   end
