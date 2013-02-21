09ef4bbd055bc8be71601673fbd386b2bed58099
lynnfaraday <lomeara@gmail.com>
2013-02-20 23:26:12 -0500

    Adding login event specs.  Fixed a goof in the plugin factory spec's use of the container.

--- game/plugins/login/lib/login_events.rb
+++ game/plugins/login/lib/login_events.rb
@@ -9,7 +9,7 @@ module AresMUSH
       
       def on_player_connected(args)
         client = args[:client]
-        @client_monitor.emit_all t('login.player_has_connected', :name => client.name)
+        @client_monitor.emit_all t('login.announce_player_connected', :name => client.name)
       end
       
       def on_player_created(args)
@@ -19,7 +19,7 @@ module AresMUSH
 
       def on_player_disconnected(args)
         client = args[:client]
-        @client_monitor.emit_all t('login.player_has_disconnected', :name => client.name)
+        @client_monitor.emit_all t('login.announce_player_disconnected', :name => client.name)
       end
     end
   end
---
+++ game/plugins/login/spec/login_events_specs.rb
@@ -0,0 +1,44 @@
+$:.unshift File.join(File.dirname(__FILE__), *%w[.. lib])
+
+require "aresmush"
+require "login_events"
+
+module AresMUSH
+  module Login
+    describe LoginEvents do
+      before do
+        AresMUSH::Locale.stub(:translate).with("login.announce_player_connected", { :name => "Bob" }) { "announce_player_connected" }
+        AresMUSH::Locale.stub(:translate).with("login.announce_player_disconnected", { :name => "Bob" }) { "announce_player_disconnected" }
+        AresMUSH::Locale.stub(:translate).with("login.announce_player_created", { :name => "Bob" }) { "announce_player_created" }
+        
+        @client_monitor = double(ClientMonitor)
+        container = double(Container)
+        container.stub(:client_monitor) { @client_monitor }
+        @client = double(Client)
+        @client.stub(:name) { "Bob" }
+        @login_events = LoginEvents.new(container)
+      end
+      
+      describe :on_player_connected do
+        it "should announce the player" do
+          @client_monitor.should_receive(:emit_all).with("announce_player_connected")
+          @login_events.on_player_connected(:client => @client)
+        end
+      end
+      
+      describe :on_player_created do
+        it "should announce the player" do
+          @client_monitor.should_receive(:emit_all).with("announce_player_created")
+          @login_events.on_player_created(:client => @client)
+        end
+      end
+      
+      describe :on_player_disconnected do
+        it "should announce the player" do
+          @client_monitor.should_receive(:emit_all).with("announce_player_disconnected")
+          @login_events.on_player_disconnected(:client => @client)
+        end
+      end
+    end
+  end
+end
