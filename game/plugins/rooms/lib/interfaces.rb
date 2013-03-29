4012926ed9bf5267cacb106742b9ada0b53f1676
lynnfaraday <lomeara@gmail.com>
2013-03-28 23:08:46 -0400

    Find object method was case-sensitive and shouldn't have been.

--- game/plugins/rooms/lib/interfaces.rb
+++ game/plugins/rooms/lib/interfaces.rb
@@ -17,13 +17,13 @@ module AresMUSH
     end
     
     def self.find_visible_object(name, client)
-      return client.player if (name == t("object.me"))
+      return client.player if (name.downcase == t("object.me"))
 
       loc_id = client.location
-      return Room.find_one(loc_id) if (name == t("object.here"))
+      return Room.find_one(loc_id) if (name.downcase == t("object.here"))
       
       contents = contents(loc_id)
-      Room.notify_if_not_exatly_one(client) { contents.select { |c| c["name"] == name } }      
+      Room.notify_if_not_exatly_one(client) { contents.select { |c| c["name_upcase"] == name.upcase } }      
     end  
     
     def self.exits_from(loc_id)
