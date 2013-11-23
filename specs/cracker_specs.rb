ef4b09ddb88d973b140c42ad1d8866ac74a4db6f
lynnfaraday <lomeara@gmail.com>
2013-11-23 16:19:53 -0500

    Copy/paste error.

--- specs/cracker_specs.rb
+++ specs/cracker_specs.rb
@@ -71,68 +71,6 @@ module AresMUSH
         cracked[:args].should eq "/  sw    2"
       end
     end
-
-    describe :logged_in? do  
-      before do
-        @cmd = Command.new(@client, "test/sw foo")
-      end
-
-      it "returns true if the char is set" do
-        @client.stub(:char) { { "name" => "Bob"} }
-        @cmd.logged_in?.should be_true
-      end
-
-      it "returns false if the char is not set" do
-        @client.stub(:char) { nil }
-        @cmd.logged_in?.should be_false
-      end
-    end
-
-    describe :root_is? do      
-      # Note - more in-depth testing in :cmd_root
-      it "should match the specified root" do
-        cmd = Command.new(@client, "test/foo bar")
-        cracked[:root]_is?("test").should be_true
-      end
-
-      it "should not match a different root" do
-          cmd = Command.new(@client, "test/foo bar")
-          cracked[:root]_is?("foo").should be_false
-        end
-      
-      it "should ignore case in the root" do
-          cmd = Command.new(@client, "TesT/foo bar")
-          cracked[:root]_is?("test").should be_true
-      end        
-    end
-
-    describe :crack_args! do
-      it "should expand the args string into a more meaningful hash" do
-        cmd = Command.new(@client, "test/foo bar=baz+harvey")
-        cmd.crack_args!(/(?<a>.+)=(?<b>.+)\+(?<c>.+)/)
-        cracked[:args].a.should eq "bar"
-        cracked[:args].b.should eq "baz"
-        cracked[:args].c.should eq "harvey"
-      end
-      
-      it "should still return a hash reader it can't crack the args" do 
-        cmd = Command.new(@client, "test/foo bar=baz+harvey")
-        cmd.crack_args!(/(?<a>.+)\/(?<b>.+)/)
-        cracked[:args].a.should be_nil
-        cracked[:args].b.should be_nil
-      end
-    end
-
-    describe :can_crack_args? do
-      it "should be true if it can crack the args" do
-        cmd = Command.new(@client, "test/foo bar=baz+harvey")
-        cmd.can_crack_args?(/(?<a>.+)=(?<b>.+)\+(?<c>.+)/).should be_true
-      end
-      
-      it "should be false if it can't crack the args" do
-        cmd = Command.new(@client, "test/foo bar=baz+harvey")
-        cmd.can_crack_args?(/(?<a>.+)\/(?<b>.+)/).should be_false
-      end
-    end    
+  
   end
 end
