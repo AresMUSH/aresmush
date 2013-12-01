a0c5e48c1e30e965fa5919abf9d07edb1fee09e5
lynnfaraday <lomeara@gmail.com>
2013-12-01 16:44:14 -0500

    Cleaned up quit specs.

--- game/plugins/login/spec/quit/quit_specs.rb
+++ game/plugins/login/spec/quit/quit_specs.rb
@@ -6,27 +6,24 @@ module AresMUSH
 
       before do
         @cmd = double(Command)
-        @client = double(Client).as_null_object      
+        @client = double(Client)
         @quit = Quit.new
         SpecHelpers.stub_translate_for_testing
       end
 
       describe :want_command do
-        it "should want the quit command if logged in" do
+        it "should want the quit command" do
           @cmd.stub(:root_is?).with("quit") { true }
-          @cmd.stub(:logged_in?) { true }
           @quit.want_command?(@cmd).should be_true
         end
 
-        it "should want the quit command if not logged in" do
+        it "should want the quit command" do
           @cmd.stub(:root_is?).with("quit") { true }
-          @cmd.stub(:logged_in?) { false }
           @quit.want_command?(@cmd).should be_true
         end
 
         it "should not want another command" do
           @cmd.stub(:root_is?).with("quit") { false }
-          @cmd.stub(:logged_in?) { true }
           @quit.want_command?(@cmd).should be_false
         end        
       end
@@ -34,10 +31,12 @@ module AresMUSH
       describe :on_command do
         it "should disconnect the client" do
           @client.should_receive(:disconnect)
+          @client.stub(:emit_ooc)
           @quit.on_command(@client, @cmd)
         end
         
         it "should send the bye text" do
+          @client.stub(:disconnect)
           @client.should_receive(:emit_ooc).with("login.goodbye")
           @quit.on_command(@client, @cmd)
         end
