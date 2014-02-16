e749286c626e35be863e4a30473f8f7290c64c7d
lynnfaraday <lomeara@gmail.com>
2014-02-16 10:17:41 -0500

    New plugin test helper experiment.

--- game/plugins/plugin_test_loader.rb
+++ game/plugins/plugin_test_loader.rb
@@ -15,3 +15,22 @@ RSpec.configure do |c|
   AresMUSH::TagExtensions.register  
 end
 
+module AresMUSH
+  module CommandTestHelper
+    include MockClient
+    
+    attr_accessor :client, :char, :cmd, :handler
+    
+    def init_handler(handler_class, cmd)
+      mock_client = build_mock_client
+      @client = mock_client[:client]
+      @char = mock_client[:char]
+      
+      @cmd = Command.new(cmd)
+      
+      @handler = handler_class.new
+      @handler.client = @client
+      @handler.cmd = @cmd
+    end
+  end
+end
--- game/plugins/who/specs/hide_cmd_specs.rb
+++ game/plugins/who/specs/hide_cmd_specs.rb
@@ -3,68 +3,55 @@ require_relative "../../plugin_test_loader"
 module AresMUSH
   module Who
     describe HideCmd do
-      include MockClient
-
+      include CommandTestHelper
+      
       before do
-        @hide = HideCmd.new
-        @cmd = double
-        @client = double
-        @hide.cmd = @cmd
-        @hide.client = @client
+        init_handler(HideCmd, "hide")
         SpecHelpers.stub_translate_for_testing        
       end
       
       describe :want_command do
         it "should want the hide command" do
-          @cmd.stub(:root_is?).with("hide") { true }
-          @hide.want_command?(@client, @cmd).should be_true
+          handler.want_command?(client, cmd).should be_true
         end
 
         it "should not want another command" do
-          @cmd.stub(:root_is?).with("hide") { false }
-          @hide.want_command?(@client, @cmd).should be_false
+          cmd.stub(:root_is?).with("hide") { false }
+          handler.want_command?(client, cmd).should be_false
         end
       end
       
       describe :validate do
         it "should reject the command if there are args" do
-          @client.stub(:logged_in?) { true }
-          @cmd.stub(:root_only?) { false }
-          @hide.validate.should eq 'who.invalid_hide_syntax'
+          cmd.stub(:root_only?) { false }
+          client.stub(:logged_in?) { true }
+          handler.validate.should eq 'who.invalid_hide_syntax'
         end
         
         it "should reject the command if not logged in" do
-          @cmd.stub(:root_only?) { true }
-          @client.stub(:logged_in?) { false }            
-          @hide.validate.should eq 'dispatcher.must_be_logged_in'
+          client.stub(:logged_in?) { false }            
+          handler.validate.should eq 'dispatcher.must_be_logged_in'
         end
         
         it "should accept the command otherwise" do
-          @client.stub(:logged_in?) { true }
-          @cmd.stub(:root_only?) { true }
-          @hide.validate.should be_nil 
+          client.stub(:logged_in?) { true }
+          handler.validate.should be_nil 
         end
       end
 
-      describe :handle do        
-        before do
-          @mock_client = build_mock_client
-          @hide.client = @mock_client[:client]
-          SpecHelpers.stub_translate_for_testing
-        end
-
+      describe :handle do
         it "should toggle the hidden flag on" do
-          @mock_client[:char].should_receive(:hidden) { false }
-          @mock_client[:char].should_receive(:hidden=).with(true)
-          @mock_client[:client].should_receive(:emit_success).with('who.hide_enabled')
-          @hide.handle
+          char.should_receive(:hidden) { false }
+          char.should_receive(:hidden=).with(true)
+          client.should_receive(:emit_success).with('who.hide_enabled')
+          handler.handle
         end
         
         it "should toggle the hidden flag off" do
-          @mock_client[:char].should_receive(:hidden) { true }
-          @mock_client[:char].should_receive(:hidden=).with(false)
-          @mock_client[:client].should_receive(:emit_success).with('who.hide_disabled')
-          @hide.handle
+          char.should_receive(:hidden) { true }
+          char.should_receive(:hidden=).with(false)
+          client.should_receive(:emit_success).with('who.hide_disabled')
+          handler.handle
         end
       end
     end
