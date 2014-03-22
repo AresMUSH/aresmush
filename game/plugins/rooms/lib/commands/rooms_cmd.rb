8ab55441a637b951789c8591a577d310510a3dd2
lynnfaraday <lomeara@gmail.com>
2014-03-22 01:11:40 -0400

    Room dir search.

--- game/plugins/rooms/lib/commands/rooms_cmd.rb
+++ game/plugins/rooms/lib/commands/rooms_cmd.rb
@@ -23,12 +23,11 @@ module AresMUSH
         if (self.name.nil?)
           rooms = Room.all
         else
-          rooms = Room.find_by_name_upcase(self.name)
+          rooms = Room.find_all_by_name_upcase(self.name.upcase)
         end
         rooms = rooms.sort { |a,b| a.name_upcase <=> b.name_upcase}
         rooms = rooms.map { |r| "#{r.id} #{r.name}"}
         client.emit BorderedDisplay.list(rooms, t('rooms.room_directory'))
-        
       end
     end
   end
