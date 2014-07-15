fd22ff994589c6c0ae5a29a4583c6b3a6d5f4038
lynnfaraday <lomeara@gmail.com>
2014-07-14 22:29:26 -0400

    Adding sweep command.

---
+++ game/plugins/utils/lib/sweep_cmd.rb
@@ -0,0 +1,37 @@
+module AresMUSH
+  module Utils
+    class SweepCmd
+      include Plugin
+      include PluginWithoutSwitches
+      include PluginWithoutArgs
+      include PluginRequiresLogin
+      
+      attr_accessor :message
+      
+      def want_command?(client, cmd)
+        cmd.root_is?("sweep")
+      end
+      
+      def handle
+        outside = client.room.exits.select { |e| e.name == 'O' }
+        if (outside.empty?)
+          outside = client.room.exits.first
+        end
+        
+        if (outside.nil?)
+          client.emit_failure t('sweep.cant_find_exit')
+          return
+        end
+        
+        client.room.characters.each do |c|
+          other_client = Global.client_monitor.find_client(c)
+          if (other_client.nil?)
+            Rooms.move_to(nil, c, outside.dest)
+          end
+        end
+        
+        client.emit_success t('sweep.sweep_complete')
+      end
+    end
+  end
+end
