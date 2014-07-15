1ea2f97f2f1b8cd6a1fdcb9a61f585beeaef6a39
lynnfaraday <lomeara@gmail.com>
2014-07-14 22:34:22 -0400

    Added the x=y/z arg cracker.

--- lib/aresmush/commands/common_cracks.rb
+++ lib/aresmush/commands/common_cracks.rb
@@ -11,5 +11,13 @@ module AresMUSH
     def self.arg1_space_arg2
       /(?<arg1>[\S]+) (?<arg2>.+)/
     end
+    
+    def self.arg1_equals_arg2_slash_arg3
+      /(?<arg1>[^\=]+)\=(?<arg2>[^\/]+)\/(?<arg3>.+)/
+    end
+    
+    def self.arg1_equals_arg2_slash_optional_arg3
+      /(?<arg1>[^\=]+)\=(?<arg2>[^\/]+)\/?(?<arg3>.+)?/
+    end
   end
 end
--- specs/commands/common_cracks_specs.rb
+++ specs/commands/common_cracks_specs.rb
@@ -39,6 +39,59 @@ module AresMUSH
       end
     end
     
+    describe "arg1_equals_arg2_slash_arg3" do
+      it "should crack a matching command" do
+        args = ArgCracker.crack(CommonCracks.arg1_equals_arg2_slash_arg3, "a=b/c")
+        args.arg1.should eq "a"
+        args.arg2.should eq "b"
+        args.arg3.should eq "c"
+      end
+      
+      it "should set values to nil if not matched" do
+        args = ArgCracker.crack(CommonCracks.arg1_equals_arg2, "a b")
+        args.arg1.should be_nil
+        args.arg2.should be_nil
+        args.arg3.should be_nil
+      end
+      
+      it "should match an arg3 with equals or slashes" do
+        args = ArgCracker.crack(CommonCracks.arg1_equals_arg2_slash_arg3, "a=b/c = d/e")
+        args.arg1.should eq "a"
+        args.arg2.should eq "b"
+        args.arg3.should eq "c = d/e"
+      end
+    end
+    
+    describe "arg1_equals_arg2_slash_optional_arg3" do
+      it "should crack a matching command" do
+        args = ArgCracker.crack(CommonCracks.arg1_equals_arg2_slash_optional_arg3, "a=b/c")
+        args.arg1.should eq "a"
+        args.arg2.should eq "b"
+        args.arg3.should eq "c"
+      end
+      
+      it "should crack a command without the optional arg" do
+        args = ArgCracker.crack(CommonCracks.arg1_equals_arg2_slash_optional_arg3, "a=b")
+        args.arg1.should eq "a"
+        args.arg2.should eq "b"
+        args.arg3.should be_nil
+      end
+      
+      it "should set values to nil if not matched" do
+        args = ArgCracker.crack(CommonCracks.arg1_equals_arg2_slash_optional_arg3, "a b")
+        args.arg1.should be_nil
+        args.arg2.should be_nil
+        args.arg3.should be_nil
+      end
+      
+      it "should match an arg3 with equals or slashes" do
+        args = ArgCracker.crack(CommonCracks.arg1_equals_arg2_slash_optional_arg3, "a=b/c = d/e")
+        args.arg1.should eq "a"
+        args.arg2.should eq "b"
+        args.arg3.should eq "c = d/e"
+      end
+    end
+    
     describe "arg1_space_arg2" do
       it "should crack a matching command" do
         args = ArgCracker.crack(CommonCracks.arg1_space_arg2, "a b")
