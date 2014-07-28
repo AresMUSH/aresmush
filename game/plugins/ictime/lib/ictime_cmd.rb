d34d95c82333e53e142c73499e5ad412fce4b332
lynnfaraday <lomeara@gmail.com>
2014-07-28 18:21:07 -0400

    IC Time command.

--- game/plugins/ictime/lib/ictime_cmd.rb
+++ game/plugins/ictime/lib/ictime_cmd.rb
@@ -7,15 +7,11 @@ module AresMUSH
       include PluginWithoutArgs
            
       def want_command?(client, cmd)
-        cmd.root_is?("time")
+        cmd.root_is?("ictime")
       end
       
       def handle
-        server_time = t('time.server_time', :time => DateTime.now.strftime("%a %b %d, %Y %l:%M%P"))
-        local_time = t('time.local_time', :time => OOCTime.local_time_str(client, Time.now))
-        timezone = t('time.timezone', :timezone => client.char.timezone)
-        display_text = "#{server_time}%r#{local_time}%r%r#{timezone}"
-        client.emit BorderedDisplay.text display_text
+        client.emit BorderedDisplay.text t('ictime.ictime', :time => ICTime.ictime)
       end
     end
   end
