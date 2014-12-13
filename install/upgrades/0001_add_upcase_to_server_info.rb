3a72b06766097b71a4dc4f95ff5300a0176a02d7
lynnfaraday <lomeara@gmail.com>
2014-12-13 07:51:29 -0500

    Adding upgrade script for server info.

---
+++ install/upgrades/0001_add_upcase_to_server_info.rb
@@ -0,0 +1,19 @@
+module AresMUSH
+  bootstrapper = AresMUSH::Bootstrapper.new
+  
+  puts "======================================================================="
+  puts "Update some new fields in server info"
+  puts "======================================================================="
+  
+  ServerInfo.all.each do |s|
+    if (s.category.nil?)
+      s.category = "Social"
+    end
+    s.game_open = "yes"
+    s.name_upcase = s.name.upcase
+    s.category_upcase = s.category.upcase
+    s.save!
+  end
+  
+  puts "Upgrade complete!"
+end
