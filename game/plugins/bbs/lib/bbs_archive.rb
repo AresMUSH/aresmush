d6b506b07aa28f22942dc45e008e84e7c5285fd3
lynnfaraday <lomeara@gmail.com>
2014-12-20 10:31:53 -0500

    Error check max posts/msgs before archive.

--- game/plugins/bbs/lib/bbs_archive.rb
+++ game/plugins/bbs/lib/bbs_archive.rb
@@ -23,24 +23,30 @@ module AresMUSH
       end
       
       def handle
-          Bbs.with_a_board(self.board_name, client) do |board|  
-            board.bbs_posts.each_with_index do |post, i|
-              Global.dispatcher.queue_timer(i, "BBS Archive") do
-                Global.logger.debug "Logging bbpost #{post.id} from #{board.name}."
-                date = OOCTime.local_long_timestr(client, post.created_at)
-                author = post.author.nil? ? t('bbs.deleted_author') : post.author.name
-                text = "%R%R+ #{post.subject}"
-                text << "%r//#{author} -- #{date}//"
-                text << "%R%r#{post.message}"
-                post.bbs_replies.each do |reply|
-                  rauthor = reply.author.nil? ? t('bbs.deleted_author') : reply.author.name
-                  rdate = OOCTime.local_long_timestr(client, reply.created_at)
-                  text << "%R%R//#{t('bbs.reply_title', :name => rauthor, :date => rdate)}//"
-                  text << "%R%R#{reply.message}"
-                end
-                client.emit text
+        client.emit_ooc t('bbs.starting_archive')
+        Bbs.with_a_board(self.board_name, client) do |board|  
+          if board.bbs_posts.count > 30
+            client.emit_failure t('bbs.too_much_for_archive')
+            return
+          end
+          
+          board.bbs_posts.each_with_index do |post, i|
+            Global.dispatcher.queue_timer(i, "BBS Archive") do
+              Global.logger.debug "Logging bbpost #{post.id} from #{board.name}."
+              date = OOCTime.local_long_timestr(client, post.created_at)
+              author = post.author.nil? ? t('bbs.deleted_author') : post.author.name
+              text = "%R%R+ #{post.subject}"
+              text << "%r//#{author} -- #{date}//"
+              text << "%R%r#{post.message}"
+              post.bbs_replies.each do |reply|
+                rauthor = reply.author.nil? ? t('bbs.deleted_author') : reply.author.name
+                rdate = OOCTime.local_long_timestr(client, reply.created_at)
+                text << "%R%R//#{t('bbs.reply_title', :name => rauthor, :date => rdate)}//"
+                text << "%R%R#{reply.message}"
               end
+              client.emit text
             end
+          end
         end
       end      
     end
--- game/plugins/mail/lib/mail_backup_cmd.rb
+++ game/plugins/mail/lib/mail_backup_cmd.rb
@@ -8,7 +8,13 @@ module AresMUSH
         cmd.root_is?("mail") && cmd.switch_is?("backup")
       end
       
+      def check_too_much_mail
+        return t('mail.too_much_for_backup') if client.char.mail.count > 30
+        return nil
+      end
+      
       def handle
+        client.emit_ooc t('mail.starting_backup')
         client.char.mail.each_with_index do |delivery, i|
           Global.dispatcher.queue_timer(i, "Mail Backup #{client.char.name}") do
             Global.logger.debug "Logging mail #{delivery.id} from #{client.char.name}."
