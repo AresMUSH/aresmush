9984530075a796ebbf00de1177fb671d833879f4
lynnfaraday <lomeara@gmail.com>
2014-12-20 10:06:22 -0500

    Clarify mail backup help.

--- game/plugins/mail/lib/mail_backup_cmd.rb
+++ game/plugins/mail/lib/mail_backup_cmd.rb
@@ -1,16 +1,16 @@
 module AresMUSH
   module Mail
-    class MailArchiveCmd
+    class MailBackupCmd
       include Plugin
       include PluginRequiresLogin
            
       def want_command?(client, cmd)
-        cmd.root_is?("mail") && cmd.switch_is?("archive")
+        cmd.root_is?("mail") && cmd.switch_is?("backup")
       end
       
       def handle
         client.char.mail.each_with_index do |delivery, i|
-          Global.dispatcher.queue_timer(i, "Mail Archive #{client.char.name}") do
+          Global.dispatcher.queue_timer(i, "Mail Backup #{client.char.name}") do
             Global.logger.debug "Logging mail #{delivery.id} from #{client.char.name}."
             client.emit Mail.message_renderer.render(client, delivery)
           end
