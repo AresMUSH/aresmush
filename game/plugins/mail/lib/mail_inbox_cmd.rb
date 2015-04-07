32e509770f4db07326213c775e2284a12e0e7f3f
lynnfaraday <lomeara@gmail.com>
2015-04-07 01:01:34 -0400

    Removing unused methods/tests.

--- game/plugins/mail/lib/mail_inbox_cmd.rb
+++ game/plugins/mail/lib/mail_inbox_cmd.rb
@@ -4,10 +4,6 @@ module AresMUSH
       include Plugin
       include PluginRequiresLogin
            
-      def initialize
-        Mail.build_renderers
-      end
-      
       def want_command?(client, cmd)
         cmd.root_is?("mail") && cmd.switch.nil? && cmd.args.nil?
       end
--- game/plugins/tinker/lib/tinker_cmd.rb
+++ game/plugins/tinker/lib/tinker_cmd.rb
@@ -19,36 +19,9 @@ module AresMUSH
       
       def handle
         # Put whatever you need to do here.
-        list = Global.config['fs3combat']['old_vehicles'].map { |w| print_wpn(w) }
-        client.emit BorderedDisplay.list list
         client.emit_success "Done!"
       end
-      
-      def print_wpn(w)
-        text = ""
-#        fields = [ 'description', 'atack_skill',  'defense_skill', 'lethality', 'penetration', 'weapon_type', 'weapon_class', 'is_automatic', 'ammo', 'recoil', 'range', 'blast', 'damage_type', 'accuracy', 'has_shrapnel', 'reload_time' ]
-#fields = [ 'desc', 'hitloc', 'protection', 'class' ]
-fields = ['desc', 'pilotskill', 'toughness', 'armor', 'hitloc', 'weapons', 'dodge']
-        w.split('|').each_with_index do |f, i|
-          if (i == 0)
-            x = f.split('=')
-            name = x[0].after('_').before(' ').gsub(/_/, ' ').titleize
-            text << "%R        #{name}:"
-            text << "%R            description: \"#{x[1]}\""
-          elsif (i == 5)
-            x = f.split(' ')
-            text << "%R            weapons:"
-            x.each do |y|
-              text << "%R                - #{y.gsub(/_/, ' ').titleize}"
-            end
-          elsif (i == 3)
-            next
-          else
-            text << "%R            #{fields[i]}: #{f}"
-          end
-        end
-        text
-      end
+
     end
   end
 end
