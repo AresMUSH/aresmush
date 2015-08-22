3d5b272ab02b4064c3456fd4f9c42c53e8351976
lynnfaraday <lomeara@gmail.com>
2015-08-22 03:27:10 -0400

    Fixing related aptitude check.

--- game/plugins/fs3skills/lib/set_related_apt_cmd.rb
+++ game/plugins/fs3skills/lib/set_related_apt_cmd.rb
@@ -24,26 +24,19 @@ module AresMUSH
         self.name = titleize_input(cmd.args.arg1)
         self.related_apt = titleize_input(cmd.args.arg2)
       end
-      
-      def check_valid_attr
-        return nil if self.related_apt.nil?
-        return t('fs3skills.invalid_aptitude', :name => self.related_apt) if !FS3Skills.aptitude_names.include?(self.related_apt)
-        return nil
+
+      def check_valid_ability
+        return nil if client.char.fs3_interests.include?(self.name)
+        retirn nil if client.char.fs3_expertise.include?(self.name)
+        return t('fs3skills.invalid_ability_for_related_apt', :ability => self.name)
       end
       
-      def check_name
-        ability_type = FS3Skills.get_ability_type(client.char, self.name)
-        invalid_types = [:action, :advantage, :aptitude]
-        return t('fs3skills.cant_change_fixed_related_apts') if invalid_types.include?(ability_type)
+      def check_valid_attr
+        return t('fs3skills.invalid_aptitude', :name => self.related_apt) if !FS3Skills.aptitude_names.include?(self.related_apt)
         return nil
       end
         
       def handle
-        if (FS3Skills.ability_rating(client.char, self.name) == 0)
-          client.emit_failure t('fs3skills.invalid_ability_for_related_apt', :ability => self.name)
-          return
-        end
-        
         client.char.fs3_related_apts[self.name] = self.related_apt
         client.char.save
         client.emit_success t('fs3skills.related_apt_set', :ability => self.name, :related_apt => self.related_apt)
