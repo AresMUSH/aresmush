48ddd1f5d130b0e87180f2aa60dd63a2ffac023b
lynnfaraday <lynn@wordsmyth.org>
2015-09-12 22:30:43 -0400

    Map names shouldn't have to be lowercase.

--- game/plugins/maps/lib/map_cmd.rb
+++ game/plugins/maps/lib/map_cmd.rb
@@ -21,17 +21,17 @@ module AresMUSH
         
           if (!map)
             client.emit_failure t('maps.no_such_map')
-            return
+          else
+            client.emit BorderedDisplay.text map, t('maps.map_title', :area => self.area)
           end
-        
-          client.emit BorderedDisplay.text map, t('maps.map_title', :area => self.area)
         end
       end
       
       def map_for_area(area)
         return nil if area.nil?
-        return nil if !Maps.available_maps.include?(area.downcase)
-        File.read(File.join(Maps.maps_dir, "#{area.downcase}.txt"), :encoding => "UTF-8")
+        matches = Maps.available_maps.select { |m| m.downcase == area.downcase }
+        return nil if matches.empty?
+        File.read(File.join(Maps.maps_dir, "#{matches[0]}.txt"), :encoding => "UTF-8")
       end
     end
   end
