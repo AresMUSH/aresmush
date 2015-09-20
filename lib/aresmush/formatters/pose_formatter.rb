72264035337a85c54cc6dd50ca467d2bad1bd95a
lynnfaraday <lynn@wordsmyth.org>
2015-09-19 21:28:23 -0400

    Allow an empty space after an emit shortcut.

--- lib/aresmush/formatters/pose_formatter.rb
+++ lib/aresmush/formatters/pose_formatter.rb
@@ -9,6 +9,8 @@ module AresMUSH
         t('object.pose', :name => name, :msg => msg.rest(":"))
       elsif msg.start_with?(";")
         t('object.semipose', :name => name, :msg => msg.rest(";"))
+      elsif msg.start_with?("\\ ")
+        msg.rest("\\ ")
       elsif msg.start_with?("\\")
         msg.rest("\\")
       else
