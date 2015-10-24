58b1289f7c7af4cf67f8c28b83bb14686be6ab7e
lynnfaraday <lynn@wordsmyth.org>
2015-10-24 04:54:34 -0400

    More bulletproofing of error handling.

--- lib/aresmush/error_block.rb
+++ lib/aresmush/error_block.rb
@@ -1,13 +1,13 @@
 module AresMUSH
   def self.with_error_handling(client, desc, &block)
     begin
-      yield block
-      return true
-      # Allow system exit to bubble up so it shuts the system down.
-    rescue SystemExit
-      raise SystemExit
-    rescue Exception => e
       begin
+        yield block
+        return true
+        # Allow system exit to bubble up so it shuts the system down.
+      rescue SystemExit
+        raise SystemExit
+      rescue Exception => e
         id = client.nil? ? nil : client.id
         message = t('dispatcher.unexpected_error', :desc => desc, :error_info => e)
         Global.logger.error("Error in #{desc}: client=#{id} error=#{e} backtrace=#{e.backtrace[0,10]}")
@@ -17,9 +17,12 @@ module AresMUSH
         if (Global.dispatcher)
           Global.dispatcher.queue_event UnhandledErrorEvent.new(message)
         end
-      rescue Exception => e2
-        Global.logger.error("Error inside of error handling: error=#{e2} backtrace=#{e2.backtrace[0,10]}")
+        return false
       end
+    rescue SystemExit
+      raise SystemExit
+    rescue Exception => e2
+      Global.logger.error("Untrapped error in error handling: error=#{e2} backtrace=#{e2.backtrace[0,10]}")
       return false
     end
   end
