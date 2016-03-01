a8afcaa9dd9166a7fcb6992979bb6fe663a6da2b
M. Scott Key <mscottkey@gmail.com>
2016-02-29 21:30:50 -0800

    Removed FS3 System
    Removed FS3 from IK branch

--- game/plugins/fs3combat/interfaces/combat_interface.rb
+++
@@ -1,9 +0,0 @@
-module AresMUSH
-  module FS3Combat
-    def self.register_pose(char)
-      return if !char.combatant
-      char.combatant.posed = true
-      char.combatant.save
-    end
-  end
-end
--- game/plugins/fs3combat/lib/combat_events.rb
+++
@@ -1,7 +0,0 @@
-module AresMUSH
-  module FS3Combat
-    class CombatEvents
-      include CommandHandler
-    end
-  end
-end
