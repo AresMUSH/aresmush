d35ded54785b459bdec36b4f7def498cea660c55
lynnfaraday <lynn@wordsmyth.org>
2016-11-08 22:54:53 -0500

    Fixing tests.

--- game/plugins/handles/specs/char_connected_event_specs.rb
+++ game/plugins/handles/specs/char_connected_event_specs.rb
@@ -30,12 +30,14 @@ module AresMUSH
           @char.stub(:handle) { @handle }
           @char.stub(:name) { "Bob" }
           @char.stub(:id) { 111 }
-          response = { "status" => "success", "data" => { "linked" => true, "autospace" => "x", "timezone" => "t", "friends" => "f", "quote_color" => "q" }}
+          response = { "status" => "success", "data" => { "linked" => true, "autospace" => "x", "timezone" => "t", "friends" => "f", "quote_color" => "q", "page_color" => "pc", "page_autospace" => "ps" }}
           
           
           @connector.should_receive(:sync_handle).with(123, "Bob", 111) { AresCentral::AresResponse.new(response) }  
           @char.should_receive(:update).with({:pose_quote_color => "q"})
           @char.should_receive(:update).with({:pose_autospace => "x"})
+          @char.should_receive(:update).with({:page_autospace => "ps"})
+          @char.should_receive(:update).with({:page_color => "pc"})
           @char.should_receive(:update).with({:timezone => "t"})
           @handle.should_receive(:update).with(friends: "f")
           @client.should_receive(:emit_success).with("handles.handle_synced")
