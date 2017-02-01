8e67e4755066e19c72aa03783db73eab315d942e
lynnfaraday <lynn@wordsmyth.org>
2017-02-01 07:42:56 -0500

    Adding admin XP view.

--- game/plugins/fs3skills/lib/commands/xp_cmd.rb
+++ game/plugins/fs3skills/lib/commands/xp_cmd.rb
@@ -4,11 +4,24 @@ module AresMUSH
     class XpCmd
       include CommandHandler
       include CommandRequiresLogin
-      include CommandWithoutArgs
-      
+
+      attr_accessor :target
+
+      def crack!
+        self.target = !cmd.args ? enactor_name : trim_input(cmd.args)
+      end
+
+      def check_permission
+        return nil if self.target == enactor_name
+        return nil if enactor.has_any_role?(Global.read_config("fs3skills", "roles", "can_view_sheets"))
+        return t('dispatcher.not_allowed')
+      end
+
       def handle
-        template = XpTemplate.new(enactor)
-        client.emit template.render
+        ClassTargetFinder.with_a_character(self.target, client, enactor) do |model|
+          template = XpTemplate.new(model)
+          client.emit template.render
+        end
       end
     end
   end
