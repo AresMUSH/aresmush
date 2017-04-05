f0fe70b67fd2bfaecc1d981845cf5ef94d371345
lynnfaraday <lynn@wordsmyth.org>
2017-04-05 01:23:52 -0400

    Fixing module names for events.

--- game/plugins/plugins.rb
+++ game/plugins/plugins.rb
@@ -14,7 +14,7 @@ module AresMUSH
         "custom",
         "demographics", 
         "describe", 
-        "events_teamup",  # You can make this events_local if you don't like teamup
+        "events",  # You can make this events_local if you don't like teamup
         "friends", 
         "fs3combat", 
         "fs3skills", 
