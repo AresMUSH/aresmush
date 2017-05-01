246e5cde375bdc0d60bbda0984c12ea8e21390ba
lynnfaraday <lynn@wordsmyth.org>
2017-04-30 20:13:41 -0400

    Moving mail prefs onto char.

--- game/plugins/mail/lib/mail_inbox_cmd.rb
+++ game/plugins/mail/lib/mail_inbox_cmd.rb
@@ -4,9 +4,9 @@ module AresMUSH
       include CommandHandler
            
       def handle        
-        prefs = Mail.get_or_create_mail_prefs(enactor)
-        show_from = prefs.mail_filter != Mail.sent_tag && !prefs.mail_filter.start_with?("review")
-        template = InboxTemplate.new(enactor, Mail.filtered_mail(enactor), show_from, prefs.mail_filter)
+        filter = enactor.mail_filter
+        show_from = filter != Mail.sent_tag && !filter.start_with?("review")
+        template = InboxTemplate.new(enactor, Mail.filtered_mail(enactor), show_from, filter)
         client.emit template.render
       end
     end
