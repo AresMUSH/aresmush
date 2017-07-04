b4cde19a9579ac5cebd23b853004e0e861490b57
lynnfaraday <lynn@wordsmyth.org>
2017-07-04 08:34:55 -0400

    Trap config errors more sensibly.

--- lib/aresmush/config/config_reader.rb
+++ lib/aresmush/config/config_reader.rb
@@ -59,7 +59,12 @@ module AresMUSH
     
     def load_config_file(file)
       Global.logger.debug "Loading config from #{file}."
-      self.config = self.config.merge_yaml(file)
+      begin 
+        self.config = self.config.merge_yaml(file)
+      rescue Exception => ex
+        # Turn mysterious YAML errors into something a little more useful.
+        raise "Error reading YAML from #{file}.  See http://aresmush.com/tutorials/code/yaml/ for troubleshooting help: #{ex}"
+      end
     end
     
   end
