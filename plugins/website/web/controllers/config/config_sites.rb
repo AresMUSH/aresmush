6ac7f0444b6403203f99599e2d30a580ea87b7a7
lynnfaraday <lynn@wordsmyth.org>
2017-12-17 15:00:09 -0500

    Improving suspect and ban config.

---
+++ plugins/website/web/controllers/config/config_sites.rb
@@ -0,0 +1,29 @@
+module AresMUSH
+  class WebApp
+    get '/admin/config_sites/?', :auth => :admin do
+      @sites = Global.read_config("sites")
+
+      erb :"admin/config_sites"
+    end
+    
+    post '/admin/config_sites/update', :auth => :admin do
+      
+      @sites = Global.read_config("sites")
+      puts @params.inspect
+      
+      @sites['banned'] = @params[:banned_sites].split("\r\n").map { |n| n.downcase }
+      @sites['suspect'] = @params[:suspect_sites].split("\r\n").map { |n| n.downcase }
+      @sites['ban_proxies'] = !!@params[:ban_proxies]
+      config = {
+        'sites' => @sites
+      }
+      write_config_file File.join(AresMUSH.game_path, 'config', 'sites.yml'), config.to_yaml
+      
+      
+      flash[:info] = "Saved!"
+      reload_config
+      redirect '/admin'
+    end
+        
+  end
+end
