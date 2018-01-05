82b92151a33f14ef5d34f5c6b7f74c09cba808ce
lynnfaraday <lynn@wordsmyth.org>
2018-01-04 23:47:26 -0500

    Character gallery.

--- plugins/profile/lib/_load.rb
+++ plugins/profile/lib/_load.rb
@@ -1,5 +1,7 @@
 $:.unshift File.dirname(__FILE__)
 
+load "character_groups_request_handler.rb"
+load "character_request_handler.rb"
 load "characters_request_handler.rb"
 load "helpers.rb"
 load "profile_model.rb"
--- plugins/website/lib/get_game_info_handler.rb
+++ plugins/website/lib/get_game_info_handler.rb
@@ -17,11 +17,11 @@ module AresMUSH
           website_welcome: WebHelpers.format_markdown_for_html(Global.read_config('website', 'website_welcome')),
           onlineCount: Global.client_monitor.logged_in.count,
           ictime: ICTime.ic_datestr(ICTime.ictime),
-          config: Global.config_reader.config,
           date_entry_format: Global.read_config('date_and_time', 'date_entry_format_help').upcase,
           disabled_plugins: disabled,
           who_count: Global.client_monitor.logged_in.count,
-          scene_count: Scene.all.select { |s| !s.completed }.count
+          scene_count: Scene.all.select { |s| !s.completed }.count,
+          roster_enabled: Idle.roster_enabled?
         } 
       end
     end
