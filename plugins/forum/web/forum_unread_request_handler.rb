a9194d4fd2ae01f2f10e9abc24d2dad333026f45
lynnfaraday <lynn@wordsmyth.org>
2018-01-29 01:54:07 -0500

    Forum next unread.

---
+++ plugins/forum/web/forum_unread_request_handler.rb
@@ -0,0 +1,30 @@
+module AresMUSH
+  module Forum
+    class ForumUnreadRequestHandler
+      def handle(request)
+                
+        enactor = request.enactor
+        
+        error = WebHelpers.check_login(request)
+        return error if error
+        
+        first_unread = nil
+        category = nil
+        BbsBoard.all_sorted.each do |b|
+          category = b
+          first_unread = b.first_unread(enactor)
+          break if first_unread
+        end
+          
+        if (!first_unread)
+          return {}
+        end
+        
+        {
+          category_id: category.id,
+          post_id: first_unread.id
+        }
+      end
+    end
+  end
+end
