4f86a1a96d742f5bb41d5d7c4552bf54b9e72156
lynnfaraday <lynn@wordsmyth.org>
2018-03-06 01:14:18 -0500

    Unnecessary AWS backup, redis install script tweak, FS3 opposed roll fix.

--- plugins/fs3skills/commands/roll_opposed_cmd.rb
+++ plugins/fs3skills/commands/roll_opposed_cmd.rb
@@ -19,41 +19,51 @@ module AresMUSH
       end
       
       def handle
-        ClassTargetFinder.with_a_character(self.name1, client, enactor) do |model1|
-          
-          if (self.name2)
-            result = ClassTargetFinder.find(self.name2, Character, enactor)
-            model2 = result.target
-            self.name2 = !model2 ? self.name2 : model2.name
-          end
-                                  
-          if (!model2 && !self.roll_str2.is_integer?)
-            client.emit_failure t('fs3skills.numbers_only_for_npc_skills')
-            return
-          end
-          
-          die_result1 = FS3Skills.parse_and_roll(client, model1, self.roll_str1)
-          die_result2 = FS3Skills.parse_and_roll(client, model2, self.roll_str2)
-            
-          return if !die_result1
-          return if !die_result2
-            
-          successes1 = FS3Skills.get_success_level(die_result1)
-          successes2 = FS3Skills.get_success_level(die_result2)
-            
-          results = FS3Skills.opposed_result_title(self.name1, successes1, self.name2, successes2)
-            
-          message = t('fs3skills.opposed_roll_result', 
-          :name1 => model1.name,
-          :name2 => !model2 ? t('fs3skills.npc', :name => self.name2) : model2.name,
-          :roll1 => self.roll_str1,
-          :roll2 => self.roll_str2,
-          :dice1 => FS3Skills.print_dice(die_result1),
-          :dice2 => FS3Skills.print_dice(die_result2),
-          :result => results)  
-            
-          FS3Skills.emit_results message, client, enactor_room, false
+        
+        result = ClassTargetFinder.find(self.name1, Character, enactor)
+        model1 = result.target
+        if (!model1 && !self.roll_str1.is_integer?)
+          client.emit_failure t('fs3skills.numbers_only_for_npc_skills')
+          return
         end
+                                
+        if (!model2 && !self.roll_str2.is_integer?)
+          client.emit_failure t('fs3skills.numbers_only_for_npc_skills')
+          return
+        end
+                  
+        if (self.name2)
+          result = ClassTargetFinder.find(self.name2, Character, enactor)
+          model2 = result.target
+          self.name2 = !model2 ? self.name2 : model2.name
+        end
+                                
+        if (!model2 && !self.roll_str2.is_integer?)
+          client.emit_failure t('fs3skills.numbers_only_for_npc_skills')
+          return
+        end
+          
+        die_result1 = FS3Skills.parse_and_roll(client, model1, self.roll_str1)
+        die_result2 = FS3Skills.parse_and_roll(client, model2, self.roll_str2)
+          
+        return if !die_result1
+        return if !die_result2
+          
+        successes1 = FS3Skills.get_success_level(die_result1)
+        successes2 = FS3Skills.get_success_level(die_result2)
+            
+        results = FS3Skills.opposed_result_title(self.name1, successes1, self.name2, successes2)
+          
+        message = t('fs3skills.opposed_roll_result', 
+        :name1 => !model1 ? t('fs3skills.npc', :name => self.name1) : model1.name,
+        :name2 => !model2 ? t('fs3skills.npc', :name => self.name2) : model2.name,
+        :roll1 => self.roll_str1,
+        :roll2 => self.roll_str2,
+        :dice1 => FS3Skills.print_dice(die_result1),
+        :dice2 => FS3Skills.print_dice(die_result2),
+        :result => results)  
+          
+        FS3Skills.emit_results message, client, enactor_room, false
       end
     end
   end
