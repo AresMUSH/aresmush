dee54565f568ce6e3eee39bb9e197fc258c45c32
lynnfaraday <lynn@wordsmyth.org>
2018-03-13 23:04:29 -0400

    Wiki export using templates.

--- plugins/website/wiki_exporter.rb
+++ plugins/website/wiki_exporter.rb
@@ -1,104 +1,241 @@
+require 'open-uri'
+
 module AresMUSH
   module Website
     module WikiExporter
+      
+      class ExportHandlebarsTemplate
+        def initialize(file)
+          template = File.read(file)
+          @handlebars = Handlebars::Context.new
+          template_contents = File.read(file)
+          
+          @handlebars.register_helper("link-to") do |this, page, param1, param2, block |
+            if (block)
+              "<a href=\"index.html\">#{block.fn(this)}</a>"
+            elsif (param2)
+              "<a href=\"index.html\">#{param2.fn(this)}</a>"
+            else 
+              "<a href=\"index.html\">#{param1.fn(this)}</a>"
+            end
+          end
+          
+          @handlebars.register_helper("mut") do |this, context, block|
+            ""
+          end
+          
+          @handlebars.register_helper("not") do |this, context, block|
+            true
+          end
+          
+          @handlebars.register_helper("action") do |this, context, block|
+            ""
+          end
+          
+          @handlebars.register_helper("not-eq") do |this, context, block|
+            ""
+          end
+          
+          template_path = File.join(AresMUSH.root_path, "..", "ares-webportal", "app", "templates")
+          
+          file = File.join(template_path, 'components', 'fs3-sheet.hbs')
+          @handlebars.register_partial("fs3-sheet", File.read(file))
+
+          file = File.join(template_path, 'components', 'fs3-damage.hbs')
+          @handlebars.register_partial("fs3-damage", File.read(file))
+          
+          
+          template_contents = template_contents.gsub("{{{ansi-format text=", "{{{")
+
+          template_contents = template_contents.gsub("{{fs3-sheet char=char}}", "{{>fs3-sheet}}")
+          template_contents = template_contents.gsub("{{fs3-damage char=char}}", "{{>fs3-damage}}")
+          
+
+          template_contents = replace_char_icon(template_contents, "c")
+          template_contents = replace_char_icon(template_contents, "p")
+          
+          @template = @handlebars.compile(template_contents)
+      
+          
+        end
+      		      
+        def render(data)
+          @template.call(data)
+        end	
+        
+        def replace_char_icon(template_contents, key)
+          template_contents.gsub("{{char-icon char=#{key}}}", "<div class=\"char-icon-container\"><div class=\"log-icon-container\"><a href=\"{{#{key}.name}}.html\"><img src=\"game/uploads/{{#{key}.icon}}\" class=\"log-icon\"/></a></div></div><div class=\"log-icon-title-container\"><div class=\"log-icon-title\">{{#{key}.name}}</div></div>") 
+        end	
+      end
+      
       def self.export
-        export_path = File.join(AresMUSH.root_path, "wiki_export")
-        unless File.directory?(export_path)
-          FileUtils.mkdir_p(export_path)
-        end
-  
-        index = {}
-        index["Characters"] = {}
-        index["Scenes"] = {}
-        index["Wiki"] = {}
-        
-        WikiPage.all.to_a.sort_by { |p| p.heading }.each do |p|
-          page_name = "#{p.name}.html"
-          index["Wiki"][page_name] = p.heading
-          
-          File.open(File.join(export_path, page_name), 'w') do |f|
-            text = WebHelpers.format_markdown_for_html(p.current_version.text)
-            text = text.gsub("src=\"/", "src=\"")
-            text = text.gsub(/href\=\"\/wiki\/([^\"]+)\/?/) { "href=\"#{FilenameSanitizer.sanitize($1)}.html" }
-            text = AnsiFormatter.strip_ansi(text)
-            f.puts WikiExporter.format_wiki_template(text, p.heading)
+        begin
+          export_path = File.join(AresMUSH.root_path, "wiki_export")
+          unless File.directory?(export_path)
+            FileUtils.mkdir_p(export_path)
           end
-        end
   
-        Scene.all.select { |s| s.shared }.sort_by { |s| s.icdate }.each do |s|
-          filename = FilenameSanitizer.sanitize(s.title)
-          page_name = "#{s.icdate}-#{filename}.html"
-          index["Scenes"][page_name] = "#{s.icdate} - #{s.title}"
-          
-          File.open(File.join(export_path, page_name), 'w') do |f|
-
-            heading = "<p><b>Participants:</b> #{s.participant_names.join(',')}</p>"
-            heading << "<p><b>Location:</b> #{s.location}</p>"
-            heading << "<p><b>Summary:</b> #{s.summary}</p>"
-
-            log = WebHelpers.format_markdown_for_html(s.scene_log.log)
+          index = {}
+          index["Characters"] = {}
+          index["Scenes"] = {}
+          index["Wiki"] = {}
+        
+          template_path = File.join(AresMUSH.root_path, "..", "ares-webportal", "app", "templates")
+                
+       
+          WikiPage.all.to_a.sort_by { |p| p.heading }.each do |p|
+            page_name = "#{FilenameSanitizer.sanitize(p.name)}.html"
+            index["Wiki"][page_name] = p.heading
             
-            text = "#{heading}#{log}"
-            f.puts WikiExporter.format_wiki_template(text, s.title)
-          end
-        end
-        
-        
-        Character.all.select { |c| !c.is_admin? && c.is_approved? }.sort_by { |c| c.name }.each do |c|
-          page_name = "#{c.name}.html"
-          index["Characters"][page_name] = "#{c.name} (#{Ranks.military_name(c)})"
-          
-          File.open(File.join(export_path, page_name), 'w') do |f|
-            icon = WebHelpers.icon_for_char(c)
-            text = "<img style=\"max-height: 200px\" src=\"game/uploads/#{icon}\">"
-            text << c.demographics.sort.map { |k, v| "<p><b>#{k}:</b> #{v}</p>"}.join
-            text << "<h2>Background</h2>"
-            text << WebHelpers.format_markdown_for_html(c.background || "")
-            text << "<h2>Profile</h2>"
-            text << c.profile.sort.map { |k, v| "<h3>#{k}:</h3> <p>#{WebHelpers.format_markdown_for_html(v)}</p>"}.join
-            text << "<h2>Relationships</h2>"
-            text << c.relationships.sort.map { |k, v| "<h3>#{k}:</h3> <p>#{WebHelpers.format_markdown_for_html(v['relationship'])}</p>"}.join
-            text = text.gsub(/href\=\"\/([^\"]+)\/?/, `href="\1.html"`)
-            text = text.gsub("src=\"/", "src=\"")
-            (c.profile_gallery || []).each do |g|
-              text << "<img style=\"max-height: 200px\" src=\"game/uploads/#{g}\">"
+            puts "Parsing wiki #{p.heading}"
+            
+            File.open(File.join(export_path, page_name), 'w') do |f|
+              request = WebRequest.new( { args: { id: p.id } } )
+              response = Website::GetWikiPageRequestHandler.new.handle(request)
+              f.puts render_template(File.join(template_path, 'wiki-page.hbs'), { model: response }, p.heading)
             end
-
-            f.puts WikiExporter.format_wiki_template(text,c.name)
           end
-        end
+          
+          Scene.all.select { |s| s.shared }.sort_by { |s| s.icdate }.each do |s|
+            puts "Parsing scene #{s.id} #{s.title}"
+          
+            filename = FilenameSanitizer.sanitize(s.title)
+            page_name = "#{s.icdate}-#{filename}.html"
+            index["Scenes"][page_name] = s.date_title
+          
+            File.open(File.join(export_path, page_name), 'w') do |f|
+            
+              request = WebRequest.new( { args: { id: s.id } } )
+              response = Scenes::GetSceneRequestHandler.new.handle(request)
+              
+              f.puts render_template(File.join(template_path, 'scene.hbs'), { model: response }, s.date_title)
+            end
+          end
         
-        File.open(File.join(export_path, "index.html"), 'w') do |f|
+        
+          Chargen.approved_chars.select { |c| !c.is_admin? && c.is_approved? }.sort_by { |c| c.name }.each do |c|
+            puts "Parsing character #{c.name}"
+          
+            page_name = "#{c.name}.html"
+            index["Characters"][page_name] = "#{c.name} (#{Ranks.military_name(c)})"
+          
+            File.open(File.join(export_path, page_name), 'w') do |f|
+              template = ExportHandlebarsTemplate.new(File.join(template_path, 'char.hbs'))
+              request = WebRequest.new( { args: { id: c.id } } )
+              response = Profile::CharacterRequestHandler.new.handle(request)
+              
+              profile = ""
+              profile << '<script type="text/javascript" src="http://www.youtube.com/player_api"></script>'
+              profile << '<script type="text/javascript" src="game/scripts/music_player.js"></script>'
+
+              profile << "<h1>#{c.name}</h1>"
+              profile << "<div class=\"row profile-wrap\">"
+              request = WebRequest.new( { args: { id: c.id } } )
+              
+              profile << render_partial(File.join(template_path, 'components', 'profile-demographics.hbs'), { char: response })
+              profile << "</div>"
+              
+              profile << '<div class="profile-tab">'
+              profile << render_partial(File.join(template_path, 'components', 'profile-supplemental.hbs'), { char: response })
+              profile << "</div>"
+              
+              profile << '<div class="profile-tab">'
+              profile << render_partial(File.join(template_path, 'components', 'profile-relationships.hbs'), { char: response })
+              profile << "</div>"
+              
+              profile << '<div class="profile-tab">'
+              profile << render_partial(File.join(template_path, 'components', 'profile-system.hbs'), { char: response })
+              profile << "</div>"
+              
+              profile << '<div class="profile-tab">'
+              profile << render_partial(File.join(template_path, 'components', 'profile-gallery.hbs'), { char: response })
+              profile << "</div>"
+              
+              
+              f.puts format_wiki_template(profile, c.name)
+              
+            end
+          end
+        
           index.each do |section, data|
-            f.puts "<h1>#{section}</h1>"
-            
-            f.puts "<ul>"
-            data.each do |page, title|
-              f.puts "<li><a href=\"#{page}\">#{title}</a>"
+            File.open(File.join(export_path, "#{section}.html"), 'w') do |f|
+              text = ""
+              if (section == 'Wiki')
+                text << "<p><b><a href=\"home.html\">Wiki Home</a></b></p>"
+              end
+              
+              text << "<ul>"
+              data.each do |page, title|
+                text << "<li><a href=\"#{page}\">#{title}</a>"
+              end
+              text << "</ul>"
+              
+              f.puts format_wiki_template(text, section)
+              
             end
-            f.puts "</ul>"
           end
-        end
+          
+          File.open(File.join(export_path, "characters.html"), 'w') do |f|
+            data = Chargen.approved_chars.map { |c| 
+              {
+                name: c.name,
+                icon: WebHelpers.icon_for_char(c)
+              }
+            }
+            text = render_partial(File.join(template_path, 'components', 'char-group-list.hbs'), { chars: data } )
+            f.puts format_wiki_template text, "Characters"
+          end
+          
+          
+          File.open(File.join(export_path, "index.html"), 'w') do |f|
+            text = '<div class="jumbotron">'
+            text << "<img src=\"game/uploads/theme_images/jumbotron.png\" />"
+            tagline = Global.read_config("website", "website_tagline")
+            text << "<div class=\"jumbotron-tagline\">#{tagline}</div>"
+            welcome = Global.read_config("website", "website_welcome")
+            welcome = WebHelpers.format_markdown_for_html(welcome)
+            text << "<p>#{welcome}</p>"
+            text << "</div>"
+            f.puts format_wiki_template(text, "Home")
+          end
         
-        path = File.join(export_path, "game")
-        unless File.directory?(path)
-          FileUtils.mkdir_p(path)
-        end
+          path = File.join(export_path, "game")
+          unless File.directory?(path)
+            FileUtils.mkdir_p(path)
+          end
   
-        path = File.join(export_path, "game", "uploads")
-        FileUtils.cp_r(File.join(AresMUSH.game_path, "uploads"), path)
+          path = File.join(export_path, "game")
+          FileUtils.cp_r(File.join(AresMUSH.game_path, "uploads"), path)
+          FileUtils.cp_r(File.join(AresMUSH.game_path, "styles"), path)
+          FileUtils.cp_r(File.join(AresMUSH.game_path, "scripts"), path)
+          
+        rescue Exception => ex
+          puts "Error doing web export: #{ex} #{ex.backtrace[0,10]}"
+        end
       end    
 
-      def self.format_wiki_template(text, heading)
-        html = "<html><body>"
-        html << '<div style="background-image:url(game/uploads/theme_images/background.png);width:100%;height:200px;background-size:cover">'
-        html << '<h1 style="color:white;background-color:rgba(255,255,255,0.5)">TEST</h1>'
-        html << "</div>"
-        html << "<h1>#{heading}</h1>"
-        html << text
-        html << "</body></html>"
+      def self.clean_text(text)
+        text = text.gsub("src=\"/", "src=\"")
+        text = text.gsub(/href\=\"\/wiki\/([^\"]+)\/?/) { "href=\"#{FilenameSanitizer.sanitize($1)}.html" }
+        AnsiFormatter.strip_ansi(text)
+      end
+
+      def self.render_partial(template_path, data)
+        template = ExportHandlebarsTemplate.new(template_path)
+        text = template.render(data)
+        text
+      end
+            
+      def self.render_template(template_path, data, title)
+        template = ExportHandlebarsTemplate.new(template_path)
+        text = template.render(data)
+        WikiExporter.format_wiki_template(text, title)
+      end
       
-        AnsiFormatter.strip_ansi html
+      def self.format_wiki_template(text, title)
+        text = clean_text(text)
+        template = HandlebarsTemplate.new(File.join(AresMUSH.plugin_path, 'website', 'templates', 'wiki_export.hbs'))
+        template.render(body: text, title: title, mush_name: Global.read_config("game", "name"))
       end
     end
   end
