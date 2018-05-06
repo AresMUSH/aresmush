78163c725c454a16d7de685de658163c50ae2287
Linda Naughton <lynn@wordsmyth.org>
2018-05-05 22:42:38 -0400

    Fixing hooks commands and private sheet behavior.

--- plugins/fs3skills/web/char_profile_request_handler.rb
+++ plugins/fs3skills/web/char_profile_request_handler.rb
@@ -17,12 +17,15 @@ module AresMUSH
             date: d.ictime_str,
             description: d.description,
             severity: WebHelpers.format_markdown_for_html(FS3Combat.display_severity(d.initial_severity))
-            }          
-          }
-        else
-          damage = nil
-        end
+          }          
+        }
+      else
+        damage = nil
+      end
         
+      show_sheet = FS3Skills.can_view_sheets?(enactor) || enactor.id == char.id
+        
+      if (show_sheet)
         {
           attributes: get_ability_list(char.fs3_attributes),
           action_skills: get_ability_list(char.fs3_action_skills),
@@ -30,20 +33,27 @@ module AresMUSH
           languages: get_ability_list(char.fs3_languages),
           advantages: get_ability_list(char.fs3_advantages),
           use_advantages: FS3Skills.use_advantages?,
-          damage: damage
+          damage: damage,
+          show_sheet: show_sheet
+        }
+      else
+        {
+          damage: damage,
+          show_sheet: show_sheet
         }
       end
+    end
       
-      def get_ability_list(list)        
-        list.to_a.sort_by { |a| a.name }.map { |a| 
-          { 
-            name: a.name, 
-            rating: a.rating, 
-            rating_name: a.rating_name
+    def get_ability_list(list)        
+      list.to_a.sort_by { |a| a.name }.map { |a| 
+        { 
+          name: a.name, 
+          rating: a.rating, 
+          rating_name: a.rating_name
           }}
-      end
+        end
             
+      end
     end
   end
-end
 
