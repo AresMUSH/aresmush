3963a43470ce52e3ad674cec3733e44f352260ec
Linda Naughton <lynn@wordsmyth.org>
2018-05-05 22:44:41 -0400

    Fixing wiki edit locks.

---
+++ plugins/website/web/cancel_wiki_page_edit_handler.rb
@@ -0,0 +1,29 @@
+module AresMUSH
+  module Website
+    class CancelEditWikiPageRequestHandler
+      def handle(request)
+        name_or_id = request.args[:id]
+        enactor = request.enactor
+
+        error = WebHelpers.check_login(request)
+        return error if error
+        
+        name = WikiPage.sanitize_page_name(name)
+        page = WikiPage.find_by_name_or_id(name_or_id)
+        if (!page)
+          return { error: t('webportal.not_found') }
+        end
+        
+        lock_info = page.get_lock_info(enactor)
+        if (lock_info)
+          return
+        end
+
+        page.update(locked_by: nil)
+        
+        {
+        }
+      end
+    end
+  end
+end
