b30e9b3d99e131ea216b35f64e5b42cb37408f10
Linda Naughton <lynn@wordsmyth.org>
2018-05-18 20:30:58 -0400

    Fixing migration.

--- install/migrations/0003_beta13_update.rb
+++ install/migrations/0003_beta13_update.rb
@@ -8,11 +8,33 @@ module AresMUSH
       def migrate
         Global.logger.debug "Fixing scene config typo."
         
-        custom_config = DatabaseMigrator.read_config_file("scenes.yml")
-        if (!Global.read_config("scenes", "unshared_scene_deletion_days"))
-          custom_config["scenes"]["unshared_scene_deletion_days"] = Global.read_config("scenes", "unsared_scene_deletion_days") || 20
+        config = DatabaseMigrator.read_config_file("scenes.yml")
+        if (!config['scenes']['unshared_scene_deletion_days'])
+          config['scenes']['unshared_scene_deletion_days'] = config['scenes']['unsared_scene_deletion_days'] || 20
         end
-        DatabaseMigrator.write_config_file("scenes.yml", custom_config)
+        config['scenes'].delete 'unsared_scene_deletion_days'
+        DatabaseMigrator.write_config_file("scenes.yml", config)
+
+        Global.logger.debug "Adding engine api proxy setting."
+        config = DatabaseMigrator.read_config_file("website.yml")
+        config['website']['use_api_proxy'] = false
+        DatabaseMigrator.write_config_file("website.yml", config)
+        
+        
+        Global.logger.debug "Adding desc tag colors."
+        config = DatabaseMigrator.read_config_file("describe.yml")
+        config['describe']['tag_colors'] = { "admin" => "%xb", "beginner" => "%xg"}
+        DatabaseMigrator.write_config_file("describe.yml", config)
+        
+        
+        Global.logger.debug "Changing hook shortcut."
+        config = DatabaseMigrator.read_config_file("chargen.yml")
+        shortcuts = config['chargen']['shortcuts']
+        shortcuts.delete 'hook/set'
+        shortcuts['hook/add'] = 'hook/set'
+        config['chargen']['shortcuts'] = shortcuts
+        DatabaseMigrator.write_config_file("chargen.yml", config)
+        
                      
       end
     end
