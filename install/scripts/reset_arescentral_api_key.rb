20daf734e2a1ad717a22618ccf9065cafd44f9a3
Linda Naughton <lynn@wordsmyth.org>
2018-05-26 19:36:29 -0400

    Allow reset of api key to nil.

--- install/scripts/reset_arescentral_api_key.rb
+++ install/scripts/reset_arescentral_api_key.rb
@@ -1,13 +1,10 @@
 module AresMUSH
   
   puts "======================================================================="
-  puts "Changing Arescentral api key to the desired value.  The API key must come from the AresCentral admin."
+  puts "Resetting AresCentral API key."
   puts "======================================================================="
 
   new_key = ENV['ares_rake_param']
-  if (!new_key)
-    raise "Missing key!"
-  else
     
     game = Game.master
     game.update(api_key, new_key)
