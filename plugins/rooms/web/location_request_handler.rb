1372b8a9e5e4bb547f0f0573b24b8d4866e1eea3
Linda Naughton <lynn@wordsmyth.org>
2018-07-01 19:58:23 -0400

    Fix area navigation.

--- plugins/rooms/web/location_request_handler.rb
+++ plugins/rooms/web/location_request_handler.rb
@@ -3,19 +3,46 @@ module AresMUSH
     class LocationRequestHandler
       def handle(request)
         id = request.args[:id]
+        enactor = request.enactor
         
-        room = Room[id]
-        if (!room)
+        error = WebHelpers.check_login(request, true)
+        return error if error
+        
+        area = Area[id]
+        if (!area)
           return { error: t('webportal.not_found') }
         end
         
         {
-          id: id,
-          name: room.name,
-          area: room.area_name,
-          description: WebHelpers.format_markdown_for_html(room.description || "No description set." )
+          area: build_area_definition(area),
+          can_manage: Rooms.can_build?(enactor),
+          directory: Rooms.top_level_areas.map { |a| build_directory(a) }
         }
       end
+      
+      def build_area_definition(area)
+        { 
+          name: area.name,
+          id: area.id,
+          parent: area.parent ? { name: area.parent.name, id: area.parent.id } : nil,
+          description: area.description ? WebHelpers.format_markdown_for_html(area.description) : "",
+          locations: area.rooms.to_a.sort_by { |r| r.name }.map { |r| {
+            name: r.name,
+            id: r.id,
+            description: r.description ? WebHelpers.format_markdown_for_html(r.description) : ""
+            }},
+          children: area.sorted_children.map { |a| build_area_definition(a) }
+        }
+      end
+        
+      def build_directory(area)
+        {
+          id: area.id,
+          name: area.name,
+          children: area.children.map { |a| build_directory(a) }
+        }
+      end
+        
     end
   end
 end
