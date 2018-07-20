0424de465284d698df0007e2f99b7c7cea23b37c
Linda Naughton <lynn@wordsmyth.org>
2018-07-19 22:02:08 -0400

    Edit colors more easily.

---
+++ plugins/website/web/save_colors_request_handler.rb
@@ -0,0 +1,26 @@
+module AresMUSH
+  module Website
+    class SaveColorsRequestHandler
+      def handle(request)
+        colors = request.args[:colors]
+        enactor = request.enactor
+        
+        error = WebHelpers.check_login(request)
+        return error if error
+        
+        if (!Manage.can_manage_game?(enactor))
+          return { error: t('dispatcher.not_allowed') }
+        end
+                
+        path = File.join(AresMUSH.game_path, "styles", "colors.scss")
+        File.open(path, 'w') do |file|
+          colors.each { |name, value| file.puts "$#{name}:#{value};"}
+        end
+        
+        WebHelpers.rebuild_css
+        
+        {}
+      end
+    end
+  end
+end
