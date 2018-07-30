ce66f264d0227f0a46bbe11eb709a642630d5c26
Linda Naughton <lynn@wordsmyth.org>
2018-07-30 18:58:12 -0400

    Add admin notes.

--- plugins/utils/commands/note_delete_cmd.rb
+++ plugins/utils/commands/note_delete_cmd.rb
@@ -3,28 +3,45 @@ module AresMUSH
     class NoteDeleteCmd
       include CommandHandler
       
-      attr_accessor :name
+      attr_accessor :name, :target, :section
 
       def parse_args
-        self.name = titlecase_arg(cmd.args)
+        args = cmd.parse_args(ArgParser.arg1_equals_optional_arg2)
+        if (args.arg2)
+          self.target = titlecase_arg(args.arg1)
+          self.name = titlecase_arg(args.arg2)
+          self.section = 'admin'
+        else
+          self.target = enactor_name
+          self.name = titlecase_arg(args.arg1)
+          self.section = 'player'
+        end
       end
       
       def required_args
         [ self.name ]
       end
       
+      def check_is_allowed
+        return nil if self.target == enactor_name
+        return nil if Utils.can_access_notes?(enactor)
+        return t('dispatcher.not_allowed')
+      end
+      
       def handle
-        notes = enactor.notes
+        ClassTargetFinder.with_a_character(self.target, client, enactor) do |model|
+          notes = model.notes_section(self.section)
         
-        if (!notes.has_key?(self.name))
-          client.emit_failure t('notes.note_does_not_exist')
-          return
-        end
+          if (!notes.has_key?(self.name))
+            client.emit_failure t('notes.note_does_not_exist')
+            return
+          end
         
-        notes.delete self.name
-        enactor.update(notes: notes)
+          notes.delete self.name
+          model.update_notes_section(self.section, notes)
 
-        client.emit_success t('notes.note_deleted')
+          client.emit_success t('notes.note_deleted')
+        end
       end
     end
   end
