f41f91d34e2764a1f5c10598fe961da853e28539
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-08-13 16:47:56 -0400

    Adding spells
    Added spells, added spell action, made combat/newturn set has_cast to false, added weapon config.

--- plugins/custom/custom.rb
+++ plugins/custom/custom.rb
@@ -5,11 +5,11 @@ module AresMUSH
     def self.plugin_dir
       File.dirname(__FILE__)
     end
- 
+
     def self.shortcuts
       Global.read_config("custom", "shortcuts")
     end
- 
+
     def self.get_cmd_handler(client, cmd, enactor)
 
     #Secrets
@@ -31,54 +31,7 @@ module AresMUSH
           return GMSecretsCmd
         end
       end
-    #Potions
-      case cmd.root
-      when "potion"
-        case cmd.switch
-        when "create"
-          return CreatePotionCmd        
-        when "update"
-          return UpdatePotionCmd
-        when "give"
-          return GivePotionCmd
-        when "use"
-          return PotionUseCmd
-        end
-      end
-      case cmd.root
-      when "potions"
-        return PotionsCmd
-      end
-    #Spells
-      case cmd.root
-      when "spell"
-        case cmd.switch
-        when "request"
-          return SpellRequestCmd
-        when "mod"
-          return SpellModCmd
-        when "modis"
-          return SpellModIsCmd
-        when "cast"
-          if cmd.args.include?("=")
-            return SpellCastWithTargetCmd
-            client.emit "With target."
-          else
-            return SpellCastCmd
-          end    
-        end
-      end
-    #Schools
-      case cmd.root
-      when "school"
-        case cmd.switch
-        when "set"
-          return SetSchoolsCmd
-        else  
-          return SchoolsCmd
-        end
-      end
-        
+    
 
       return nil
     end
