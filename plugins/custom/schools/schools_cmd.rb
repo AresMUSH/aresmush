2155b7a3da32ba1e94ac28bbef32d2d673a56baf
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-08-13 15:28:51 -0400

    Error if !target
    Adding character not found error to schools command.

--- plugins/custom/schools/schools_cmd.rb
+++ plugins/custom/schools/schools_cmd.rb
@@ -2,27 +2,29 @@ module AresMUSH
   module Custom
     class SchoolsCmd
       include CommandHandler
-      
+
       attr_accessor :target
-      
+
         def parse_args
             self.target = Character.find_one_by_name(cmd.args)
         end
-        
-      
+
+
         def handle
-            # client.emit line_with_text ("#{target.name}'s Schools")
+          if !target
+            client.emit_failure t('custom.invalid_name')
+          else
             client.emit ("#{target.name}'s Schools")
             client.emit "Major School: #{target.major_school}"
               if target.minor_school != nil
                 client.emit "Minor School: #{target.minor_school}"
               end
-            # client.emit footer
-            
-            
+          end
+
+
         end
-        
-        
+
+
     end
   end
 end
