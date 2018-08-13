91184643ca906ffe9d7a1efd5227da6129fb4e0a
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-08-13 15:30:34 -0400

    error if !char
    Error if character not found.

--- plugins/custom/schools/set_schools_cmd.rb
+++ plugins/custom/schools/set_schools_cmd.rb
@@ -3,7 +3,7 @@ module AresMUSH
     class SetSchoolsCmd
       include CommandHandler
 # school/set <name>=<major/minor>/<school>` - Set a school on a character and sets their matching attribute to 1 (minor) or 2(major).
-      
+
       attr_accessor :school_type, :char, :school
 
       def parse_args
@@ -12,39 +12,40 @@ module AresMUSH
         self.school_type = titlecase_arg(args.arg2)
         self.school = titlecase_arg(args.arg3)
       end
-        
+
       def check_can_set
+        return  t('custom.invalid_name') if ! self.char
         return nil if enactor_name == self.char
         return nil if FS3Skills.can_manage_abilities?(enactor)
         return t('dispatcher.not_allowed')
-      end      
-      
+      end
+
       def check_chargen_locked
         return nil if FS3Skills.can_manage_abilities?(enactor)
         Chargen.check_chargen_locked(enactor)
       end
-        
-        
-      def handle   
+
+
+      def handle
         school_list = Global.read_config("schools")
         if school_list.include?(self.school)
           client.emit "Name: #{self.char.name} Type: #{school_type} School: #{self.school}"
           if school_type == "Major"
-            self.char.update(major_school: self.school)          
+            self.char.update(major_school: self.school)
             client.emit_success "You set #{self.char.name}'s major school to #{self.school}. "
             FS3Skills.set_ability(client, self.char, self.school, 2)
           elsif school_type == "Minor"
             self.char.update(minor_school: self.school)
             client.emit_success "You set #{self.char.name}'s minor school to #{self.school}."
-            FS3Skills.set_ability(client, self.char, self.school, 1)          
+            FS3Skills.set_ability(client, self.char, self.school, 1)
           else client.emit_failure "That is not a type of school. Choose major or minor."
           end
         else
           client.emit_failure "That is not a school of magic. Type 'schools' to see the list of schools."
         end
-       end       
-        
-        
+       end
+
+
     end
   end
 end
