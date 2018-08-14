1db38abd9ded42bf77a760ff29c67223552f7599
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-08-14 11:53:35 -0400

    Added potions
    Added all potion files, the cron (to places cron). Seems to be working as intended.

---
+++ plugins/custom/potions/potions_cmd.rb
@@ -0,0 +1,19 @@
+module AresMUSH    
+  module Custom
+    class PotionsCmd
+      include CommandHandler
+      attr_accessor :target_name
+  
+      def parse_args
+         self.target_name = cmd.args ? titlecase_arg(cmd.args) : enactor_name
+      end
+  
+      def handle
+        ClassTargetFinder.with_a_character(self.target_name, client, enactor) do |model|
+           template = PotionsTemplate.new(model)
+           client.emit template.render
+        end
+      end
+    end
+  end
+end
