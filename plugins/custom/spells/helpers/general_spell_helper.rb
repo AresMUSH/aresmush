4eaba7bfedc8903a142ad83af93ad18317d701eb
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-08-14 11:11:52 -0400

    Updating from remote. All working properly.

---
+++ plugins/custom/spells/helpers/general_spell_helper.rb
@@ -0,0 +1,85 @@
+module AresMUSH
+  module Custom
+
+    def self.is_spell?(spell)
+      spell_list = Global.read_config("spells")
+      spell_name = spell.titlecase
+      spell_list.include?(spell_name)
+    end
+
+    def self.already_cast(char)
+      has_cast = char.combatant.has_cast
+      return true if has_cast
+    end
+
+    def self.roll_combat_spell(char, combatant, school)
+      accuracy_mod = FS3Combat.weapon_stat(combatant.weapon, "accuracy")
+      special_mod = combatant.attack_mod
+      damage_mod = combatant.total_damage_mod
+      stance_mod = combatant.attack_stance_mod
+      stress_mod = combatant.stress
+      luck_mod = (combatant.luck == "Attack") ? 3 : 0
+      distraction_mod = combatant.distraction
+      spell_mod = combatant.spell_mod
+
+
+      combatant.log "Attack roll for #{combatant.name} school=#{school} accuracy=#{accuracy_mod} damage=#{damage_mod} stance=#{stance_mod} luck=#{luck_mod} stress=#{stress_mod} special=#{special_mod} distract=#{distraction_mod}"
+
+
+
+      mod = spell_mod.to_i + accuracy_mod.to_i + damage_mod.to_i  + stance_mod.to_i  + luck_mod.to_i  - stress_mod.to_i  + special_mod.to_i  - distraction_mod.to_i
+
+      school_dice = FS3Skills.ability_rating(char, school)
+      dice = mod + school_dice
+
+
+      die_result = FS3Skills.roll_dice(dice)
+      successes = die_result.count { |d| d >= FS3Skills.success_target_number }
+      botches = die_result.count { |d| d == 1 }
+
+      return successes
+
+    end
+
+    def self.combat_spell_success(spell, die_result)
+      spell_level = Global.read_config("spells", spell, "level")
+      if die_result < 1
+        return "%xrFAILS%xn"
+      elsif spell_level == 1
+        return "%xrFAILS%xn" if die_result == 1
+        return "%xgSUCCEEDS%xn"
+      elsif spell_level == 2
+        return "%xrFAILS%xn" if die_result <= 1
+        return "%xgSUCCEEDS%xn"
+      elsif spell_level == 3
+        return "%xrFAILS%xn" if die_result <= 2
+        return "%xgSUCCEEDS%xn"
+      elsif spell_level == 4
+        return "%xrFAILS%xn" if die_result <= 2
+        return "%xgSUCCEEDS%xn"
+      elsif spell_level == 5
+        return "%xrFAILS%xn" if die_result <= 3
+        return "%xgSUCCEEDS%xn"
+      elsif spell_level == 6
+        return "%xrFAILS%xn" if die_result <= 3
+        return "%xgSUCCEEDS%xn"
+      elsif spell_level == 7
+        return "%xrFAILS%xn" if die_result <= 3
+        return "%xgSUCCEEDS%xn"
+      elsif spell_level == 8
+        return "%xrFAILS%xn" if die_result <= 1
+        return "%xgSUCCEEDS%xn"
+      else
+        return "%xgSUCCEEDS%xn"
+      end
+    end
+
+    def self.roll_spell_success(caster, spell)
+      # mod = caster.spell_mod
+      school = Global.read_config("spells", spell, "school")
+      die_result = Custom.roll_combat_spell(caster, caster.combatant, school)
+      succeeds = Custom.combat_spell_success(spell, die_result)
+    end
+
+  end
+end
--- plugins/fs3combat/commands/gear/combat_weapon_cmd.rb
+++ plugins/fs3combat/commands/gear/combat_weapon_cmd.rb
@@ -3,9 +3,9 @@ module AresMUSH
     class CombatWeaponCmd
       include CommandHandler
       include NotAllowedWhileTurnInProgress
-      
+
       attr_accessor :name, :weapon, :specials
-      
+
       def parse_args
         if (cmd.args =~ /=/)
           args = cmd.parse_args( /(?<arg1>[^\=]+)\=(?<arg2>[^\+]+)\+?(?<arg3>.+)?/)
@@ -18,30 +18,31 @@ module AresMUSH
           self.weapon = titlecase_arg(args.arg1)
           specials_str = titlecase_arg(args.arg2)
         end
-        
+
         self.specials = specials_str ? specials_str.split('+') : nil
       end
 
       def required_args
         [ self.name, self.weapon ]
       end
-      
+
       def check_special_allowed
         return nil if !self.specials
-        allowed_specials = FS3Combat.weapon_stat(self.weapon, "allowed_specials") || []
+        special_group = FS3Combat.weapon_stat(weapon, "special_group") || ""
+        allowed_specials = Global.read_config("fs3combat", "weapon special groups", special_group) || []
         self.specials.each do |s|
-          return t('fs3combat.invalid_weapon_special', :special => s) if !allowed_specials.include?(s)
+          return t('fs3combat.invalid_weapon_special') if !allowed_specials.include?(s.downcase)
         end
         return nil
       end
-      
+
       def check_valid_weapon
         return t('fs3combat.invalid_weapon') if !FS3Combat.weapon(self.weapon)
         return nil
       end
-      
+
       def handle
-        FS3Combat.with_a_combatant(name, client, enactor) do |combat, combatant|        
+        FS3Combat.with_a_combatant(name, client, enactor) do |combat, combatant|
           FS3Combat.set_weapon(enactor, combatant, self.weapon, self.specials)
         end
       end
