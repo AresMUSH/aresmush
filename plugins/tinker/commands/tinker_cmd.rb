600543e1e6999a84c31d6e909ee5b2d49fda4998
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-08-15 16:38:48 -0400

    Misc

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -2,23 +2,22 @@ module AresMUSH
   module Tinker
     class TinkerCmd
       include CommandHandler
-
+        attr_accessor :name, :armor, :specials, :caster, :spell, :target
       def check_can_manage
         return t('dispatcher.not_allowed') if !enactor.has_permission?("tinker")
         return nil
       end
 
-      def handle
-        enactor.combatant.update(has_cast: false)
-      end
+
+
 
       def handle
-        school = cmd.args
-        successes = Custom.roll_combat_spell(enactor, enactor.combatant, cmd.args)
-        client.emit successes
-        # client.emit die_result
-        client.emit school
+        spell = "Gust"
+        emit = Global.read_config("spells", spell, "level")
+        client.emit emit
       end
+
+
 
     end
   end
