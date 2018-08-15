d6da43f3a9ef243c9ade68fe99055fb15fdfbc8e
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-08-14 23:39:28 -0400

    All working.

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -2,14 +2,29 @@ module AresMUSH
   module Tinker
     class TinkerCmd
       include CommandHandler
-
+        attr_accessor :name, :armor, :specials
       def check_can_manage
         return t('dispatcher.not_allowed') if !enactor.has_permission?("tinker")
         return nil
       end
 
+      def parse_args
+        if (cmd.args =~ /\//)
+          args = cmd.parse_args(/(?<arg1>[^\/]+)\/(?<arg2>[^\+]+)/)
+          self.name = titlecase_arg(args.arg1)
+          self.armor = titlecase_arg(args.arg2)
+          specials_str = titlecase_arg(args.arg3)
+        else
+          args = cmd.parse_args(/(?<arg1>[^\+]+)\+?(?<arg2>.+)?/)
+          self.name = enactor.name
+          self.armor = titlecase_arg(args.arg1)
+          specials_str = titlecase_arg(args.arg2)
+        end
+      end
+
       def handle
-        Custom.cast_noncombat_spell(enactor, "Create Water")
+        client.emit self.name
+        client.emit self.armor
       end
 
 
