85df6ccb25e2535be58d18146725fde3df34cbab
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-08-24 22:31:12 -0400

    App command is broken
    This is going to be a pain to fix.

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -13,25 +13,11 @@ module AresMUSH
 
 
       def handle
-
-        if (cmd.args =~ /\//)
-          args = cmd.parse_args( /(?<arg1>[^\/]+)\/(?<arg2>[^\=]+)\=?(?<arg3>.+)?/)
-          combat = enactor.combat
-          caster_name = titlecase_arg(args.arg1)
-          self.spell = titlecase_arg(args.arg2)
-          target_name = titlecase_arg(args.arg3)
-          self.caster = combat.find_combatant(caster_name)
-          self.target = combat.find_combatant(target_name)
-        else
-          args = cmd.parse_args(/(?<arg1>[^\=]+)\=?(?<arg2>.+)?/)
-          self.caster = enactor.combatant
-          self.spell = titlecase_arg(args.arg1)
-          target_name = titlecase_arg(args.arg2)
-          self.target = FS3Combat.find_named_thing(target_name, self.caster)
-        end
-        client.emit self.caster.name
-        client.emit self.spell
-        client.emit self.target.name
+        char_name = cmd.args
+        char = Character.find_one_by_name(char_name)
+        client.emit char
+        review = FS3Skills.starting_skills_check(char)        
+        client.emit review
 
 
       end
