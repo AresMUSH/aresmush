7233611d8b72d1856baf95e73467c591f4d08253
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-08-30 17:03:05 -0400

    Non combat healing

--- plugins/custom/spells/helpers/spell_cast_with_target_helper.rb
+++ plugins/custom/spells/helpers/spell_cast_with_target_helper.rb
@@ -30,6 +30,23 @@ module AresMUSH
       end
     end
 
+    def self.cast_non_combat_heal(caster, target, spell)
+      # succeeds = Custom.roll_noncombat_spell_success(caster, spell)
+      succeeds = succeeds
+      # if succeeds == "%xgSUCCEEDS%xn"
+        wound = FS3Combat.worst_treatable_wound(target)
+        heal_points = Global.read_config("spells", spell, "heal_points")
+        if (wound)
+          FS3Combat.heal(wound, heal_points)
+          FS3Combat.emit_to_combat caster.combat, t('custom.cast_heal', :name => caster.name, :spell => spell, :succeeds => succeeds, :target => target.name, :points => heal_points)
+        else
+          FS3Combat.emit_to_combat caster.combat, t('custom.cast_heal_no_effect', :name => caster.name, :spell => spell, :succeeds => succeeds, :target => target.name)
+        end
+      # else
+      #   FS3Combat.emit_to_combat caster.combat, t('custom.casts_spell', :name => caster.name, :spell => spell, :succeeds => succeeds)
+      # end
+    end
+
     def self.cast_multi_heal(caster, target_string, spell)
       succeeds = Custom.roll_combat_spell_success(caster.combatant, spell)
       if succeeds == "%xgSUCCEEDS%xn"
--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -13,12 +13,26 @@ module AresMUSH
 
 
       def handle
+        char = enactor
+        school = "Fire"
+        mod = 0
 
-        target_string = cmd.args
-        caster = enactor
-        spell = "Minor Heal"
-        Custom.cast_multi_heal(caster, target_string, spell )
+        roll = char.roll_ability(school, mod)
+        client.emit roll[:successes]
+        target_name = cmd.args
+        target = FS3Combat.find_named_thing(target_name, enactor)
+      
+
+        Custom.cast_non_combat_heal(enactor, target, "Minor Heal")
+
+
+        # target_string = cmd.args
+        # caster = enactor
+        # spell = "Minor Heal"
+        # Custom.cast_multi_heal(caster, target_string, spell )
       end
+
+
 
 
 
