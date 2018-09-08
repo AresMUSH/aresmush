5e484e4bd01145c4ce7d138bb64e5e3c2858b20b
Linda Naughton <lynn@wordsmyth.org>
2018-09-07 22:05:11 -0400

    Configurable cg tips in misc tab.

--- install/migrations/0010_beta22_update.rb
+++ install/migrations/0010_beta22_update.rb
@@ -18,6 +18,13 @@ module AresMUSH
           c.update(show_titles: true)
         end
         
+        Global.logger.debug "Configurable chargen tips."
+        config = DatabaseMigrator.read_config_file("chargen.yml")
+        config['chargen']['bg_blurb'] = "You don't need to write a novel. Just cover the basics: who you are, why are you here, and is there anything noteworthy about you. Be sure to explain any unusual skills."
+        config['chargen']['hooks_blurb'] = "RP Hooks are interesting things about your character that others can hook into for RP. This could be an odd personality quirk, a reputation your character has, or anything else that might inspire someone to connect with you. Looking around at other character's sheets can be a good way to get inspiration for RP Hooks."
+        config['chargen']['desc_blurb'] = "You can set both your main description, visible when people look at you, and a short description that shows your character at a glance."
+        DatabaseMigrator.write_config_file("chargen.yml", config)   
+                
       end
     end
   end
--- plugins/chargen/web/chargen_info_request_handler.rb
+++ plugins/chargen/web/chargen_info_request_handler.rb
@@ -40,7 +40,10 @@ module AresMUSH
         {
           fs3: fs3,
           group_options: groups,
-          date_format: Global.read_config("datetime", "date_entry_format_help")
+          date_format: Global.read_config("datetime", "date_entry_format_help"),
+          bg_blurb: Website.format_markdown_for_html(Global.read_config("chargen", "bg_blurb")),
+          hooks_blurb: Website.format_markdown_for_html(Global.read_config("chargen", "hooks_blurb")),
+          desc_blurb: Website.format_markdown_for_html(Global.read_config("chargen", "desc_blurb"))
         }
       end
     end
