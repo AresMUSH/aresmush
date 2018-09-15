a6c3dd4192da3f9d2c0929d80965145daf5c6907
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-09-14 22:53:15 -0400

    Stray death helper breaking .dead
    deleted extra death helper. stupid.

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -14,10 +14,10 @@ module AresMUSH
 
       def handle
 
-        name_string = "test1 test2 testing1"
-        targets = Custom.parse_spell_targets(enactor, name_string)
-        client.emit targets
-
+        combatant = enactor.combatant
+        combatant.character.update(dead: true )
+        client.emit combatant.character.dead
+      end
 
 
 
