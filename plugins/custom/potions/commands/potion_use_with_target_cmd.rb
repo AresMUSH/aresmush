51ef12ef33ecb84070bb3bcd8eba02f119c9858b
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-09-16 23:29:12 -0400

    Potions with target
    Largely working, chasing down a few bugs.

---
+++ plugins/custom/potions/commands/potion_use_with_target_cmd.rb
@@ -0,0 +1,174 @@
+module AresMUSH
+  module Custom
+    class PotionUseWithTargetCmd
+    #potion/use <potion>=<target>
+      include CommandHandler
+      attr_accessor :potion, :potion_name, :potion, :roll, :spell, :caster, :target, :caster_combat, :target_combat, :target_name
+
+      def parse_args
+        if (cmd.args =~ /\//)
+          #Forcing NPC or PC to cast
+          args = cmd.parse_args( /(?<arg1>[^\/]+)\/(?<arg2>[^\=]+)\=?(?<arg3>.+)?/)
+          combat = enactor.combat
+          caster_name = titlecase_arg(args.arg1)
+          self.spell = titlecase_arg(args.arg2)
+          self.potion_name = titlecase_arg(args.arg2)
+          self.target_name = titlecase_arg(args.arg3)
+          #Returns char or NPC
+          self.caster = FS3Combat.find_named_thing(caster_name, enactor)
+          self.target = FS3Combat.find_named_thing(target_name, enactor)
+
+          #Returns combatant
+          if enactor.combat
+            self.target_combat = combat.find_combatant(self.target_name)
+            self.caster_combat = combat.find_combatant(caster_name)
+          end
+
+        else
+          #Enactor casts
+          args = cmd.parse_args(/(?<arg1>[^\=]+)\=?(?<arg2>.+)?/)
+          self.potion_name = titlecase_arg(args.arg1)
+          self.spell = titlecase_arg(args.arg1)
+          self.target_name = titlecase_arg(args.arg2)
+
+          #Returns char or NPC
+          self.caster = enactor
+          self.target = FS3Combat.find_named_thing(self.target_name, self.caster)
+
+          #Returns combatant
+          if enactor.combat
+            combat = enactor.combat
+            self.caster_combat = enactor.combatant
+            self.target_combat = combat.find_combatant(target_name)
+          end
+
+        end
+        self.potion = Custom.find_potion_has(enactor, self.potion_name)
+      end
+
+      def check_errors
+        return t('custom.invalid_name') if !self.target
+      end
+
+      def handle
+      #Reading Config
+        require_target = Global.read_config("spells", self.spell, "require_target")
+        weapon = Global.read_config("spells", self.spell, "weapon")
+        weapon_specials = Global.read_config("spells", self.spell, "weapon_specials")
+        armor = Global.read_config("spells", self.spell, "armor")
+        armor_specials = Global.read_config("spells", self.spell, "self.armor_specials")
+        is_stun = Global.read_config("spells", self.spell, "is_stun")
+        roll = Global.read_config("spells", self.spell, "roll")
+        damage_inflicted = Global.read_config("spells", self.spell, "damage_inflicted")
+        school = Global.read_config("spells", self.spell, "school")
+        spell_mod = Global.read_config("spells", self.spell, "spell_mod")
+        heal_points = Global.read_config("spells", self.spell, "heal_points")
+        is_revive = Global.read_config("spells", self.spell, "is_revive")
+        stance = Global.read_config("spells", self.spell, "stance")
+        lethal_mod = Global.read_config("spells", self.spell, "lethal_mod")
+        attack_mod = Global.read_config("spells", self.spell, "attack_mod")
+        defense_mod = Global.read_config("spells", self.spell, "defense_mod")
+        spell_mod = Global.read_config("spells", self.spell, "spell_mod")
+        if self.caster.combat
+          if self.caster_combat.is_ko
+            client.emit_failure t('custom.spell_ko')
+          elsif (!caster_combat.is_npc? &&  !Custom.find_potion_has(caster, self.potion_name))
+            client.emit_failure t('custom.dont_have_potion')
+          else
+
+            #Inflict damage
+            if damage_inflicted
+              Custom.potion_inflict_damage(self.caster_combat, self.target, self.spell)
+            end
+
+            #Healing
+            if heal_points
+              Custom.potion_heal_with_target(self.caster_combat, self.target, self.spell)
+            end
+
+            #Revive
+            if is_revive
+              if (!self.target_combat.is_ko)
+                    client.emit_failure t('custom.not_ko', :target => self.target.name)
+              else
+                Custom.potion_revive(self.caster_combat, self.target_combat, self.spell)
+              end
+            end
+
+            #Set Lethal Mod
+            if lethal_mod
+              Custom.potion_lethal_mod_with_target(self.caster, self.target_combat, self.spell)
+            end
+
+            #Set defense mod
+            if defense_mod
+              Custom.potion_defense_mod_with_target(self.caster, self.target_combat, self.spell)
+            end
+
+            #Set attack mod
+            if attack_mod
+              Custom.potion_attack_mod_with_target(self.caster, self.target_combat, self.spell)
+            end
+
+            #Set spell mod
+            if spell_mod
+              Custom.potion_spell_mod_with_target(self.caster, self.target_combat, self.spell)
+            end
+
+            #Change stance
+            if stance
+              Custom.potion_stance_with_target(self.caster_combat, self.target_combat, self.spell)
+            end
+
+            #Equip Armor
+            if armor
+              Custom.potion_equip_armor_with_target(enactor, self.caster_combat, self.target_combat, self.spell)
+            end
+
+            #Equip Armor Specials
+            if armor_specials
+              FS3Combat.emit_to_combat self.caster.combat, t('custom.use_potion', :name => self.caster.name, :potion => spell)
+                Custom.potion_equip_armor_specials_with_target(enactor, self.caster_combat, self.target_combat, self.spell)
+            end
+
+            #Equip Weapon
+            if weapon
+              Custom.potion_equip_weapon_with_target(enactor, self.caster_combat, self.target_combat, self.spell)
+            end
+
+            #Equip Weapon Specials
+            if weapon_specials
+              FS3Combat.emit_to_combat self.caster.combat, t('custom.use_potion', :name => self.caster.name, :potion => spell)
+              Custom.potion_equip_weapon_specials_with_target(enactor, self.caster_combat, self.target_combat, self.spell)
+            end
+            # For some reason, both of these break setting weapons and armor. I have no idea why.
+
+            self.caster_combat.update(has_cast: true)
+            FS3Combat.set_action(client, self.caster_combat, self.caster.combat, self.caster_combat, FS3Combat::PotionAction, "")
+          end
+
+
+        else
+          if heal_points
+            if Custom.knows_spell?(caster, self.spell)
+              Custom.potion_non_combat_heal_with_target(self.caster, self.target, self.spell)
+            else
+              client.emit_failure t('custom.dont_know_spell')
+            end
+          else
+            client.emit_failure t('custom.not_in_combat')
+          end
+        end
+        self.potion.delete
+
+      end
+
+
+
+
+
+
+
+    end
+  end
+end
---
+++ plugins/custom/potions/helpers/potions_use_with_target_helper.rb
@@ -0,0 +1,123 @@
+module AresMUSH
+  module Custom
+
+    def self.potion_inflict_damage_with_target(caster, target, spell)
+      damage_desc = Global.read_config("spells", spell, "damage_desc")
+      damage_inflicted = Global.read_config("spells", spell, "damage_inflicted")
+      return t('custom.cant_heal_dead') if (target.dead)
+      FS3Combat.inflict_damage(target, damage_inflicted, damage_desc)
+      FS3Combat.emit_to_combat caster.combat, t('custom.potion_damage', :name => caster.name, :potion => spell, :target => target.name, :damage_desc => spell.downcase)
+    end
+
+    def self.potion_heal_with_target(caster, target, spell)
+      wound = FS3Combat.worst_treatable_wound(target)
+      heal_points = Global.read_config("spells", spell, "heal_points")
+      if (wound)
+        FS3Combat.heal(wound, heal_points)
+        FS3Combat.emit_to_combat caster.combat, t('custom.potion_heal', :name => caster.name, :potion => spell, :target => target.name, :points => heal_points)
+      else
+        FS3Combat.emit_to_combat caster.combat, t('custom.potion_heal_no_effect', :name => caster.name, :potion => spell, :target => target.name)
+      end
+    end
+
+    def self.potion_non_combat_heal_with_target(caster, target, spell)
+      client = Login.find_client(caster)
+      wound = FS3Combat.worst_treatable_wound(target)
+      heal_points = Global.read_config("spells", spell, "heal_points")
+      if (wound)
+        FS3Combat.heal(wound, heal_points)
+        client.emit t('custom.potion_heal', :name => caster.name, :potion => spell, :target => target.name, :points => heal_points)
+      else
+        client.emit t('custom.potion_heal_no_effect', :name => caster.name, :potion => spell, :target => target.name)
+      end
+    end
+
+    def self.potion_revive(caster_combat, target_combat, spell)
+      target_combat.update(is_ko: false)
+      FS3Combat.emit_to_combat caster_combat.combat, t('custom.potion_res', :name => caster_combat.name, :potion => spell, :target => target_combat.name)
+      FS3Combat.emit_to_combatant target_combat, t('custom.been_resed', :name => caster_combat.name)
+    end
+
+    # def self.cast_revive(caster, target, target_combat, spell)
+    #   succeeds = Custom.roll_combat_spell_success(caster, spell)
+    #   if succeeds == "%xgSUCCEEDS%xn"
+    #     Custom.undead(target)
+    #     FS3Combat.emit_to_combat caster.combat, t('custom.cast_res', :name => caster.name, :spell => spell, :succeeds => succeeds, :target => target.name)
+    #     FS3Combat.emit_to_combatant target_combat, t('custom.been_resed', :name => caster.name)
+    #   else
+    #     FS3Combat.emit_to_combat caster.combat, t('custom.casts_spell', :name => caster.name, :potion => spell, :succeeds => succeeds)
+    #   end
+    # end
+
+    def self.potion_lethal_mod_with_target(caster, target_combat, spell)
+      lethal_mod = Global.read_config("spells", spell, "lethal_mod")
+      current_mod = target_combat.damage_lethality_mod
+      new_mod = current_mod + lethal_mod
+      target_combat.update(damage_lethality_mod: new_mod)
+      FS3Combat.emit_to_combat caster.combat, t('potion_mod', :name => caster.name, :potion => spell, :target =>  target_combat.name, :mod => lethal_mod, :type => "lethality", :total_mod => target_combat.damage_lethality_mod)
+    end
+
+    def self.potion_defense_mod_with_target(caster,  target_combat, spell)
+      defense_mod = Global.read_config("spells", spell, "defense_mod")
+      current_mod =  target_combat.defense_mod
+      new_mod = current_mod + defense_mod
+      target_combat.update(defense_mod: new_mod)
+      FS3Combat.emit_to_combat caster.combat, t('custom.potion_mod', :name => caster.name, :target => target_combat.name, :potion => spell, :mod => defense_mod, :type => "defense", :total_mod => target_combat.defense_mod)
+    end
+
+    def self.potion_attack_mod_with_target(caster, target_combat, spell)
+      attack_mod = Global.read_config("spells", spell, "attack_mod")
+      current_mod = target_combat.attack_mod
+      new_mod = current_mod + attack_mod
+      target_combat.update(attack_mod: new_mod)
+      FS3Combat.emit_to_combat caster.combat, t('custom.potion_mod', :name => caster.name, :target => target_combat.name, :potion => spell, :mod => attack_mod, :type => "attack", :total_mod => target_combat.attack_mod)
+    end
+
+    def self.potion_spell_mod_with_target(caster, target_combat, spell)
+      spell_mod = Global.read_config("spells", spell, "spell_mod")
+      current_mod = target_combat.spell_mod.to_i
+      new_mod = current_mod + spell_mod
+      target_combat.update(spell_mod: new_mod)
+      FS3Combat.emit_to_combat caster.combat, t('custom.potion_mod', :name => caster.name, :target => target_combat.name, :potion => spell, :mod => spell_mod, :type => "spell", :total_mod => target_combat.spell_mod)
+    end
+
+    def self.potion_stance_with_target(caster, target_combat, spell)
+      stance = Global.read_config("spells", spell, "stance")
+      target_combat.update(stance: stance)
+      FS3Combat.emit_to_combat caster.combat, t('custom.potion_stance', :name => caster.name, :target => target_combat.name, :potion => spell, :stance => stance)
+    end
+
+    def self.potion_equip_weapon_with_target(enactor, caster_combat, target_combat, spell)
+      weapon = Global.read_config("spells", spell, "weapon")
+      armor = Global.read_config("spells", spell, "armor")
+      if armor
+
+      else
+        FS3Combat.emit_to_combat caster_combat.combat, t('custom.use_potion', :name => caster_combat.name, :potion => spell)
+      end
+      FS3Combat.set_weapon(enactor, target_combat, weapon)
+
+    end
+
+    def self.potion_equip_weapon_specials_with_target(enactor, caster_combat, target_combat, spell)
+      weapon_specials_str = Global.read_config("spells", spell, "weapon_specials")
+      weapon_specials = weapon_specials_str ? weapon_specials_str.split('+') : nil
+      FS3Combat.set_weapon(enactor, target_combat, target_combat.weapon, weapon_specials)
+
+    end
+
+    def self.potion_equip_armor_with_target(enactor, caster_combat, target_combat, spell)
+      FS3Combat.emit_to_combat caster_combat.combat, t('custom.use_potion', :name => caster_combat.name, :potion => spell)
+      armor = Global.read_config("spells", spell, "armor")
+      FS3Combat.set_armor(enactor, target_combat, armor)
+    end
+
+
+    def self.potion_equip_armor_specials_with_target(enactor, caster_combat, target_combat, spell)
+      armor_specials_str = Global.read_config("spells", spell, "armor_specials")
+      armor_specials = armor_specials_str ? armor_specials_str.split('+') : nil
+      FS3Combat.set_armor(enactor, target_combat, target_combat.armor, armor_specials)
+    end
+
+  end
+end
