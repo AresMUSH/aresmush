a5f4a292d9b185fc0fa7c74e2105e39b1c2d9fff
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-09-18 16:41:51 -0400

    Adjusting spell success levels

--- plugins/custom/spells/helpers/general_spell_helper.rb
+++ plugins/custom/spells/helpers/general_spell_helper.rb
@@ -53,25 +53,22 @@ module AresMUSH
 
     def self.combat_spell_success(spell, die_result)
       spell_level = Global.read_config("spells", spell, "level")
-      if die_result < 1
-        return "%xrFAILS%xn"
-      elsif spell_level == 1
-        return "%xrFAILS%xn" if die_result == 0
+      if spell_level == 1
         return "%xgSUCCEEDS%xn"
       elsif spell_level == 2
-        return "%xrFAILS%xn" if die_result <= 1
+        return "%xrFAILS%xn" if die_result < 1
         return "%xgSUCCEEDS%xn"
       elsif spell_level == 3
         return "%xrFAILS%xn" if die_result <= 1
         return "%xgSUCCEEDS%xn"
       elsif spell_level == 4
-        return "%xrFAILS%xn" if die_result <= 2
+        return "%xrFAILS%xn" if die_result <= 1
         return "%xgSUCCEEDS%xn"
       elsif spell_level == 5
         return "%xrFAILS%xn" if die_result <= 2
         return "%xgSUCCEEDS%xn"
       elsif spell_level == 6
-        return "%xrFAILS%xn" if die_result <= 3
+        return "%xrFAILS%xn" if die_result <= 2
         return "%xgSUCCEEDS%xn"
       elsif spell_level == 7
         return "%xrFAILS%xn" if die_result <= 3
