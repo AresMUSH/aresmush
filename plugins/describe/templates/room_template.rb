11b57e15fd80f63ec7ea81ee9ac5f5142ce5d546
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-09-27 15:12:20 -0400

    Room exits to ( )
    fixing for spawn reasons

--- plugins/describe/templates/room_template.rb
+++ plugins/describe/templates/room_template.rb
@@ -2,19 +2,19 @@ module AresMUSH
   module Describe
     # Template for a room.
     class RoomTemplate < ErbTemplateRenderer
-             
+
       attr_accessor :room
-                     
+
       def initialize(room, enactor)
         @room = room
         @enactor = enactor
-        super File.dirname(__FILE__) + "/room.erb"        
+        super File.dirname(__FILE__) + "/room.erb"
       end
-      
+
       def is_foyer
         @room.is_foyer?
       end
-      
+
       # List of all exits in the room.
       def exits
         if (@room.is_foyer?)
@@ -23,57 +23,57 @@ module AresMUSH
           @room.exits.sort_by(:name, :order => "ALPHA")
         end
       end
-      
-      # List of online characters, sorted by name.      
+
+      # List of online characters, sorted by name.
       def online_chars
         @room.characters.select { |c| Login.is_online?(c) }.sort_by { |c| c.name }
       end
-      
+
       # Available detail views.
       def details
         @room.details.empty? ? nil : @room.details.keys.sort.join(", ")
       end
-      
+
       def logging_enabled
         @room.logging_enabled?
       end
-      
+
       # Short IC date/time string
       def ic_time
         ICTime.ic_datestr ICTime.ictime
       end
-      
+
       def area
         @room.area_name
       end
-      
+
       # Room grid coordinates, e.g. (1,2)
       def grid
         "(#{@room.grid_x},#{@room.grid_y})"
       end
-      
+
       def web_watchers
         return nil if !@room.scene || @room.scene.watchers.empty?
         @room.scene.watchers.map { |c| c.name }.join(", ")
       end
-      
+
       def description
         Describe::RoomDescBuilder.build(@room)
       end
-      
+
       def ooc_time
         OOCTime.local_short_timestr(@enactor, Time.now)
       end
-      
+
       def foyer_exits
         @room.exits.select { |e| e.name.is_integer? }.sort_by { |e| e.name }
       end
-      
+
       def non_foyer_exits
         @room.exits.select { |e| !e.name.is_integer? }.sort_by { |e| e.name }
       end
-      
-     
+
+
       def foyer_status(e, i)
         chars = e.dest.characters
         if (!e.lock_keys.empty?)
@@ -89,22 +89,22 @@ module AresMUSH
         room_name = "#{e.dest.name} (#{status})"
         "#{linebreak}%xh[#{e.name}]%xn #{left(room_name,29)}"
       end
-      
+
       def char_shortdesc(char)
         char.shortdesc ? " - #{char.shortdesc}" : ""
       end
-      
+
       def char_tag(char)
         colors = Global.read_config("describe", "tag_colors") || {}
         return "#{colors['admin']}[#{t('describe.admin_tag')}]%xn" if char.is_admin?
         return "#{colors['beginner']}[#{t('describe.beginner_tag')}]%xn" if char.is_beginner?
         return nil
       end
-      
+
       def char_name(char)
         Demographics.name_and_nickname(char)
       end
-      
+
       # Shows the AFK message, if the player has set one, or the automatic AFK warning,
       # if the character has been idle for a really long time.
       def char_afk(char)
@@ -126,11 +126,11 @@ module AresMUSH
           ""
         end
       end
-      
+
       def exit_name(e)
-        "[#{e.name}]"
+        "(#{e.name})"
       end
-      
+
       def exit_destination(e)
         locked =  e.allow_passage?(@enactor) ? "" : "%xr*#{t('describe.locked')}*%xn "
         if (e.dest)
@@ -140,7 +140,7 @@ module AresMUSH
         end
         "#{locked}#{name}"
       end
-      
+
       def exit_linebreak(i)
         i % 2 == 0 ? "%r" : ""
       end
