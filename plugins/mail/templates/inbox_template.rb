11fd4f84df356828c5528166d63bf34b7696a23a
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-10-01 20:37:16 -0400

    Mail [ to (
    Saving our spawns one [ at a time.

--- plugins/mail/templates/inbox_template.rb
+++ plugins/mail/templates/inbox_template.rb
@@ -2,9 +2,9 @@ module AresMUSH
   module Mail
     # Template for an individual mail message
     class InboxTemplate < ErbTemplateRenderer
-      
+
       attr_accessor :tag, :messages
-      
+
       def initialize(enactor, messages, show_from, tag)
         @enactor = enactor
         @messages = messages
@@ -12,52 +12,52 @@ module AresMUSH
         @tag = tag
         super File.dirname(__FILE__) + "/inbox.erb"
       end
-      
+
       def filters_enabled
         @tag != Mail.inbox_tag
       end
-        
+
       def tag_title
         if (@tag.start_with?("review"))
           return t('mail.sent_review', :name => @tag.after(" "))
         end
         @tag
       end
-      
+
       def mail_header
-        @show_from ? t('mail.inbox_title') : t('mail.sent_title') 
+        @show_from ? t('mail.inbox_title') : t('mail.sent_title')
       end
-      
+
       def is_sent_mail
         @tag == Mail.sent_tag
       end
-      
+
       def date(msg)
         msg.created_date_str(@enactor)
       end
-      
+
       # Message sent to or sent from, depending on the inbox mode.
       def to_or_from(msg)
         @show_from ? author(msg) : msg.to_list
       end
-      
+
       def author(msg)
         msg.author_name
       end
-      
+
       # Message sent to.  Note that this is just the individual recipient of THIS delivery,
       # not a list of all people who received the message.
       def sent_to(msg)
         !msg.character ? t('mail.deleted_recipient') : msg.character.name
       end
-      
+
       # Message tags, like unread or marked for deletion
       def tags(msg)
         unread = msg.read ? "-" : t('mail.unread_marker')
         trashed = msg.tags.include?(Mail.trashed_tag) ? t('mail.trashed_marker') : "-"
-        " [#{unread}#{trashed}]  "
+        " (#{unread}#{trashed})  "
       end
-     
+
     end
   end
 end
