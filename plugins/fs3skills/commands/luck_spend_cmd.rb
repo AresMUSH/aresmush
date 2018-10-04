56644878d13d6d86084469f7088dfd7589089c4c
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-10-04 16:45:21 -0400

    Spending and awarding luck creates job

--- plugins/fs3skills/commands/luck_spend_cmd.rb
+++ plugins/fs3skills/commands/luck_spend_cmd.rb
@@ -2,7 +2,7 @@ module AresMUSH
   module FS3Skills
     class LuckSpendCmd
       include CommandHandler
-      
+
       attr_accessor :reason
 
       def parse_args
@@ -12,18 +12,22 @@ module AresMUSH
       def required_args
         [ self.reason ]
       end
-      
+
       def checK_luck
         return t('fs3skills.not_enough_points') if enactor.luck < 1
         return nil
       end
-      
+
       def handle
-        
+
         enactor.spend_luck(1)
         enactor_room.emit_ooc t('fs3skills.luck_point_spent', :name => enactor_name, :reason => reason)
         Achievements.award_achievement(enactor, "fs3_luck_spent", 'fs3', "Spent a luck point.")
-          
+
+        job_message = t('custom.spent_luck', :name => enactor.name, :reason => self.reason)
+        category = Global.read_config("jobs", "luck_category")
+        Jobs.create_job(category, t('custom.spent_luck_title', :name => enactor.name), job_message, Game.master.system_character)
+
         Global.logger.info "#{enactor_name} spent luck on #{reason}."
       end
     end
