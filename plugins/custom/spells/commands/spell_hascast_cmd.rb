75ab19199626effbece4d72e176e72d062e2a63c
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-10-06 22:25:42 -0400

    NPCs reset hascast

--- plugins/custom/spells/commands/spell_hascast_cmd.rb
+++ plugins/custom/spells/commands/spell_hascast_cmd.rb
@@ -3,38 +3,13 @@ module AresMUSH
     class SpellHascastCmd
     #spell/hascast <name>=<true/false>
       include CommandHandler
-      attr_accessor :caster_combat, :hascast
+      attr_accessor :target, :hascast
 
       def parse_args
         args = cmd.parse_args(ArgParser.arg1_equals_arg2)
-        if (cmd.args =~ /\//)
-          #Forcing NPC or PC to cast
-          args = cmd.parse_args( /(?<arg1>[^\/]+)\/(?<arg2>[^\=]+)\=?(?<arg3>.+)?/)
-          combat = enactor.combat
-          caster_name = titlecase_arg(args.arg1)
-          self.hascast = titlecase_arg(args.arg2)
-
-          #Returns combatant
-          if enactor.combat
-            self.caster_combat = combat.find_combatant(caster_name)
-          else
-            client.emit_failure t('FS3.not_in_combat')
-          end
-
-        else
-          #Enactor casts
-          args = cmd.parse_args(/(?<arg1>[^\=]+)\=?(?<arg2>.+)?/)
-          self.hascast = titlecase_arg(args.arg1)
-
-          #Returns combatant
-          if enactor.combat
-            combat = enactor.combat
-            self.caster_combat = enactor.combatant
-          else
-            client.emit_failure t('FS3.not_in_combat')
-          end
-
-        end
+        combat = enactor.combat
+        self.target = combat.find_combatant(args.arg1)
+        self.hascast = trim_arg(args.arg2)
       end
 
       def handle
@@ -43,8 +18,8 @@ module AresMUSH
         elsif self.hascast == "false"
           hascast = false
         end
-        self.caster_combat.update(has_cast: hascast)
-        client.emit_success "#{caster_combat.name}'s has_cast has been set to #{caster_combat.has_cast}."
+        self.target.update(has_cast: hascast)
+        client.emit_success "#{target.name}'s has_cast has been set to #{target.has_cast}."
       end
     end
   end
