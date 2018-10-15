56c6c30958a2565b614ba7797c51d5f1878fdb8d
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-10-15 12:16:00 -0400

    Setting up magical item collections.

---
+++ plugins/custom/magic-items/magic-items_char.rb
@@ -0,0 +1,19 @@
+module AresMUSH
+  class Character < Ohm::Model
+    collection :magic_item, "AresMUSH::PotionsHas"
+    attribute :magic_item_equipped, :string, default: "None"
+
+  end
+end
+
+  class MagicItem < Ohm::Model
+    include ObjectModel
+    attribute :name
+    index :name
+    reference :character, "AresMUSH::Character"
+    attribute :description
+    attribute :spell
+    attribute :weapon_specials
+    attribute :armor_specials
+
+  end
