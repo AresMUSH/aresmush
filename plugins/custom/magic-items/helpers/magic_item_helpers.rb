1adf533074c886c716f1a28671b59beba03d3956
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-10-15 15:57:57 -0400

    Equip items

--- plugins/custom/magic-items/helpers/magic_item_helpers.rb
+++ plugins/custom/magic-items/helpers/magic_item_helpers.rb
@@ -6,8 +6,8 @@ module AresMUSH
       is_item = Global.read_config("magic-items", item_name)
     end
 
-    def self.find_item (char, item)
-      char.magic_items.select { |a| a.name == item }.first
+    def self.find_item (char, item_name)
+      char.magic_items.select { |a| a.name == item_name }.first
     end
 
   end
