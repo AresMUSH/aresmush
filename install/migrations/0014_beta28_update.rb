7ee9ebccaee54c42cb060da5a2931439dc14b4b2
Linda Naughton <lynn@wordsmyth.org>
2018-10-20 01:39:12 -0400

    Restrict connections and creation.

--- install/migrations/0014_beta28_update.rb
+++ install/migrations/0014_beta28_update.rb
@@ -21,12 +21,20 @@ module AresMUSH
         config = DatabaseMigrator.read_config_file("server.yml")
         config['server']['private_key_file_path'] = ''
         config['server']['certificate_file_path'] = ''
-        config['server']['use_ssl'] = false
+        config['server']['use_https'] = false
         config['server']['bind_address'] = ''
         DatabaseMigrator.write_config_file("server.yml", config)  
         
           
-        
+        Global.logger.debug "Login permissions."
+        config = DatabaseMigrator.read_config_file("login.yml")
+        config['login']['allow_creation'] = true
+        config['login']['login_not_allowed_message'] = ''
+        config['login']['creation_not_allowed_message'] = ''
+        config['login']['shortcuts']['pcreate'] = 'create/reserve'
+        DatabaseMigrator.write_config_file("login.yml", config)  
+        everyone_role = Role.find_one_by_name("everyone")
+        everyone_role.add_permission "login"
       end
     end
   end
---
+++ plugins/login/commands/reserve_cmd.rb
@@ -0,0 +1,31 @@
+module AresMUSH
+  module Login
+    class ReserveCmd
+      include CommandHandler
+      
+      attr_accessor :charname
+      
+      def parse_args
+        self.charname = cmd.args ? titlecase_arg(cmd.args.before(" ")) : nil
+      end
+      
+      def check_name
+        return t('dispatcher.invalid_syntax', :cmd => 'create') if !charname
+        return Character.check_name(charname)
+      end
+      
+      def check_can_reserve
+        return t('dispatcher.not_allowed')  if !Login.can_manage_login?(enactor)
+        return nil
+      end
+      
+      def handle
+        char = Character.create(name: charname, room: Game.master.welcome_room)
+        temp_password = Login.set_random_password(char)
+        
+        client.emit_success(t('login.char_reserved', :name => charname, :password => temp_password))
+        Global.dispatcher.queue_event CharCreatedEvent.new(nil, char.id)
+      end
+    end
+  end
+end
