5b55c0641aab7f33a7b1b7fd212fd3de5d541c4d
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-10-22 09:11:18 -0400

    Fix invalid special message

--- plugins/fs3combat/commands/gear/combat_weapon_cmd.rb
+++ plugins/fs3combat/commands/gear/combat_weapon_cmd.rb
@@ -33,7 +33,7 @@ module AresMUSH
         special_group = FS3Combat.weapon_stat(weapon, "special_group") || ""
         allowed_specials = Global.read_config("fs3combat", "weapon special groups", special_group) || []
         self.specials.each do |s|
-          return t('fs3combat.invalid_weapon_special') if !allowed_specials.include?(s.downcase)
+          return t('fs3combat.invalid_weapon_special', :special => s) if !allowed_specials.include?(s.downcase)
         end
         return nil
       end
