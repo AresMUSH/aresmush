e9bcf7fd5ddb6d286d1ab4d29dfc14d6080a8569
Linda Naughton <lynn@wordsmyth.org>
2018-10-25 18:51:44 -0400

    Better error if wiki include vars missing.

--- plugins/website/wiki_markdown/include.rb
+++ plugins/website/wiki_markdown/include.rb
@@ -9,12 +9,11 @@ module AresMUSH
         input = matches[1]
         return "" if !input
 
-        error_message = "<div class=\"alert alert-danger\">There was a problem including #{input}.  Make sure the page exists and all required variables are set.</div>"
+        page_name = input.before("\n").strip.downcase
         
         begin          
           vars = {}
           
-          page_name = input.before("\n").strip.downcase
           split_vars = (input.after("\n") || "").split("|")
           split_vars.each do |v|
             var_name = v.before('=')
@@ -32,7 +31,8 @@ module AresMUSH
           page.current_version.text % vars
 
         rescue Exception => ex
-          Global.logger.debug "Error loading include #{input} : #{ex}"
+          Global.logger.debug "Error loading include #{page_name} : #{ex}"
+          error_message = "<div class=\"alert alert-danger\">There was a problem including #{page_name}.  Make sure the page exists and all required variables are set.  Error: #{ex.message}</div>"
           return error_message
         end
       end
