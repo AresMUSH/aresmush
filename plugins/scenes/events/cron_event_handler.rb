a91c8f191c0fb8e2aa4969ffd532f6ae5c4c76ce
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-10-25 23:52:40 -0400

    Debugging scenes

--- plugins/scenes/events/cron_event_handler.rb
+++ plugins/scenes/events/cron_event_handler.rb
@@ -7,7 +7,7 @@ module AresMUSH
           clear_rooms
           clear_watchers
         end
-        
+
         if (Global.read_config("scenes", "delete_unshared_scenes"))
           config = Global.read_config("scenes", "unshared_scene_cleanup_cron")
           if Cron.is_cron_match?(config, event.time)
@@ -19,19 +19,23 @@ module AresMUSH
       def clear_watchers
         Scene.all.each { |s| s.watchers.replace([]) }
       end
-      
+
       def clear_rooms
         rooms = Room.all.select { |r| !!r.scene_set || !!r.scene || !r.scene_nag}
+        active_scenes = Scene.all.select { |s| !s.completed }.map { |s| s.id }
+        Global.logger.debug "Room cleanup cron running: #{active_scenes}."
+
+
         rooms.each do |r|
           if (r.clients.empty?)
             if (r.scene_set)
               r.update(scene_set: nil)
             end
-            
+
             if (!r.scene_nag)
               r.update(scene_nag: true)
             end
-            
+
             if (should_stop_empty_scene(r))
               Global.logger.debug("Stopping scene in #{r.name}")
               Scenes.stop_scene(r.scene)
@@ -39,15 +43,15 @@ module AresMUSH
           end
         end
       end
-      
+
       def delete_unshared_scenes
         # Completed scenes that haven't been shared are deleted after a few days.
 
         warn_days = Global.read_config('scenes', 'unshared_scene_warning_days')
         delete_days = Global.read_config('scenes', 'unshared_scene_deletion_days')
-        
+
         Scene.all.select { |s| s.completed && !s.shared }.each do |scene|
-          
+
           elapsed_days = (Time.now - scene.date_completed) / 86400
           if (elapsed_days > delete_days  && scene.deletion_warned)
             Global.logger.info "Deleting scene #{scene.id} - #{scene.title} completed #{scene.date_completed}"
@@ -59,12 +63,12 @@ module AresMUSH
           end
         end
       end
-      
+
       def should_stop_empty_scene(room)
         scene = room.scene
         return false if !scene
         return true if !scene.temp_room
-        
+
         last_activity = scene.last_activity || Time.now
         idle_timeout = Global.read_config("scenes", "idle_scene_timeout_days")
         elapsed_days = (Time.now - last_activity) / 86400
