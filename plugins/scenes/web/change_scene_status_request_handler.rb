fc4bab9e437f2322c6d4373f9d788ad1b9522203
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-10-25 23:08:49 -0400

    Debugging scene stopping for faraday

--- plugins/scenes/web/change_scene_status_request_handler.rb
+++ plugins/scenes/web/change_scene_status_request_handler.rb
@@ -5,55 +5,57 @@ module AresMUSH
         scene = Scene[request.args[:id]]
         status = request.args[:status]
         enactor = request.enactor
-        
+
         if (!scene)
           return { error: t('webportal.not_found') }
         end
+
+        Global.logger.debug "Scene change: #{request}"
         
         error = Website.check_login(request)
         return error if error
-        
+
         if (!Scenes.can_access_scene?(enactor, scene))
           return { error: t('scenes.access_not_allowed') }
         end
-        
+
         case status
         when "restart"
           if (!scene.completed)
             return { error: t('scenes.cant_restart_in_progress_scene') }
           end
-          
+
           if (scene.shared)
             return { error: t('scenes.cant_restart_shared_scene') }
           end
-          
+
           Scenes.restart_scene(scene)
-          
+
         when "stop"
           if (scene.completed)
             return { error: t('scenes.scene_already_completed')}
           end
-          
+
           Scenes.stop_scene(scene)
-          
+
         when "share"
           if (!scene.all_info_set?)
             return { error: t('scenes.missing_required_fields')}
           end
-          
+
           Scenes.share_scene(scene)
-          
+
         when "unshare"
           if (!scene.shared)
             return { error: t('scenes.scene_not_shared')}
           end
-          
+
           Scenes.unshare_scene(enactor, scene)
-          
+
         else
           return { error: t('webportal.unexpected_error') }
         end
-        
+
         {}
       end
     end
