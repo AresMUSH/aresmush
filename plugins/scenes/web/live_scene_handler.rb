d5ba88b53577258a593350fd351952ba0c16a022
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-10-27 15:28:45 -0400

    Debugging scenes

--- plugins/scenes/web/live_scene_handler.rb
+++ plugins/scenes/web/live_scene_handler.rb
@@ -4,44 +4,44 @@ module AresMUSH
       def handle(request)
         scene = Scene[request.args[:id]]
         enactor = request.enactor
-        
+
         if (!scene)
           return { error: t('webportal.not_found') }
         end
-        
+
         error = Website.check_login(request, true)
         return error if error
-        
+
         if (!Scenes.can_read_scene?(enactor, scene))
           return { error: t('scenes.scene_is_private') }
         end
-        
+
         if (scene.shared)
           return { error: t('scenes.scene_already_shared') }
         end
-        
+
         if (!scene.logging_enabled)
           return { error: t('scenes.cant_join_unlogged_scene')}
         end
-        
+
         if (enactor)
           scene.mark_read(enactor)
-            
-          if (!Scenes.is_watching?(scene, enactor))
-            scene.watchers.add enactor
-          end
+
+          # if (!Scenes.is_watching?(scene, enactor))
+          #   scene.watchers.add enactor
+          # end
 
         end
-        
+
         participants = Scenes.participants_and_room_chars(scene)
             .sort_by {|p| p.name }
-            .map { |p| { 
-              name: p.name, 
-              id: p.id, 
-              icon: Website.icon_for_char(p), 
+            .map { |p| {
+              name: p.name,
+              id: p.id,
+              icon: Website.icon_for_char(p),
               is_ooc: p.is_admin? || p.is_playerbit?,
               online: Login.is_online?(p)  }}
-            
+
         {
           id: scene.id,
           title: scene.title,
@@ -57,9 +57,9 @@ module AresMUSH
           participants: participants,
           scene_type: scene.scene_type ? scene.scene_type.titlecase : 'unknown',
           can_edit: enactor && Scenes.can_access_scene?(enactor, scene),
-          poses: scene.poses_in_order.map { |p| { 
-            char: { name: p.character.name, icon: Website.icon_for_char(p.character) }, 
-            order: p.order, 
+          poses: scene.poses_in_order.map { |p| {
+            char: { name: p.character.name, icon: Website.icon_for_char(p.character) },
+            order: p.order,
             id: p.id,
             is_setpose: p.is_setpose,
             is_system_pose: p.is_system_pose?,
