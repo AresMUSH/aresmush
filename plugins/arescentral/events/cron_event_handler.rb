93a8b87fdfa4061b3ba7f35d004069ad91e8629d
Linda Naughton <lynn@wordsmyth.org>
2018-10-27 21:59:04 -0400

    Event dispatching without spawns.  Better cron handler logging.

--- plugins/arescentral/events/cron_event_handler.rb
+++ plugins/arescentral/events/cron_event_handler.rb
@@ -5,8 +5,9 @@ module AresMUSH
         config = Global.read_config("arescentral", "directory_update_cron")
         return if !Cron.is_cron_match?(config, event.time)
         
-        AresMUSH.with_error_handling(nil, "Updating game info with AresCentral.") do
-        
+        Global.logger.debug "Updating game info with AresCentral."
+
+        Global.dispatcher.spawn("Updating game info with AresCentral", client) do        
           if (AresCentral.is_registered?)
             AresCentral.update_game
           elsif (AresCentral.is_public_game?)
