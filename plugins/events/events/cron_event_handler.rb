b7cfd8bf33a21d0108f9752c32f9cf49f4090ffd
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-10-27 20:09:24 -0400

    Update plugins/events/events/cron_event_handler.rb

--- plugins/events/events/cron_event_handler.rb
+++ plugins/events/events/cron_event_handler.rb
@@ -1,28 +1,29 @@
 module AresMUSH
-  module Events    
+  module Events
     class CronEventHandler
       def on_event(event)
         config = Global.read_config("events", "event_alert_cron")
         return if !Cron.is_cron_match?(config, event.time)
-        
+        Global.logger.debug "Starting event cron."
         Event.all.each do |e|
           time_until_event = e.time_until_event
           if (!e.reminded &&  time_until_event < (60 * 20) && time_until_event > 0)
-            
+
             message = t('events.event_starting_soon', :title => e.title,
                :starts => e.start_time_standard)
-               
+
             Global.notifier.notify_ooc(:event_starting, message) do |char|
               true
             end
             e.update(reminded: true)
           end
-          
+
           if (e.time_until_event < 0)
             e.delete
           end
         end
+        Global.logger.debug "Ending event cron."
       end
-    end    
+    end
   end
 end
