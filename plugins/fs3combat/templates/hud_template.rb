9abba4f9b20b59534db4c5e6afc0277020e95d83
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-10-30 16:24:09 -0400

    Adding armor to HUD

--- plugins/fs3combat/templates/hud_template.rb
+++ plugins/fs3combat/templates/hud_template.rb
@@ -4,39 +4,39 @@ module AresMUSH
 
 
       attr_accessor :combat
-      
+
       def initialize(combat)
         @combat = combat
         super File.dirname(__FILE__) + "/hud.erb"
       end
-           
+
       def organizer
         t('fs3combat.combat_hud_organizer', :org => @combat.organizer.name)
       end
-      
+
       def title
         scene = @combat.scene ? @combat.scene.id : t('global.none')
         t('fs3combat.combat_hud_header', :num => @combat.id, :scene => scene)
       end
-       
+
       def teams
         @combat.active_combatants.sort_by{|c| c.team}.group_by {|c| c.team}
       end
-      
-      
+
+
       def format_action(c)
         action = c.action ? c.action.print_action_short : "----"
         "#{action} #{format_stance(c)}"
       end
-      
+
       def format_stance(c)
         case c.stance
         when "Normal"
           text = ""
-        else          
+        else
           text = "(#{c.stance[0,3].upcase})"
         end
-        
+
         if (c.is_in_vehicle?)
           if (c.piloting)
             text << "(plt)"
@@ -47,22 +47,22 @@ module AresMUSH
         if (c.mount_type)
           text << "(mnt)"
         end
-        
+
         text
       end
-      
+
       def pilot(v)
         v.pilot ? v.pilot.name : t('fs3combat.no_pilot')
       end
-      
+
       def passengers(v)
         v.passengers.map { |p| p.name }.join(",")
       end
-      
+
       def non_combatants
         @combat.non_combatants.map { |c| c.name}.join(", ")
       end
-      
+
       def format_vehicle(c)
         if (c.piloting)
           text = t('fs3combat.piloting_hud', :vehicle => c.piloting.name)
@@ -73,22 +73,27 @@ module AresMUSH
         end
         text
       end
-      
+
       def format_damage(c)
         return "%xh%xr#{t('fs3combat.ko_status')}%xn" if c.is_ko
         FS3Combat.print_damage(c.total_damage_mod)
       end
-      
+
       def format_weapon(c)
         weapon = "#{c.weapon}"
-        
+
         if (c.max_ammo > 0)
           notes = " (#{c.ammo})"
         else
           notes = ""
         end
-        
+
         "#{weapon}#{notes}"
+      end
+
+
+      def format_armor(c)
+        armor = "#{c.armor}"
       end
     end
   end
