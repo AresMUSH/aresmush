c3f2edd9a6610652565e142daa3319f6229c1a4c
Linda Naughton <lynn@wordsmyth.org>
2018-11-10 15:16:04 -0500

    Combat log reversed.

--- plugins/fs3combat/commands/status/combat_log_cmd.rb
+++ plugins/fs3combat/commands/status/combat_log_cmd.rb
@@ -21,7 +21,7 @@ module AresMUSH
         end
         
         if (combat.debug_log)
-          list = combat.debug_log.combat_log_messages.sort_by(:timestamp).map { |l| "#{l.created_at} #{l.message}"}
+          list = combat.debug_log.combat_log_messages.sort_by(:timestamp).map { |l| "#{l.created_at} #{l.message}"}.reverse
         else
           list = []
         end
