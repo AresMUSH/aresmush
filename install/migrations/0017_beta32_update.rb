207e334d44de28e8517f82b490f410951f444259
Linda Naughton <lynn@wordsmyth.org>
2018-11-14 09:24:21 -0500

    Configurable sidebar.

---
+++ install/migrations/0017_beta32_update.rb
@@ -0,0 +1,21 @@
+module AresMUSH  
+  module Migrations
+    class MigrationBeta32Update
+      def require_restart
+        false
+      end
+      
+      def migrate
+        Global.logger.debug "Adding default top nav."
+
+        default_config = DatabaseMigrator.read_distr_config_file("website.yml")
+
+        config = DatabaseMigrator.read_config_file("website.yml")
+        config['website'].delete 'wiki_nav'
+        config['website']['top_navbar'] = default_config['website']['top_navbar']
+        DatabaseMigrator.write_config_file("website.yml", config)    
+         
+      end
+    end
+  end
+end
