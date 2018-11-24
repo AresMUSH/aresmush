8d7c8e7b96d467e6de5dd60fd215ec892cdedcbf
Linda Naughton <lynn@wordsmyth.org>
2018-11-24 03:33:18 -0500

    Fix default upload folder when nicknames enabled.

--- plugins/profile/web/character_request_handler.rb
+++ plugins/profile/web/character_request_handler.rb
@@ -104,7 +104,8 @@ module AresMUSH
         
         {
           id: char.id,
-          name: Demographics.name_and_nickname(char),
+          name: char.name,
+          name_and_nickname: Demographics.name_and_nickname(char),
           all_fields: all_fields,
           fullname: char.demographic(:fullname),
           military_name: Ranks.military_name(char),
