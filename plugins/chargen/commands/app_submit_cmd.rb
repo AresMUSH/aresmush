26d5bf2ffe24ec5ba252fdd26811c132ed94910f
Linda Naughton <lynn@wordsmyth.org>
2018-11-29 08:46:00 -0500

    Allow notes from in-game app submission.

--- plugins/chargen/commands/app_submit_cmd.rb
+++ plugins/chargen/commands/app_submit_cmd.rb
@@ -3,6 +3,12 @@ module AresMUSH
     class AppSubmitCmd
       include CommandHandler
       
+      attr_accessor :app_notes
+      
+      def parse_args
+        self.app_notes = cmd.args
+      end
+      
       def check_approval
         return t('chargen.you_are_already_approved') if enactor.is_approved?
         return nil
@@ -10,9 +16,10 @@ module AresMUSH
       
       def handle
         if (cmd.switch_is?("confirm"))
-          client.emit_success Chargen.submit_app(enactor)
+          client.emit_success Chargen.submit_app(enactor, client.program[:app_notes])
           client.emit_ooc t('chargen.approval_reminder')
         else
+          client.program[:app_notes] = self.app_notes
           client.emit_ooc t('chargen.app_confirm')
         end
       end
