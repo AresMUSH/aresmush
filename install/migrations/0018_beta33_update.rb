9a38d2cfbbcada87451fab9fc7a1861d4a972832
Linda Naughton <lynn@wordsmyth.org>
2018-12-01 03:13:20 -0500

    Version, config, migration bump.

--- install/migrations/0018_beta33_update.rb
+++ install/migrations/0018_beta33_update.rb
@@ -6,26 +6,30 @@ module AresMUSH
       end
       
       def migrate
-        Global.logger.debug "Adding pm shortcut."
-
+        Global.logger.debug "Adding pm shortcut and page markers."
         config = DatabaseMigrator.read_config_file("page.yml")
         config['page']['shortcuts']['pm'] = 'page'
         config['page']['page_start_marker'] = '<'
         config['page']['page_end_marker'] = '>'
         DatabaseMigrator.write_config_file("page.yml", config)    
 
+        Global.logger.debug "Adding glance format."
+        default_config = DatabaseMigrator.read_distr_config_file("describe.yml")
+        config = DatabaseMigrator.read_config_file("describe.yml")
+        config['describe']['glance_format'] = default_config['describe']['glance_format']
+        DatabaseMigrator.write_config_file("describe.yml", config)    
+
+        Global.logger.debug "Adding job category shortcut."
         config = DatabaseMigrator.read_config_file("jobs.yml")
         config['jobs']['shortcuts']['job/category'] = 'job/cat'
         DatabaseMigrator.write_config_file("jobs.yml", config)    
          
+        Global.logger.debug "Adding whisper shortcuts."
         config = DatabaseMigrator.read_config_file("scenes.yml")
         config['scenes']['shortcuts']['whisp'] = 'whisper'
         config['scenes']['shortcuts']['mutter'] = 'whisper'
         config['scenes']['shortcuts'].delete 'whisper'        
         DatabaseMigrator.write_config_file("scenes.yml", config)    
-         
-         
-        
       end
     end
   end
