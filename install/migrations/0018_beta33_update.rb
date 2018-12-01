a0d873ea3499d749c7739ac29b71ae74abacf6d4
Linda Naughton <lynn@wordsmyth.org>
2018-12-01 03:09:18 -0500

    Configurable glance, migrations and misc.

--- install/migrations/0018_beta33_update.rb
+++ install/migrations/0018_beta33_update.rb
@@ -10,11 +10,19 @@ module AresMUSH
 
         config = DatabaseMigrator.read_config_file("page.yml")
         config['page']['shortcuts']['pm'] = 'page'
+        config['page']['page_start_marker'] = '<'
+        config['page']['page_end_marker'] = '>'
         DatabaseMigrator.write_config_file("page.yml", config)    
 
         config = DatabaseMigrator.read_config_file("jobs.yml")
         config['jobs']['shortcuts']['job/category'] = 'job/cat'
         DatabaseMigrator.write_config_file("jobs.yml", config)    
+         
+        config = DatabaseMigrator.read_config_file("scenes.yml")
+        config['scenes']['shortcuts']['whisp'] = 'whisper'
+        config['scenes']['shortcuts']['mutter'] = 'whisper'
+        config['scenes']['shortcuts'].delete 'whisper'        
+        DatabaseMigrator.write_config_file("scenes.yml", config)    
          
          
         
