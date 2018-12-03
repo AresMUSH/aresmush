aec8c7bdd9b6caa38026305b145e05bce2865f54
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-12-03 11:01:50 -0500

    Adding build/request

---
+++ plugins/custom/build_request_cmd.rb
@@ -0,0 +1,23 @@
+module AresMUSH
+  module Custom
+    class BuildRequestCmd
+    # build/request <room name>=<text>
+      include CommandHandler
+
+      attr_accessor :room, :details
+
+      def parse_args
+        args = cmd.parse_args(ArgParser.arg1_equals_arg2)
+        self.room = trim_arg(args.arg1)
+        self.details = trim_arg(args.arg2)
+      end
+
+      def handle
+        client.emit_success t('custom.build_requested')
+        subj = "Build #{room} for #{enactor.name}"
+        Jobs.create_job("BUILD", subj, details, enactor)
+      end
+
+    end
+  end
+end
