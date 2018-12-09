699765a9568017f4c0bb8672f171cb0c5e1ae735
Linda Naughton <lynn@wordsmyth.org>
2018-12-08 21:15:01 -0500

    Player bit on other players.

--- plugins/status/commands/playerbit_cmd.rb
+++ plugins/status/commands/playerbit_cmd.rb
@@ -3,23 +3,38 @@ module AresMUSH
     class PlayerBitCmd
       include CommandHandler
       
-      attr_accessor :option
+      attr_accessor :option, :name
       
       def parse_args
-        self.option = OnOffOption.new(cmd.args)
+        if (cmd.args =~ /\=/)
+          args = cmd.parse_args(ArgParser.arg1_equals_arg2)
+          self.name = titlecase_arg(args.arg1)
+          self.option = OnOffOption.new(args.arg2)
+        else
+          self.name = enactor_name
+          self.option = OnOffOption.new(cmd.args)
+        end
       end
       
       def required_args
-        [ self.option ]
+        [ self.option, self.name ]
       end
       
       def check_status
         return self.option.validate
       end
       
+      def check_can_manage_status
+        return nil if self.name == enactor_name
+        return t('dispatcher.not_allowed') if !Status.can_manage_status?(enactor)
+        return nil
+      end
+      
       def handle        
-        enactor.update(is_playerbit: self.option.is_on?)
-        client.emit_ooc self.option.is_on? ? t('status.set_playerbit_on') : t('status.set_playerbit_off')
+        ClassTargetFinder.with_a_character(self.name, client, enactor) do |model|
+          model.update(is_playerbit: self.option.is_on?)
+          client.emit_ooc self.option.is_on? ? t('status.set_playerbit_on') : t('status.set_playerbit_off')
+        end
       end
     end
   end
