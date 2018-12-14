3bca79bde6f8c604c5af9ac06194115263b7c888
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-12-14 12:17:29 -0500

    Adding half seasons

--- plugins/ictime/public/ictime_api.rb
+++ plugins/ictime/public/ictime_api.rb
@@ -4,33 +4,33 @@ module AresMUSH
       t = DateTime.now
       ICTime.convert_to_ictime(t)
     end
-    
+
     # Used in certain places where you don't want to display the full date and time
     # and just want a shorter version - typically just month/day/year
     def self.ic_datestr(time)
       ICTime.ic_short_timestr(time)
     end
-        
+
     def self.ic_short_timestr(time)
       format = Global.read_config("datetime", "short_date_format")
       format = substitute_ic_names(format, time)
       l(time, format: format)
     end
-  
+
     def self.ic_long_timestr(time)
       format = Global.read_config("datetime", "long_date_format")
       format = substitute_ic_names(format, time)
       l(time, format: format)
     end
-    
+
     def self.is_night?(area)
       ICTime.time_of_day(area) == "night"
     end
-    
+
     def self.is_day?(area)
       ICTime.time_of_day(area) != "night"
     end
-    
+
     def self.time_of_day(area)
       # Area is not currently used; all areas assumed to be on same day/night cycle.
       # You can change this with custom code.
@@ -46,15 +46,19 @@ module AresMUSH
         "night"
       end
     end
-    
+
     def self.season(area)
       time = ICTime.ictime
       day_hash = time.month * 100 + time.mday
       case day_hash
-        when 101..401 then 'winter'
-        when 402..630 then 'spring'
-        when 701..930 then 'summer'
-        when 1001..1231 then 'fall'
+        when 101..315 then 'winter'
+        when 316..415 then 'early-spring'
+        when 416..531 then 'spring'
+        when 601..630 then 'early-summer'
+        when 701..907 then 'summer'
+        when 908..1015 then 'early-fall'
+        when 1016..1120 then 'fall'
+        when 1121..1231 then 'early-winter'
       end
     end
   end
