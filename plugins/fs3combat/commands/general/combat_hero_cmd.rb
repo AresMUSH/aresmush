b38bb78cd0364b7e86e622fa2063b178331769cc
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-12-18 16:46:54 -0500

    combat/hereo resets death counter

--- plugins/fs3combat/commands/general/combat_hero_cmd.rb
+++ plugins/fs3combat/commands/general/combat_hero_cmd.rb
@@ -3,30 +3,31 @@ module AresMUSH
     class CombatHeroCmd
       include CommandHandler
       include NotAllowedWhileTurnInProgress
-      
-      
+
+
       def check_points
         return t('fs3combat.no_luck') if enactor.luck < 1
         return nil
       end
-    
-      
+
+
       def handle
         FS3Combat.with_a_combatant(enactor_name, client, enactor) do |combat, combatant|
           if (!combatant.is_ko)
             client.emit_failure t('fs3combat.not_koed')
             return
           end
-          
+
           enactor.spend_luck(1)
           Achievements.award_achievement(enactor, "fs3_hero", 'fs3', "Heroed from a knockout.")
-          
+
           combatant.update(is_ko: false)
+          combatant.update(death_count: 0  )
           wound = FS3Combat.worst_treatable_wound(enactor)
           if (wound)
             FS3Combat.heal(wound, 1)
           end
-          
+
           FS3Combat.emit_to_combat combat, t('fs3combat.back_in_the_fight', :name => enactor_name), nil, true
         end
       end
