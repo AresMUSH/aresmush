87b9104cc46ae6ce684f14ee79779a51836895c9
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-01-10 14:30:59 -0500

    Muting achievements for a bit.

--- plugins/achievements/public/achievement_api.rb
+++ plugins/achievements/public/achievement_api.rb
@@ -1,13 +1,13 @@
 module AresMUSH
   module Achievements
-    
+
     def self.is_enabled?
       !Global.plugin_manager.is_disabled?("achievements")
     end
-    
+
     def self.award_achievement(char, name, type = nil, message = nil)
       return if char.is_admin? || char.is_npc? || char.is_guest?
-      
+
       achievement_data = Achievements.custom_achievement_data(name)
       if (achievement_data)
         if (!type)
@@ -17,20 +17,20 @@ module AresMUSH
           message = achievement_data['message']
         end
       end
-      
+
       if (!type || !message)
         raise "Invalid achievement details.  Missing name or message."
       end
-      
+
       if (Achievements.is_enabled? && !Achievements.has_achievement?(char, name))
         Achievement.create(character: char, type: type, name: name, message: message)
         notification = t('achievements.achievement_earned', :name => char.name, :message => message)
-        Global.notifier.notify_ooc(:achievement, notification) do |char|
-          true
-        end
+        # Global.notifier.notify_ooc(:achievement, notification) do |char|
+        #   true
+        # end
       end
     end
-    
+
     def self.achievements_list(char)
       data = {}
 
@@ -41,7 +41,7 @@ module AresMUSH
       if (char.is_playerbit?)
         alts = AresCentral.alts_of(char)
         alts = alts.concat Character.all.select { |c| c.profile_tags.include?("player:#{char.name}".downcase)}
-        
+
         alts.each do |c|
           c.achievements.each do |a|
             if (data[a.name])
@@ -55,12 +55,12 @@ module AresMUSH
       end
       data.sort_by { |name, data| data[:type] }
     end
-   
+
     def self.build_achievements(player)
-      return {} if !Achievements.is_enabled? 
-      
+      return {} if !Achievements.is_enabled?
+
       icon_types = Global.read_config('achievements', 'types')
-      
+
       Achievements.achievements_list(player).map { |name, data| {
         name: name,
         type: data[:type],
