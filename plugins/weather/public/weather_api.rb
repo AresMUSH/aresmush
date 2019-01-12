bf839a8ac0c436b651f4376eeae4e147dc4b24d7
Linda Naughton <lynn@wordsmyth.org>
2019-01-12 02:18:19 -0500

    Fixing weather lookup for parent areas.

--- plugins/weather/public/weather_api.rb
+++ plugins/weather/public/weather_api.rb
@@ -4,17 +4,25 @@ module AresMUSH
       !Global.plugin_manager.is_disabled?("weather")
     end
     
-    def self.weather_for_area(area)
+    def self.weather_for_area(area_name)
       Weather.load_weather_if_needed
       
       # Get the weather for the current area if there is one.
-      weather = Weather.current_weather[area] || Weather.current_weather["default"]
-
+      if (Weather.current_weather.has_key?(area_name))        
+        weather = Weather.current_weather[area_name]
+      else
+        area = Area.find_one_by_name(area_name)
+        if (area && area.parent)
+          return Weather.weather_for_area(area.parent.name)
+        end
+        weather = Weather.current_weather["Default"]
+      end
+      
       # This handles the 'no weather' case, returning nil.
       return nil if !weather || weather.empty?
 
-      season = ICTime.season(area)
-      time_of_day = ICTime.time_of_day(area)
+      season = ICTime.season(area_name)
+      time_of_day = ICTime.time_of_day(area_name)
       temperature = weather[:temperature]
       condition = weather[:condition]
       
