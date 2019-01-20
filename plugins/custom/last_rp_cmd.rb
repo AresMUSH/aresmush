005917fe5fe6ee5d3e1f42da3843054472501e7e
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-01-20 16:03:35 -0500

    Update last_rp_cmd.rb

--- plugins/custom/last_rp_cmd.rb
+++ plugins/custom/last_rp_cmd.rb
@@ -18,8 +18,9 @@ module AresMUSH
         chars.each do |c|
           char_name = c.name
           last_scene = Character.find_one_by_name(char_name).scenes_starring.sort_by { |s| s.created_at }.reverse[0]
+          scene_count = Character.find_one_by_name(char_name).scenes_starring.count
           if last_scene
-            client.emit "#{last_scene.created_at} - #{char_name} - #{last_scene.title}" 
+            client.emit "#{last_scene.created_at} - #{char_name} - #{last_scene.title}. #{scene_count} scenes total." 
           else
             client.emit "#{char_name}: %xrNONE%xn"
           end
