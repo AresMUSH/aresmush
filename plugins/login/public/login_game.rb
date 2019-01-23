de54d56a4b14a1e025e23b4610efec0bb699842d
Linda Naughton <lynn@wordsmyth.org>
2019-01-22 19:58:08 -0500

    Login MOTD and disabled guests.

--- plugins/login/public/login_game.rb
+++ plugins/login/public/login_game.rb
@@ -2,5 +2,6 @@ module AresMUSH
   class Game
     attribute :login_activity, :type => DataType::Hash, :default => {}
     attribute :login_activity_samples, :type => DataType::Integer
+    attribute :login_motd
   end
 end
