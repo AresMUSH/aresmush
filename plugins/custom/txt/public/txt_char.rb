249cf99e08d440b0553bb832d46211c476bdbdff
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-01-26 07:29:00 -0500

    deleting extra txt

--- plugins/custom/txt/public/txt_char.rb
+++
@@ -1,6 +0,0 @@
-module AresMUSH
-    class Character
-        attribute :txt_last, :type => DataType::Array, :default => []
-        attribute :txt_color
-    end
-  end
--- plugins/custom/txt/public/txt_last_char.rb
+++
@@ -1,5 +0,0 @@
-module AresMUSH
-    class Character
-        attribute :txt_last, :type => DataType::Array, :default => []
-    end
-  end
