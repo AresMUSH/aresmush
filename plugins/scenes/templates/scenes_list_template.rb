ef16ae5f7bcc8ef0400dbc1a644e421b282a2d72
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-01-31 20:57:41 -0500

    Watchable scenes in 'scenes' cmd

--- plugins/scenes/templates/scenes_list_template.rb
+++ plugins/scenes/templates/scenes_list_template.rb
@@ -1,47 +1,55 @@
 module AresMUSH
   module Scenes
     class SceneListTemplate < ErbTemplateRenderer
-             
+
       attr_accessor :scenes, :enactor
-                     
+
       def initialize(scenes, enactor)
         @scenes = scenes
         @enactor = enactor
-        super File.dirname(__FILE__) + "/scenes_list.erb"        
+        super File.dirname(__FILE__) + "/scenes_list.erb"
       end
-      
+
       def characters(scene)
         scene.participants.select { |p| !p.who_hidden }
            .sort_by { |p| p.name }
            .map { |p| p.room == scene.room ? p.name : "%xh%xx#{p.name}%xn"}
            .join(", ")
-        
+
       end
-      
+
       def title(scene)
         "##{scene.id} <#{privacy(scene)}> - #{scene.title || scene.location}"
       end
-      
+
       def organizer(scene)
         "#{t('scenes.organizer_title', :name => scene.owner_name )}"
       end
-      
+
       def location(scene)
         "#{scene.location} (#{location_type(scene)})"
       end
-      
+
       def last_activity(scene)
         return "-" if !scene.last_activity
         TimeFormatter.format(Time.now - scene.last_activity)
       end
-      
+
       def location_type(scene)
         scene.temp_room ? t('scenes.temproom_scene') : t('scenes.grid_scene')
       end
-      
+
       def privacy(scene)
-        color = scene.private_scene ? "%xr" : "%xg"
-        message = scene.private_scene ? t('scenes.private') : t('scenes.open')
+        if scene.private_scene
+          message = t('scenes.private')
+          color = "%xr"
+        elseif scene.watchable_scene
+          message = "Watchable"
+          color = "%xy"
+        else
+          message = t('scenes.open')
+          color = "%xg"
+        end
         "#{color}#{message}%xn"
       end
     end
