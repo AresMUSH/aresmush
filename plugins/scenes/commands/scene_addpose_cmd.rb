64df11d55ea06134eacbb32b0ceb115dd4ce3b87
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-02-02 15:09:26 -0500

    Update scene_addpose_cmd.rb

--- plugins/scenes/commands/scene_addpose_cmd.rb
+++ plugins/scenes/commands/scene_addpose_cmd.rb
@@ -2,14 +2,14 @@ module AresMUSH
   module Scenes
     class SceneAddPoseCmd
       include CommandHandler
-      
+
       attr_accessor :pose
-      
+
       attr_accessor :all, :scene_num
-      
+
       def parse_args
         if (cmd.args =~ /=/)
-          args = cmd.parse_args(ArgParser.arg1_equals_arg2)          
+          args = cmd.parse_args(ArgParser.arg1_equals_arg2)
           self.scene_num = integer_arg(args.arg1)
           self.pose = trim_arg(args.arg2)
         else
@@ -17,20 +17,20 @@ module AresMUSH
           self.pose = trim_arg(cmd.args)
         end
       end
-      
+
       def log_command
         # Don't log poses
       end
-      
+
       def handle
         Scenes.with_a_scene(self.scene_num, client) do |scene|
-          if (!Scenes.can_read_scene?(enactor, scene))
+          if (!Scenes.can_join_scene?(enactor, scene))
             client.emit_failure t('dispatcher.not_allowed')
             return
           end
-          
+
           Scenes.add_to_scene(scene, self.pose, enactor)
-          client.emit_success t('scenes.pose_added', :pose => self.pose )          
+          client.emit_success t('scenes.pose_added', :pose => self.pose )
         end
       end
     end
