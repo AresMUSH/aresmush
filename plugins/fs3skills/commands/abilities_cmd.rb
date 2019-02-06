c5c7f65027f6a5225e65b9ac35393daa64967e32
Linda Naughton <lynn@wordsmyth.org>
2019-02-06 08:37:40 -0500

    Sort ability names.

--- plugins/fs3skills/commands/abilities_cmd.rb
+++ plugins/fs3skills/commands/abilities_cmd.rb
@@ -14,17 +14,17 @@ module AresMUSH
               { attrs: FS3Skills.attrs, num_pages: num_pages, page: cmd.page })
         when 2
           template = AbilityPageTemplate.new("/action_skills.erb", 
-              { skills: FS3Skills.action_skills, num_pages: num_pages, page: cmd.page })
+              { skills: FS3Skills.action_skills.sort_by { |a| a['name'] }, num_pages: num_pages, page: cmd.page })
         when 3
           template = AbilityPageTemplate.new("/background_skills.erb", 
               { skills: FS3Skills.background_skills, num_pages: num_pages, page: cmd.page } )
         when 4
           template = AbilityPageTemplate.new("/languages.erb", 
-              { skills: FS3Skills.languages, num_pages: num_pages, page: cmd.page })
+              { skills: FS3Skills.languages.sort_by { |a| a['name'] }, num_pages: num_pages, page: cmd.page })
         when 5 
           if (FS3Skills.use_advantages?)
             template = AbilityPageTemplate.new("/advantages.erb",
-               { advantages: FS3Skills.advantages, num_pages: num_pages, page: cmd.page })
+               { advantages: FS3Skills.advantages.sort_by { |a| a['name'] }, num_pages: num_pages, page: cmd.page })
            else
              client.emit_failure t('pages.not_that_many_pages')
              return
