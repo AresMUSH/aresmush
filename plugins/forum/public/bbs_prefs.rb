5623397befaa0c95618790f5e63baa622f567ff1
Linda Naughton <lynn@wordsmyth.org>
2019-02-09 00:29:53 -0500

    Unread forum efficiency.

--- plugins/forum/public/bbs_prefs.rb
+++ plugins/forum/public/bbs_prefs.rb
@@ -1,20 +1,4 @@
 module AresMUSH  
-  class Character 
-    attribute :forum_muted, :type => DataType::Boolean
-    collection :bbs_prefs, "AresMUSH::BbsPrefs"
-    
-    before_delete :delete_bbs_prefs
-    
-    def is_forum_muted?
-      self.forum_muted
-    end
-    
-    def delete_bbs_prefs
-      self.bbs_prefs.each { |p| p.destroy }
-    end
-    
-  end
-  
   class BbsPrefs < Ohm::Model
     include ObjectModel
             
