8e29ddae8ab4db56498bfb6ca8b35a1cf9412974
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-02-13 12:13:15 -0500

    Comps template

---
+++ plugins/custom/comps/templates/comps_template.rb
@@ -0,0 +1,26 @@
+module AresMUSH
+  module Custom
+    class CompsTemplate < ErbTemplateRenderer
+      attr_accessor :char, :paginator
+
+      def initialize(char, paginator)
+         @char = char
+         @paginator = paginator
+      end
+
+      # def initialize(char)
+      #   @char = char
+      #   super File.dirname(__FILE__) + "/comps.erb"
+      # end
+
+      def comps_recieved
+        @char.comps.to_a
+      end
+
+      def comps_list
+        self.comps_recieved.sort_by { |s| s.date }
+      end
+
+    end
+  end
+end
