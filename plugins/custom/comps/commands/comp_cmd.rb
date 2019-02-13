18efd382925d3ae8041153cc2574da84268e3942
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-02-13 12:12:50 -0500

    Give a comp

--- plugins/custom/comps/commands/comp_cmd.rb
+++ plugins/custom/comps/commands/comp_cmd.rb
@@ -3,7 +3,7 @@ module AresMUSH
     class CompGiveCmd
       #comp <name>=<text>
       include CommandHandler
-      attr_accessor
+      attr_accessor :target, :comp
 
       def parse_args
        args = cmd.parse_args(ArgParser.arg1_equals_arg2)
@@ -11,10 +11,22 @@ module AresMUSH
        self.comp = args.arg2
       end
 
+      def check_errors
+        return t('custom.invalid_name') if !self.target
+        return nil
+      end
+
       def handle
-        Comps.create(date: Time.now, character: enactor, comp: self.comp)
-        client.emit_success t('custom.left_comp', :name => self.target.name)
-        Cookies.give_cookie(self.target, enactor.client, enactor)
+        date = Time.now.strftime("%Y-%m-%d")
+        client.emit date
+
+        Comps.create(date: date, character: self.target, comp_msg: self.comp, from: enactor.name)
+
+        client.emit_success t('custom.left_comp', :from => enactor.name)
+        message = t('custom.has_left_comp', :from => enactor.name)
+        Login.emit_if_logged_in self.target, message
+
+        # Cookies.give_cookie(self.target, client, enactor)
       end
 
     end
