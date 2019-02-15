8c618d5414b6c25bc018a4e5d986addd2dd6da75
Linda Naughton <lynn@wordsmyth.org>
2019-02-14 23:27:13 -0500

    Idle warn sends message.  Remove many chars at once.

--- install/migrations/0025_beta42_update.rb
+++ install/migrations/0025_beta42_update.rb
@@ -11,6 +11,12 @@ module AresMUSH
         Character.all.each do |c|
           c.update(forum_read_posts: c.forum_read_posts.map { |p| p.to_s }.uniq)
         end
+        
+        Global.logger.debug "Adding idle warn message."
+        config = DatabaseMigrator.read_config_file("idle.yml")
+        config['idle']['idle_warn_msg'] = "You haven't played in awhile and your character is in danger of idling out."
+        DatabaseMigrator.write_config_file("idle.yml", config)  
+        
 
       end
     end
