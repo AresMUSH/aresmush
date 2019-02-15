461ff46978d27745f59e720b5388bee5ed5d153f
Linda Naughton <lynn@wordsmyth.org>
2019-02-14 23:22:46 -0500

    Error checking erb template set.

--- engine/aresmush/templates/erb_template_renderer.rb
+++ engine/aresmush/templates/erb_template_renderer.rb
@@ -10,10 +10,16 @@ module AresMUSH
     end
       		      
     def render
+      if (!@template)
+        raise "Template file not found."
+      end
       @template.evaluate(self)
     end		
     
     def render_local(locals)
+      if (!@template)
+        raise "Template file not found."
+      end
       @template.evaluate(locals)
     end    
   end
