527397ee8924bc801038938fcdcb5e58c47b4bda
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-02-27 11:35:57 -0500

    Fixing comps merge issues

--- plugins/custom/comps/comps_char.rb
+++ plugins/custom/comps/comps_char.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module AresMUSH
 
   class Character < Ohm::Model
@@ -16,22 +15,3 @@ class Comps < Ohm::Model
   attribute :from
 
 end
-=======
-module AresMUSH
-
-  class Character < Ohm::Model
-    collection :comps, "AresMUSH::Comps"
-  end
-end
-
-
-class Comps < Ohm::Model
-  include ObjectModel
-
-  index :character
-  reference :character, "AresMUSH::Character"
-  attribute :comp_msg
-  attribute :from
-
-end
->>>>>>> 46fa0d75499f1ee394365d6780cfea5e50e1af96
