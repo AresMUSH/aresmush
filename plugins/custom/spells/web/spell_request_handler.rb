9328db4c51d80f247dedea3bdbe0a9fd63d92408
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-03-05 17:12:47 -0500

    Spell cards on portal

--- plugins/custom/spells/web/spell_request_handler.rb
+++ plugins/custom/spells/web/spell_request_handler.rb
@@ -1,32 +1,46 @@
 module AresMUSH
   module Custom
-    class SpellRequestHandler
+    class SpellsRequestHandler
       def handle(request)
         spell_list = Global.read_config("spells")
+        school = request.args['school'].titlecase || ""
+        spell_list = spell_list.select { |name, data|  data['school'] == 'Air' }
+        spells = build_list(spell_list)
 
         {
-
-          desc: Global.read_config("spells", spell, "desc"),
-          available: Global.read_config("spells", spell, "available"),
-          level: Global.read_config("spells", spell, "level"),
-          school: Global.read_config("spells", spell, "school"),
-          potion: Global.read_config("spells", spell, "is_potion"),
-          duration: Global.read_config("spells", spell, "duration"),
-          casting_time: Global.read_config("spells", spell, "casting_time"),
-          range: Global.read_config("spells", spell, "range"),
-          los: Global.read_config("spells", spell, "line_of_sight"),
-          area: Global.read_config("spells", spell, "area"),
-          targets:  Global.read_config("spells", spell, "target_num"),
-          heal: Global.read_config("spells", spell, "heal_points"),
-          defense_mod: Global.read_config("spells", spell, "defense_mod"),
-          attack_mod:  Global.read_config("spells", spell, "attack_mod"),
-          lethal_mod: Global.read_config("spells", spell, "attack_mod"),
-          spell_mod: Global.read_config("spells", spell, "spell_mod"),
-          weapon: Global.read_config("spells", spell, "weapon"),
-          armor: Global.read_config("spells", spell, "armor")
-
+          spells: spells.group_by { |s| s['level'] }
         }
 
+      end
+
+
+      def build_list(hash)
+        hash.sort.map { |name, data| {
+          key: name,
+          name: name.titleize,
+          desc: data['desc'],
+          available: data['available'],
+          level: data['level'],
+          school: data['school'],
+          potion: data['is_potion'],
+          duration: data['duration'],
+          casting_time: data['casting_time'],
+          range: data['range'],
+          area: data['area'],
+          los: data['line_of_sight'],
+          targets: data['target_num'],
+          heal: data['heal_points'],
+          defense_mod: data['defense_mod'],
+          attack_mod: data['attack_mod'],
+          lethal_mod: data['lethal_mod'],
+          spell_mod: data['spell_mod'],
+          weapon: data['weapon'],
+          armor: data['armor'],
+          armor_specials: data['armor_specials'],
+          weapon_specials: data['weapon_specials']
+
+          }
+        }
       end
 
     end
