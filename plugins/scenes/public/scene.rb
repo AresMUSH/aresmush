0e06dfd613ace85c3f896de1bf125ec979aa78eb
Linda Naughton <lynn@wordsmyth.org>
2019-03-21 16:58:51 -0400

    Use poses sorted for replace and undo.

--- plugins/scenes/public/scene.rb
+++ plugins/scenes/public/scene.rb
@@ -46,7 +46,7 @@ module AresMUSH
     end
     
     def last_posed
-      last_pose = self.scene_poses.to_a[-1]
+      last_pose = self.poses_in_order.to_a[-1]
       last_pose ? last_pose.character : nil
     end
     
