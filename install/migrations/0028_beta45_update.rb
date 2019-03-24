f6c323cfd9cb8e31572664c629d51ed78e612aa4
Linda Naughton <lynn@wordsmyth.org>
2019-03-24 12:58:35 -0400

    Pose order improvements.

--- install/migrations/0028_beta45_update.rb
+++ install/migrations/0028_beta45_update.rb
@@ -11,6 +11,12 @@ module AresMUSH
         config['mail']['end_marker'] = ']'
         config['mail']['start_marker'] = '['
         DatabaseMigrator.write_config_file("mail.yml", config)  
+        
+        Global.logger.debug "Adding scene skip shortcut."
+        config = DatabaseMigrator.read_config_file("scenes.yml")
+        config['scenes']['shortcuts']['pose/skip'] = 'pose/drop'
+        DatabaseMigrator.write_config_file("scenes.yml", config)  
+        
       end
     end
   end
