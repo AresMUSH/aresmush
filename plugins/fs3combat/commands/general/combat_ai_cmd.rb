756f69456a75412ff0e785f85c9dd4e327557f76
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-03-28 10:39:47 -0400

    Update combat_ai_cmd.rb

--- plugins/fs3combat/commands/general/combat_ai_cmd.rb
+++ plugins/fs3combat/commands/general/combat_ai_cmd.rb
@@ -3,22 +3,22 @@ module AresMUSH
     class CombatAiCmd
       include CommandHandler
       include NotAllowedWhileTurnInProgress
-      
+
       attr_accessor :force, :names
-      
+
       def parse_args
        self.force = (cmd.args && cmd.args.downcase == "force")
        self.names = (!self.force && cmd.args) ? cmd.args.split(" ").map { |n| titlecase_arg(n) } : nil
      end
-     
+
       def check_in_combat
         return t('fs3combat.you_are_not_in_combat') if !enactor.is_in_combat?
         return nil
       end
-      
+
       def handle
         combat = enactor.combat
-        
+
         if (combat.organizer != enactor)
           client.emit_failure t('fs3combat.only_organizer_can_do')
           return
@@ -28,31 +28,31 @@ module AresMUSH
 
         if (self.force)
           npcs = combat.active_combatants.select { |c| c.is_npc? }
-          
+
         elsif (self.names)
           npcs = []
           self.names.each do |raw_name|
             name = raw_name.before(":")
             target = raw_name.after(":")
-            
+
             combatant = combat.find_combatant(name)
-      
+
             if (!combatant)
               client.emit_failure t('fs3combat.not_in_combat', :name => name)
               next
             end
-            
+
             if (!target.blank?)
               FS3Combat.set_action(client, enactor, combat, combatant, FS3Combat::AttackAction, target)
             else
               npcs << combatant
             end
           end
-          
+
         else
-          npcs = combat.active_combatants.select { |c| c.is_npc? && !c.action }
+          npcs = combat.active_combatants.select { |c| c.is_npc? && !c.action && !c.is_ko }
         end
-                
+
         npcs.each_with_index do |c, i|
           FS3Combat.ai_action(combat, client, c, enactor)
         end
