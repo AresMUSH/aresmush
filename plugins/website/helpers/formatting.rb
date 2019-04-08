ff289bce0b0394f14e0dc27f008ec687d93a6453
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-04-08 09:24:21 -0400

    Update formatting.rb

--- plugins/website/helpers/formatting.rb
+++ plugins/website/helpers/formatting.rb
@@ -2,7 +2,7 @@ module AresMUSH
   module Website
     def self.format_markdown_for_html(output)
       return nil if !output
-        
+
       text = format_output_for_html(output)
       text = text.gsub(ANSI.reset, " #{ANSI.reset}")
       allow_html = Global.read_config('website', 'allow_html_in_markdown')
@@ -10,25 +10,25 @@ module AresMUSH
       text = html_formatter.to_html text
       text
     end
-      
+
     def self.format_output_for_html(output)
       return nil if !output
-        
-      AresMUSH::MushFormatter.format output, true
+
+      AresMUSH::MushFormatter.format output
     end
-      
+
     # Takes something from a text box and replaces carriage returns with %r's for MUSH.
     def self.format_input_for_mush(input)
       return nil if !input
       input.gsub(/\r\n/, '%r').gsub(/\n/, '%r')
     end
 
-    # Takes MUSH text and formats it for a text box with %r's becoming line breaks.      
+    # Takes MUSH text and formats it for a text box with %r's becoming line breaks.
     def self.format_input_for_html(input)
       return nil if !input
       input.gsub(/%r/i, "\n")
     end
-    
+
     def self.icon_for_char(char)
       if (char)
         icon = char.profile_icon
@@ -38,10 +38,10 @@ module AresMUSH
       else
         icon = nil
       end
-        
+
       icon.blank? ? nil : icon
     end
-    
+
     def self.icon_for_name(name)
       char = Character.find_one_by_name(name)
       Website.icon_for_char(char)
