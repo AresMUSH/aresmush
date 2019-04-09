79bfbc5d783b04e80a66e66e6f9004e61d0e5029
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-04-09 12:10:10 -0400

    Plot preferences

--- plugins/profile/web/profile_edit_request_handler.rb
+++ plugins/profile/web/profile_edit_request_handler.rb
@@ -4,46 +4,46 @@ module AresMUSH
       def handle(request)
         enactor = request.enactor
         char = Character[request.args[:id]]
-        
+
         if (!enactor)
           return { error: t('webportal.login_required') }
         end
-        
+
         error = Website.check_login(request)
         return error if error
-        
+
         if (!char)
           return { error: t('webportal.not_found') }
         end
-        
+
         if (!Profile.can_manage_char_profile?(enactor, char))
           return { error: t('dispatcher.not_allowed') }
         end
-        
+
         if (!char.is_approved?)
           return { error: t('profile.not_yet_approved') }
         end
-        
+
         demographics = {}
         profile = {}
         relationships = {}
-        
-        Global.read_config('demographics')['editable_properties'].sort.each do |d| 
-          demographics[d.downcase] = 
+
+        Global.read_config('demographics')['editable_properties'].sort.each do |d|
+          demographics[d.downcase] =
             {
               name: d.titlecase,
               value: char.demographic(d)
             }
         end
-                  
-        profile = char.profile.sort.each_with_index.map { |(section, data), index| 
+
+        profile = char.profile.sort.each_with_index.map { |(section, data), index|
           {
             name: section.titlecase,
             text: Website.format_input_for_html(data),
             key: index
           }
         }
-        
+
         relationships = char.relationships.sort.each_with_index.map { |(name, data), index| {
           name: name,
           category: data['category'],
@@ -51,10 +51,10 @@ module AresMUSH
           order: data['order'],
           text: Website.format_input_for_html(data['relationship'])
         }}
-        
+
         files = Profile.character_page_files(char)
         files = files.sort.map { |f| Website.get_file_info(f) }
-        
+
         {
           id: char.id,
           name: char.name,
@@ -62,21 +62,20 @@ module AresMUSH
           demographics: demographics,
           background: Website.format_input_for_html(char.background),
           rp_hooks: Website.format_input_for_html(char.rp_hooks),
+          plot_prefs: Website.format_input_for_html(char.plot_prefs),
           desc: Website.format_input_for_html(char.description),
           shortdesc: char.shortdesc ? char.shortdesc : '',
           relationships: relationships,
           profile: profile,
           gallery: (char.profile_gallery || {}).map { |f| Website.get_file_info(f) },
           tags: char.profile_tags,
-          files: files, 
+          files: files,
           profile_image: char.profile_image ? Website.get_file_info(char.profile_image) : nil,
           profile_icon: char.profile_icon ? Website.get_file_info(char.profile_icon) : nil,
           bg_shared: char.bg_shared
-          
+
         }
       end
     end
   end
 end
-
-
