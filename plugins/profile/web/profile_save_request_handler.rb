15a73105f9b87e6ccb5b5069229d657ce06f13f3
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-04-09 12:10:10 -0400

    Plot preferences

--- plugins/profile/web/profile_save_request_handler.rb
+++ plugins/profile/web/profile_save_request_handler.rb
@@ -2,36 +2,36 @@ module AresMUSH
   module Profile
     class ProfileSaveRequestHandler
       def handle(request)
-        
+
         enactor = request.enactor
         char = Character[request.args[:id]]
 
         if (!enactor)
           return { error: t('webportal.login_required') }
         end
-        
+
         error = Website.check_login(request)
         return error if error
-        
+
         if (!char)
           return { error: t('webportal.not_found') }
         end
-        
+
         if (!Profile.can_manage_char_profile?(enactor, char))
           return { error: t('dispatcher.not_allowed') }
         end
-        
+
         if (!char.is_approved?)
           return { error: t('profile.not_yet_approved') }
         end
-              
+
         request.args[:demographics].each do |name, value|
           if (value.blank? && Demographics.required_demographics.include?(name))
             return { error: t('webportal.missing_required_fields') }
           end
           char.update_demographic name, value
         end
-        
+
         tags = (request.args[:tags] || []).map { |t| t.downcase }.select { |t| !t.blank? }
         gallery = (request.args[:gallery] || []).map { |g| g.downcase }
         profile_image = build_image_path(char, request.args[:profile_image])
@@ -40,7 +40,7 @@ module AresMUSH
         char.update(profile_image: profile_image)
         char.update(profile_icon: profile_icon)
         char.update(profile_tags: tags)
-        
+
         relationships = {}
         (request.args[:relationships] || {}).each do |name, data|
           relationships[name.titleize] = {
@@ -49,26 +49,27 @@ module AresMUSH
             category: data['category'].blank? ? "Associates" : data['category'].titleize
             }
         end
-        
+
         char.update(rp_hooks: Website.format_input_for_mush(request.args[:rp_hooks]))
+        char.update(plot_prefs: Website.format_input_for_mush(request.args[:plot_prefs]))
         char.update(relationships: relationships)
         char.update(bg_shared: request.args[:bg_shared].to_bool)
-        
-        
+
+
         ## DO PROFILE LAST SO IT TRIGGERS THE SOURCE HISTORY UPDATE
         profile = {}
         (request.args[:profile] || {}).each do |name, text|
           profile[name.titleize] = Website.format_input_for_mush(text)
         end
         char.set_profile(profile, enactor)
-        
+
         Achievements.award_achievement(enactor, "profile_edit", 'portal', "Edited your character profile.")
-        
-        
-        {    
+
+
+        {
         }
       end
-      
+
       def build_image_path(char, arg)
         return nil if !arg
         folder = Profile.character_page_folder(char)
@@ -77,5 +78,3 @@ module AresMUSH
     end
   end
 end
-
-
