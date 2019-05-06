0a472406aef191c5a1e788157a6cc6b7da2bbfdb
Linda Naughton <lynn@wordsmyth.org>
2019-05-06 02:41:34 -0400

    Pages read marker.

--- plugins/page/events/page_cron_event_handler.rb
+++ plugins/page/events/page_cron_event_handler.rb
@@ -2,12 +2,12 @@ module AresMUSH
   module Page   
     class CronEventHandler
       def on_event(event)
-        config = Global.read_config("page", "unread_page_deletion_cron")
+        config = Global.read_config("page", "page_deletion_cron")
         return if !Cron.is_cron_match?(config, event.time)
         
-        delete_days = Global.read_config('page', 'unread_page_deletion_days')
+        delete_days = Global.read_config('page', 'page_deletion_days')
         
-        PageMessage.select { |p| p.is_read? }.each do |msg|
+        PageMessage.each do |msg|
           elapsed_days = (Time.now - msg.created_at) / 86400
           if (elapsed_days > delete_days)
             msg.delete
