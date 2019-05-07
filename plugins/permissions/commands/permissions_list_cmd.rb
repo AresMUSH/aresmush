d72821044fe354278beaa6192f2187c707dc689f
g33kcub <g33kcub@gmail.com>
2019-05-07 03:58:14 -0400

    Update permissions_list_cmd.rb

--- plugins/permissions/commands/permissions_list_cmd.rb
+++ plugins/permissions/commands/permissions_list_cmd.rb
@@ -3,22 +3,8 @@ module AresMUSH
     class PermissionsListCmd
       include CommandHandler
       def handle
-        list = []
-        all_help = Global.read_config("permissions", "permissions") || {}
-
-        Permissions.permissions.sort.each do |d|
-          help = all_help[d]
-          editable = "    "
-          name = "%xh#{d.titleize}#{editable}%xn"
-
-          if (!help)
-            help = "%xc#{d} <value>%xn"
-          end
-          list << "#{editable}#{help}"
-        end
-
-        footer = "%ld%R* - #{t('demographics.demographics_ediable')}"
-        template = BorderedListTemplate.new(list, t('Permissions.permission_title'), footer) 
+        permissions = Permissions.permissions
+        template = PermissionPageTemplate.new permissions
         client.emit template.render
       end
     end
