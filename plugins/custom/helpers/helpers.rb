77e6f2d32088dfaaf29f0e43581f07452b2e6e8e
g33kcub <g33kcub@gmail.com>
2019-05-07 03:49:50 -0400

    Update helpers.rb

--- plugins/custom/helpers/helpers.rb
+++ plugins/custom/helpers/helpers.rb
@@ -1,6 +1,6 @@
 module AresMUSH
   module Custom
-    def channel_alert(message)
+    def self.channel_alert(message)
       channel = Global.read_config("custom", "alert_channel")
       if (channel)
         Channels.send_to_channel(channel, message)
