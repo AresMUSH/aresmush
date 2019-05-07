549812b80e75722a8a43ade25bc35d6a741f55a0
g33kcub <g33kcub@gmail.com>
2019-05-07 01:24:09 -0400

    global logger add-on

--- plugins/fs3magix/events/sanity_cron_handler.rb
+++ plugins/fs3magix/events/sanity_cron_handler.rb
@@ -4,6 +4,7 @@ module AresMUSH
       def on_event(event)
         config = Global.read_config("fs3magix", "sanity_cron")
         return if !Cron.is_cron_match?(config, event.time)
+        Global.logger.debug "Time for a sanity check."
       end
     end
   end
--- plugins/fs3magix/events/will_cron_handler.rb
+++ plugins/fs3magix/events/will_cron_handler.rb
@@ -4,6 +4,7 @@ module AresMUSH
       def on_event(event)
         config = Global.read_config("fs3magix", "will_cron")
         return if !Cron.is_cron_match?(config, event.time)
+        Global.logger.debug "Time for restoration of will."
       end
     end
   end
