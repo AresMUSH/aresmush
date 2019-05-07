1ac5832322578c6ef9adf9dd04017c16bfe432b8
g33kcub <g33kcub@gmail.com>
2019-05-07 03:42:24 -0400

    Update permissions_list_cmd.rb

--- plugins/permissions/commands/permissions_list_cmd.rb
+++ plugins/permissions/commands/permissions_list_cmd.rb
@@ -3,7 +3,7 @@ module AresMUSH
     class PermissionsListCmd
       include CommandHandler
       def handle
-        PermissionPageTemplate.new("/attributes.erb", Permissions.permissions)
+        PermissionPageTemplate.new("/permissions.erb", Permissions.permissions)
         client.emit template.render
       end
     end
