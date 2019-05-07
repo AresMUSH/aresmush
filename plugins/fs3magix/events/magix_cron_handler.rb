3dd5ea10d311747a66ce59a8c79566dd3fe203c4
g33kcub <g33kcub@gmail.com>
2019-05-07 07:40:38 -0400

    cron changes

--- plugins/fs3magix/events/magix_cron_handler.rb
+++ plugins/fs3magix/events/magix_cron_handler.rb
@@ -1,15 +1,20 @@
 module AresMUSH
   module FS3Magix
     class MagixCronHandler
-      def on_event_will(event)
-        config = Global.read_config("fs3magix", "will_cron")
-        return if !Cron.is_cron_match?(config, event.time)
+      def on_event(event)
+        if (Cron.is_cron_match?(Global.read_config("fs3magix", "sanity_cron"), event.time))
+          handle_sanity_check
+        end
+
+        if (Cron.is_cron_match?(Global.read_config("fs3matrix", "will_cron"), event.time))
+          handle_will_restoration
+        end
+      end
+      def handle_will_restoration
         Global.logger.debug "Time for restoration of will."
         Custom.channel_alert("Running Will Restoration.")
       end
-      def on_event_sanity(event)
-        config = Global.read_config("fs3magix", "sanity_cron")
-        return if !Cron.is_cron_match?(config, event.time)
+      def handle_sanity_check
         Global.logger.debug "Time for a sanity check."
         Custom.channel_alert("Running Sanity Check.")
       end
--- plugins/fs3skills/events/xp_cron_handler.rb
+++ plugins/fs3skills/events/xp_cron_handler.rb
@@ -1,20 +1,20 @@
 module AresMUSH
-  module FS3Skills    
+  module FS3Skills
     class XpCronHandler
       def on_event(event)
         config = Global.read_config("fs3skills", "xp_cron")
         return if !Cron.is_cron_match?(config, event.time)
-        
+
         Global.logger.debug "XP awards."
-        
+        Custom.channel_alert("Handing out Periodic XP.")
         periodic_xp = Global.read_config("fs3skills", "periodic_xp")
         max_xp = Global.read_config("fs3skills", "max_xp_hoard")
-        
+
         approved = Chargen.approved_chars
         approved.each do |a|
           FS3Skills.modify_xp(a, periodic_xp)
         end
       end
-    end    
+    end
   end
 end
