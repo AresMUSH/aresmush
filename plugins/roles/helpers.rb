be2c0e9fe23c4bf564d10566d25bae53bd627019
g33kcub <g33kcub@gmail.com>
2019-05-07 06:27:18 -0400

    Update helpers.rb

--- plugins/roles/helpers.rb
+++ plugins/roles/helpers.rb
@@ -1,22 +1,23 @@
 module AresMUSH
   module Roles
     def self.all_roles
-      Role.all.map { |r| r.name }
+      role_list = Role.all.map { |r| r.name }
+      role_list.sort
     end
-    
+
     def self.restricted_roles
       Role.all.select { |r| r.is_restricted }.map { |r| r.name }
     end
-    
+
     def self.can_assign_role?(actor)
       actor.is_admin?
     end
-    
+
     def self.is_restricted?(name)
       role = Role.find_one_by_name(name)
       restricted_roles.include?(role)
     end
-    
+
     def self.chars_with_role(name)
       role = Role.find_one_by_name(name)
       Character.all.select { |c| c.roles.include?(role) }
