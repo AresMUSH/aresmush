806c1813a2fe5e825dc874a2e2f638cb85fb17f5
g33kcub <g33kcub@gmail.com>
2019-05-07 04:02:42 -0400

    Update permissions_list_cmd.rb

--- plugins/permissions/commands/permissions_list_cmd.rb
+++ plugins/permissions/commands/permissions_list_cmd.rb
@@ -10,16 +10,11 @@ module AresMUSH
         when 1
           template = PermissionPageTemplate.new("/permissions.erb",
               { permissions: Permissions.permissions, num_pages: num_pages, page: cmd.page })
-          else
-             client.emit_failure t('pages.not_that_many_pages')
-             return
-           end
         else
           client.emit_failure t('pages.not_that_many_pages')
-          return
-        end
-
-        client.emit template.render
+            return
+       end
+      client.emit template.render
       end
     end
   end
