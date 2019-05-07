a9b17e38dc1988eb638afc7c9dbc74267981f5e2
g33kcub <g33kcub@gmail.com>
2019-05-07 07:18:08 -0400

    Update magix_cron_handler.rb

--- plugins/fs3magix/events/magix_cron_handler.rb
+++ plugins/fs3magix/events/magix_cron_handler.rb
@@ -1,13 +1,13 @@
 module AresMUSH
   module FS3Magix
     class MagixCronHandler
-      def on_event(event)
+      def on_event_will(event)
         config = Global.read_config("fs3magix", "will_cron")
         return if !Cron.is_cron_match?(config, event.time)
         Global.logger.debug "Time for restoration of will."
         Custom.channel_alert("Running Will Restoration.")
       end
-      def on_event(event)
+      def on_event_sanity(event)
         config = Global.read_config("fs3magix", "sanity_cron")
         return if !Cron.is_cron_match?(config, event.time)
         Global.logger.debug "Time for a sanity check."
