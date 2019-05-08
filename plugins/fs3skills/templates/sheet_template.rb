b838ce6d4798f00e712eedb1318aefe74e813ebf
g33kcub <g33kcub@gmail.com>
2019-05-08 11:22:21 -0400

    Update sheet_template.rb

--- plugins/fs3skills/templates/sheet_template.rb
+++ plugins/fs3skills/templates/sheet_template.rb
@@ -91,7 +91,7 @@ module AresMUSH
         "#{linebreak}#{left(name, 16)} #{right(rating_text,20)}"
       end
 
-      def format_skill(s, i, show_linked_attr = false)
+      def format_skill(s, i, show_linked_attr = true)
         name = "%xh#{s.name}:%xn"
         linked_attr = show_linked_attr ? print_linked_attr(s) : ""
         linebreak = i % 2 == 1 ? "" : "%r"
@@ -99,7 +99,7 @@ module AresMUSH
         "#{linebreak}#{left(name, 16)} #{left(rating_text, 20)}"
       end
 
-      def format_magix(s, i, show_linked_attr = false)
+      def format_magix(s, i, show_linked_attr = true)
         name = "%xh#{s.name}:%xn"
         linked_attr = show_linked_attr ? print_linked_attr(s) : ""
         linebreak = i % 2 == 1 ? "" : "%r"
