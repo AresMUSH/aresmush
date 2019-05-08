9bd9b2719c87fbce265d01f625574ab0b6d2fb51
g33kcub <g33kcub@gmail.com>
2019-05-08 13:53:03 -0400

    qwv

--- plugins/fs3skills/web/char_abilities_request_handler.rb
+++ plugins/fs3skills/web/char_abilities_request_handler.rb
@@ -4,28 +4,28 @@ module AresMUSH
       def handle(request)
         char = Character.find_one_by_name request.args[:id]
         enactor = request.enactor
-        
+
         if (!char)
           return []
         end
 
         error = Website.check_login(request, true)
         return error if error
-        
-        
+
+
         can_view = FS3Skills.can_view_sheets?(enactor) || (enactor && enactor.id == char.id)
         if (!can_view)
           return { error: t('dispatcher.not_alllowed') }
         end
-        
+
         abilities = []
-      
-        [ char.fs3_attributes, char.fs3_action_skills, char.fs3_background_skills, char.fs3_languages, char.fs3_advantages ].each do |list|
+
+        [ char.fs3_attributes, char.fs3_action_skills, char.fs3_magix_arts, char.fs3_background_skills, char.fs3_languages, char.fs3_advantages ].each do |list|
           list.each do |a|
             abilities << a.name
           end
         end
-        
+
         return abilities
       end
     end
--- plugins/fs3skills/web/chargen_char_request_handler.rb
+++ plugins/fs3skills/web/chargen_char_request_handler.rb
@@ -3,33 +3,34 @@ module AresMUSH
     class ChargenCharRequestHandler
       def handle(request)
         char = request.enactor
-        
+
         if (!char)
           return { error: t('webportal.login_required') }
         end
-        
+
         error = Website.check_login(request)
         return error if error
 
         if (char.is_approved?)
           return { error: t('chargen.you_are_already_approved')}
         end
-                
+
         return { chargen_locked: true } if Chargen.is_chargen_locked?(char)
-        
-        
+
+
         {
           fs3_attributes: get_ability_list(char, char.fs3_attributes, :attribute),
           fs3_action_skills: get_ability_list(char, char.fs3_action_skills, :action),
+          fs3_magix_arts: get_ability_list(char,char.fs3_magix_arts, :magix)
           fs3_backgrounds: get_ability_list(char, char.fs3_background_skills, :background),
           fs3_languages: get_ability_list(char, char.fs3_languages, :language),
           fs3_advantages: get_ability_list(char, char.fs3_advantages, :advantage),
           reset_needed: !char.fs3_attributes.map { |a| a.rating > 1 }.any?,
           use_advantages: FS3Skills.use_advantages?
-        } 
+        }
       end
-      
-      
+
+
       def get_ability_list(char, list, ability_type)
         case ability_type
         when :attribute
@@ -40,6 +41,10 @@ module AresMUSH
           metadata = FS3Skills.action_skills
           starting_rating = 1
           starting_rating_name = t('fs3skills.everyman_rating')
+        when :magix
+          metadata = FS3Magix.magix_arts_list
+          starting_rating = 1
+          starting_rating_name = t('fs3skills.unskilled_rating')
         when :language
           metadata = FS3Skills.languages
           starting_rating = 0
@@ -51,16 +56,16 @@ module AresMUSH
         else
           metadata = nil
         end
-        
-        abilities = list.to_a.sort_by { |a| a.name }.map { |a| 
-          { 
-            name: a.name, 
-            rating: a.rating, 
+
+        abilities = list.to_a.sort_by { |a| a.name }.map { |a|
+          {
+            name: a.name,
+            rating: a.rating,
             rating_name: a.rating_name,
             desc: (metadata) ? FS3Skills.get_ability_desc(metadata, a.name) : nil,
             specialties: (metadata) ? build_specialty_list(char, metadata, a.name) : nil
           }}
-          
+
           if (metadata)
             metadata.each do |m|
               existing = abilities.select { |a| a[:name].titlecase == m['name'].titlecase }.first
@@ -69,17 +74,17 @@ module AresMUSH
               end
             end
           end
-                    
+
           abilities.sort_by { |a| a[:name] }
       end
-      
+
       def build_specialty_list(char, metadata, ability_name)
         metadata = metadata.select { |m| m['name'] == ability_name }.first
         return nil if !metadata
         specialty_names = metadata['specialties']
 
         return nil if !specialty_names
-        
+
         specialties = []
         specialty_names.each do |s|
           ability = FS3Skills.find_ability(char, ability_name)
@@ -87,12 +92,10 @@ module AresMUSH
             specialties << { name: s, selected: ability.specialties.include?(s) }
           end
         end
-        
+
         specialties
       end
-      
+
     end
   end
 end
-
-
