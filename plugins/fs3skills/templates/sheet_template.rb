12baf006a5506f591828e6e588451859132dcdb8
g33kcub <g33kcub@gmail.com>
2019-05-08 14:08:56 -0400

    Update sheet_template.rb

--- plugins/fs3skills/templates/sheet_template.rb
+++ plugins/fs3skills/templates/sheet_template.rb
@@ -36,7 +36,6 @@ module AresMUSH
       def magix
        list = []
         @char.fs3_magix_arts.sort_by(:name, :order => "Alpha").each_with_index do |a, i|
-            if a.rating_name != "Unskilled"
               list << format_skill(a, i)
             end
         end
