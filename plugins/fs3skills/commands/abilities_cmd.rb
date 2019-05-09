7fcce385003b64199f06ced62c44859b05f21c21
g33kcub <g33kcub@gmail.com>
2019-05-09 15:41:42 -0400

    magix

--- plugins/fs3skills/commands/abilities_cmd.rb
+++ plugins/fs3skills/commands/abilities_cmd.rb
@@ -3,25 +3,28 @@ module AresMUSH
   module FS3Skills
     class AbilitiesCmd
       include CommandHandler
-      
+
       def handle
-        
-        num_pages = FS3Skills.use_advantages? ? 5 : 4
-        
+
+        num_pages = FS3Skills.use_advantages? ? 6 : 5
+
         case cmd.page
         when 1
-          template = AbilityPageTemplate.new("/attributes.erb", 
+          template = AbilityPageTemplate.new("/attributes.erb",
               { attrs: FS3Skills.attrs, num_pages: num_pages, page: cmd.page })
         when 2
-          template = AbilityPageTemplate.new("/action_skills.erb", 
+          template = AbilityPageTemplate.new("/action_skills.erb",
               { skills: FS3Skills.action_skills.sort_by { |a| a['name'] }, num_pages: num_pages, page: cmd.page })
         when 3
-          template = AbilityPageTemplate.new("/background_skills.erb", 
-              { skills: FS3Skills.background_skills, num_pages: num_pages, page: cmd.page } )
+          template = AbilityPageTemplate.new("/magix_arts.erb",
+              { skills: FS3Skills.magix.sort_by { |a| a['name'] }, num_pages: num_pages, page: cmd.page })
         when 4
-          template = AbilityPageTemplate.new("/languages.erb", 
+          template = AbilityPageTemplate.new("/background_skills.erb",
+              { skills: FS3Skills.background_skills, num_pages: num_pages, page: cmd.page } )
+        when 5
+          template = AbilityPageTemplate.new("/languages.erb",
               { skills: FS3Skills.languages.sort_by { |a| a['name'] }, num_pages: num_pages, page: cmd.page })
-        when 5 
+        when 6
           if (FS3Skills.use_advantages?)
             template = AbilityPageTemplate.new("/advantages.erb",
                { advantages: FS3Skills.advantages.sort_by { |a| a['name'] }, num_pages: num_pages, page: cmd.page })
@@ -33,7 +36,7 @@ module AresMUSH
           client.emit_failure t('pages.not_that_many_pages')
           return
         end
-      
+
         client.emit template.render
       end
     end
