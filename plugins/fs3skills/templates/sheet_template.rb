fd0296d1c997f73824c4d4d64745a3a8afd75681
g33kcub <g33kcub@gmail.com>
2019-05-09 11:23:55 -0400

    Update sheet_template.rb

--- plugins/fs3skills/templates/sheet_template.rb
+++ plugins/fs3skills/templates/sheet_template.rb
@@ -104,8 +104,7 @@ module AresMUSH
         linked_attr = show_linked_attr ? print_linked_attr(s) : ""
         linebreak = i % 2 == 1 ? "" : "%r"
         rating_text = "#{s.rating_name}#{linked_attr}"
-        has_stat = FS3Skills.has_ability(char,s.name)
-        "#{linebreak}#{left(name, 16)} #{has_stat} #{left(rating_text, 20)}"
+        "#{linebreak}#{left(name, 16)} #{left(rating_text, 20)}"
       end
 
       def format_language(s, i, show_linked_attr = false)
