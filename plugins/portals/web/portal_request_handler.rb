0d4bd65c364896d6eb537f2b2f90519970d29e5e
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-05-15 17:02:43 -0400

    Update portal_request_handler.rb

--- plugins/portals/web/portal_request_handler.rb
+++ plugins/portals/web/portal_request_handler.rb
@@ -11,21 +11,37 @@ module AresMUSH
           return { error: t('webportal.not_found') }
         end
 
-        error = Website.check_login(request, true)
-        return error if error
+
+
+        gms = portal.gms.to_a
+            .sort_by {|gm| gm.name }
+            .map { |gm| { name: gm.name, id: gm.id, is_ooc: gm.is_admin? || gm.is_playerbit?  }}
+
+
+        all_schools = portal.all_schools.to_a
+
+
+
+
 
         {
           name: portal.name,
           primary_school: portal.primary_school,
-          all_schools: portal.all_schools,
-          creatures: portal.creatures,
-          npcs: portal.npcs,
-          gms: portal.gms.to_a,
+          all_schools: all_schools,
+          edit_creatures: Website.format_input_for_html(portal.creatures),
+          creatures: Website.format_markdown_for_html(portal.creatures),
+          edit_npcs:  Website.format_input_for_html(portal.npcs),
+          npcs: Website.format_markdown_for_html(portal.npcs),
+          gms: gms,
           location: portal.location,
-          description: portal.description,
-          trivia: portal.trivia,
-          events: portal.events,
-          id: portal.id
+          edit_desc: Website.format_input_for_html(portal.description),
+          description: Website.format_markdown_for_html(portal.description),
+          edit_trivia:  Website.format_input_for_html(portal.trivia),
+          trivia: Website.format_markdown_for_html(portal.trivia),
+          edit_events:  Website.format_input_for_html(portal.events),
+          events: Website.format_markdown_for_html(portal.events),
+          id: portal.id,
+          pinterest: portal.pinterest
         }
       end
 
