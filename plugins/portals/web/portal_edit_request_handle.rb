4201775361e305283b30676e13437be8702cce94
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-05-16 16:34:42 -0400

    Update portal_edit_request_handle.rb

--- plugins/portals/web/portal_edit_request_handle.rb
+++ plugins/portals/web/portal_edit_request_handle.rb
@@ -2,23 +2,19 @@ module AresMUSH
   module Portals
     class PortalEditRequestHandler
       def handle(request)
-        Global.logger.debug "Edit Request: #{request.args}"
         portal = Portal.find_one_by_name(request.args[:id])
-        Global.logger.debug "Edit Portal: #{portal}"
-        Global.logger.debug "Edit PortalID: #{portal.id}"
-        Global.logger.debug "PrimarySchoolArgs: #{request.args[:primary_school]}"
         enactor = request.enactor
 
         if (!portal)
           return { error: t('webportal.not_found') }
         end
 
-        # error = Website.check_login(request, true)
-        # return error if error
-        #
-        # if (!enactor.is_approved?)
-        #   return { error: t('dispatcher.not_allowed') }
-        # end
+        error = Website.check_login(request, true)
+        return error if error
+
+        if (!enactor.is_approved?)
+          return { error: t('dispatcher.not_allowed') }
+        end
 
         Global.logger.debug "Portal #{portal.id} (#{portal.name} Portal) edited by #{enactor.name}."
 
@@ -49,18 +45,13 @@ module AresMUSH
 
           school_name = request.args[:primary_school].to_s
           id = Global.read_config("schools", request.args[:primary_school], "id")
-          Global.logger.debug "School Name #{school_name}"
-          Global.logger.debug "School ID #{id}"
           primary_school = {:name => school_name, :id => id}
-          Global.logger.debug "Primary School #{primary_school}"
           portal.update(primary_school: primary_school)
 
 
 
-          Global.logger.debug "gms #{request.args[:gms]}"
-          Global.logger.debug "all_schools #{request.args[:all_schools]}"
-
           portal.update(name: request.args[:name])
+          portal.update(pinterest: request.args[:pinterest])
           portal.update(creatures: request.args[:creatures])
           portal.update(npcs: request.args[:npcs])
           portal.update(location: request.args[:location])
