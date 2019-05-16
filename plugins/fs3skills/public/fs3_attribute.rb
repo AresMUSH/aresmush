38a77110b15ceb3f870d537b72f6452315b031c2
g33kcub <g33kcub@gmail.com>
2019-05-16 14:29:35 -0400

    Update fs3_attribute.rb

--- plugins/fs3skills/public/fs3_attribute.rb
+++ plugins/fs3skills/public/fs3_attribute.rb
@@ -2,31 +2,33 @@ module AresMUSH
   class FS3Attribute < Ohm::Model
     include ObjectModel
     include LearnableAbility
-    
+
     reference :character, "AresMUSH::Character"
     attribute :name
     attribute :rating, :type => DataType::Integer, :default => 0
-    
+
     index :name
-    
+
     def print_rating
       case rating
       when 0
-        return ""
+        return "0"
       when 1
-        return "%xg@%xn"
+        return "%xh%xg1%xn"
       when 2
-        return "%xg@@%xn"
+        return "%xh%xg2%xn"
       when 3
-        return "%xg@@%xy@%xn"
+        return "%xh%xy3%xn"
       when 4
-        return "%xg@@%xy@@%xn"
+        return "%xh%xr4%xn"
+      when 5
+        return "%xh%xb5%xn"
       end
     end
-    
+
     def rating_name
       case rating
-      when 0 
+      when 0
         return ""
       when 1
         return t('fs3skills.poor_rating')
