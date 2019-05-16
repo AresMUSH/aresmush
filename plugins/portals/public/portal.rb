97ca893646e8191fd65162bb27d2c4069ba6dd99
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-05-16 16:34:35 -0400

    Update portal.rb

--- plugins/portals/public/portal.rb
+++ plugins/portals/public/portal.rb
@@ -17,9 +17,9 @@ module AresMUSH
 
     set :gms, "AresMUSH::Character"
 
-    def gm_names
-      self.gmd.sort { |gm| gm.name }.map { |gm| gm.name }
-    end
+    # def gm_names
+    #   self.gmd.sort { |gm| gm.name }.map { |gm| gm.name }
+    # end
 
     def self.find_any_by_name(name_or_id)
       return [] if !name_or_id
@@ -40,40 +40,6 @@ module AresMUSH
 
     def self.named(name)
       find_one_by_name(name)
-    end
-
-    def update_portal(key, value)
-      name = key.to_s.downcase
-      if (name == "name")
-        self.update(name: value)
-        self.update(name_upcase: value.upcase)
-      elsif (name == "primary_school")
-        self.update(primary_school: value)
-      elsif (name == "all_schools")
-        all_schools = self.all_schools
-        new_all_schools = all_schools.concat [value]
-        self.update(all_schools: new_all_schools)
-      elsif (name == "creatures")
-        all_schools = self.all_schools
-        new_all_schools = all_schools.concat [value]
-        self.update(all_schools: new_all_schools)
-      elsif (name == "npcs")
-        npcs = self.npcs
-        new_npcs = npcs.concat [value]
-        self.update(npcs: new_npcs)
-      elsif (name == "gms")
-        gms = self.gms
-        new_gms = gms.concat [value]
-        self.update(gms: gms)
-      elsif (name == "description")
-        self.update(description: value)
-      elsif (name == "trivia")
-        self.update(trivia: value)
-      elsif (name == "events")
-        self.update(events: value)
-      elsif (name == "location")
-        self.update(location: value)
-      end
     end
 
 
