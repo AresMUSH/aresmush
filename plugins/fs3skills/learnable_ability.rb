acc7e2588c32afa69d854eb0621a771cb38b4902
g33kcub <g33kcub@gmail.com>
2019-05-20 12:42:01 -0400

    Update learnable_ability.rb

--- plugins/fs3skills/learnable_ability.rb
+++ plugins/fs3skills/learnable_ability.rb
@@ -1,7 +1,7 @@
 module AresMUSH
   module LearnableAbility
     def self.included(base)
-      base.send :extend, ClassMethods   
+      base.send :extend, ClassMethods
       base.send :register_data_members
     end
     module ClassMethods
@@ -10,27 +10,27 @@ module AresMUSH
         attribute :xp, :type => Ohm::DataTypes::DataType::Integer, :default => 0
       end
     end
-    
+
     def learn
       update(last_learned: Time.now)
       update(xp: self.xp + 1)
     end
-    
+
     def xp_needed
       FS3Skills.xp_needed(self.name, self.rating)
     end
-    
+
     def learning_complete
       self.xp >= self.xp_needed
     end
-    
+
     def can_learn?
       self.time_to_next_learn <= 0
     end
-    
-    def time_to_next_learn
+
+    def time_to_next_learn(a)
       return 0 if !self.last_learned
-      time_left = (FS3Skills.days_between_learning * 86400) - (Time.now - self.last_learned)
+      time_left = (FS3Skills.days_between_learning(a) * 86400) - (Time.now - self.last_learned)
       [time_left, 0].max
     end
   end
