ac016706197d1d941528cd0674eb856f558e6d08
g33kcub <g33kcub@gmail.com>
2019-05-21 16:34:31 -0400

    Update console_char.rb

--- plugins/superconsole/public/console_char.rb
+++ plugins/superconsole/public/console_char.rb
@@ -4,14 +4,13 @@ module AresMUSH
     attribute :console_experience, :type => DataType::Integer, :default => 0
     attribute :console_dmg_health, :type => DataType::Integer, :default => 0
     attribute :console_dmg_mana, :type => DataType::Integer, :default => 0
+    attribute :console_dmg_stamina, :type => DataType::Integer, :default => 0
     attribute :console_oversoul, :type => DataType::Integer, :default => 0
     attribute :console_oversoul_type
 
     collection :console_patience, "AresMUSH::ConsolePatience"
     collection :console_attributes, "AresMUSH::ConsoleAttribute"
-    collection :console_abilities, "AresMUSH::ConsoleAbility"
-    collection :console_castables, "AresMUSH::ConsoleCastable"
-    collection :console_actions, "AresMUSH::ConsoleAction"
+    collection :console_skills, "AresMUSH::ConsoleAbility"
     collection :console_inventory,"AresMUSH::ConsoleInventory"
     collection :console_equipped, "AresMUSH::ConsoleEquipped"
     collection :console_huntlog, "AresMUSH::ConsoleHuntLog"
@@ -19,5 +18,35 @@ module AresMUSH
     collection :console_companion, "AresMUSH::ConsoleCompanion"
     collection :console_status, "AresMUSH::ConsoleStatus"
 
+  end
+  before_delete :delete_abilities
+
+  def delete_abilities
+    [ self.console_patience, self.console_attributes, self.console_skills, self.console_inventory,  self.console_equipped, self.console_huntlog, self.console_questlog, self.console_companion, self.console_status,].each do |list|
+      list.each do |a|
+        a.delete
+      end
+    end
+  end
+  def level
+    self.console_level
+  end
+  def race
+    self.group("Race") || "Unknown"
+  end
+  def class
+    self.group("Class") || "Unknown"
+  end
+  def profession
+    self.group("Profession") || "Unknown"
+  end
+  def health_bar
+    unused = "%X1 %xn"
+    used = "%X242 %xn"
+    dmg = self.console_dmg_health / 2
+    left = (100 - dmg) / 2
+    good = unused.floor.repeat(left)
+    bad = used.floor.repeat(dmg)
+    good + bad
   end
 end
