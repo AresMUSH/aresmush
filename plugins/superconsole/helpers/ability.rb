b3c77cc5010def14757e6d7b20a1956127bd150b
g33kcub <g33kcub@gmail.com>
2019-05-22 13:55:33 -0400

    Update ability.rb

--- plugins/superconsole/helpers/ability.rb
+++ plugins/superconsole/helpers/ability.rb
@@ -8,5 +8,9 @@ module AresMUSH
     def self.attr_names
       attrs.map { |a| a['name'].titlecase }
     end
+
+    def self.set_ability(char, ability_name, rating)
+      ConsoleAttribute.create(character: char, name: ability_name, rating: rating, favored: false, unfavored: false)
+    end
   end
 end
