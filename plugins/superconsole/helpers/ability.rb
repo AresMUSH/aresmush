9c72cf81fe55192d43f9abbdd7f086e28b6cc9d4
g33kcub <g33kcub@gmail.com>
2019-05-22 17:24:23 -0400

    update

--- plugins/superconsole/helpers/ability.rb
+++ plugins/superconsole/helpers/ability.rb
@@ -13,6 +13,37 @@ module AresMUSH
       ConsoleAttribute.create(character: char, name: ability_name, rating: rating, favored: false, unfavored: false)
     end
 
+    def self.check_ability_name(ability)
+      return t('SuperConsole.no_special_characters') if (ability !~ /^[\w\s]+$/)
+      return nil
+    end
+
+    def self.update_ability(char,ability_name,field,value)
+      ability_name = ability_name ? ability_name.titleize : nil
+      error = SuperConsole.check_ability_name(ability_name)
+      if (error)
+        return error
+      end
+      ability_type = FS3Skills.get_ability_type(ability_name)
+      ability = FS3Skills.find_ability(char, ability_name)
+      if (ability)
+        case ability_type
+        when :attribute
+          case field
+          when rating
+            ability.update(rating: value)
+          when favored
+            ability.update(favored: value)
+          when unfavored
+            ability.update(unfavored: value)
+          else
+            t('superconsole.update_error_attribute', :name => ability_name, :field => field)
+        end
+      else
+        t('superconsole.set_ability', :name => ability_name)
+    end
+
+
     def self.get_ability_type(ability)
       ability = ability.titlecase
       if (attr_names.include?(ability))
@@ -32,7 +63,6 @@ module AresMUSH
         nil
       end
     end
-
 
     def self.ability_rating(char, ability_name)
       ability = SuperConsole.find_ability(char, ability_name)
