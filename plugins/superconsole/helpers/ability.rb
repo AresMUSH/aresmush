9e23cfc29e10f49d304b924307364c0dd51e2741
g33kcub <g33kcub@gmail.com>
2019-05-22 13:50:53 -0400

    Update ability.rb

--- plugins/superconsole/helpers/ability.rb
+++ plugins/superconsole/helpers/ability.rb
@@ -1,7 +1,12 @@
 module AresMUSH
   module SuperConsole
 
+    def self.attrs
+      Global.read_config("superconsole", "attributes")
+    end
 
-
+    def self.attr_names
+      attrs.map { |a| a['name'].titlecase }
+    end
   end
 end
