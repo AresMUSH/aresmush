fcc952ec37a36480bf59636e8a3b3eba00040f63
g33kcub <g33kcub@gmail.com>
2019-05-25 12:58:50 -0400

    Update learn_template.rb

--- plugins/superconsole/templates/learn_template.rb
+++ plugins/superconsole/templates/learn_template.rb
@@ -48,21 +48,19 @@ module AresMUSH
 
       def abils_learning
        list = []
-       stats = @char.console_skills.sort_by(:name, :order => "Alpha")
-       filter = stats.select{ |x,_| x.learned == false}
-       filtered = filter.select{ |x,_| x.learnpoints >= 1}
-            filtered.each_with_index do |a, i|
-            list << format_skill(a, i)
+       @char.console_skills.sort_by(:name, :order => "Alpha").each_with_index do |a, i|
+            list << format_skill_learn(a, i)
         end
               list
       end
-      def format_skill(a, i)
+      def format_skill_learn(a, i)
         name = "%xh#{a.name}:%xn"
-        linebreak = i % 2 == 1 ? "" : "%r"
-        lb2 = i == 0 ? "" : "#{linebreak}"
-        spacebreak = i % 2 == 0 ? "  " : ""
+        lp = a.learnpoints
+        lpn = SuperConsole.get_max_learn_adj(@char,a.name)
+        percent = (lp/lpn) * 100
+        canlearn = a.learnable ? "%xg+%xn" : "%xr-%xn"
         rating = "#{a.rating}"
-        "#{lb2}#{left(name, 34)} #{right(rating,3)}#{spacebreak}"
+        "[#{canlearn}] #{left(name, 34)} #{right(rating,3)} #{percent}% #{lp}/#{lpn}"
       end
     end
   end
