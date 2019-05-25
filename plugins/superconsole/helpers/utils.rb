5ed28550438747d31b4042f052d6c2c950b5a41b
g33kcub <g33kcub@gmail.com>
2019-05-25 18:42:51 -0400

    Update utils.rb

--- plugins/superconsole/helpers/utils.rb
+++ plugins/superconsole/helpers/utils.rb
@@ -1,10 +1,10 @@
 module AresMUSH
   module SuperConsole
-    def can_view_sheets(actor)
+    def self.can_view_sheets?(actor)
       return false if !actor
       actor.has_permission?("view_sheets")
     end
-    def can_set_stats(actor)
+    def self.can_set_stats(actor)
       actor.has_permission?("manage_stats")
     end
   end
