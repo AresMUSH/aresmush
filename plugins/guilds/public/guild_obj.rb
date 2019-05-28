9a728c5e2e6759bfbcfe07624bb200ac7b8e03e7
g33kcub <g33kcub@gmail.com>
2019-05-28 16:47:44 -0400

    Create guild_obj.rb

---
+++ plugins/guilds/public/guild_obj.rb
@@ -0,0 +1,12 @@
+module AresMUSH
+  class GuildObj < Ohm::Model
+    include ObjectModel
+
+    reference :character, "AresMUSH::Character"
+    attribute :name
+    attribute :ranking, :type => DataType::Integer, :default => 0
+    attribute :title
+
+    index :name
+  end
+end
