334fe1a6b3c0d4a3e8d8863be878165060c86404
g33kcub <g33kcub@gmail.com>
2019-05-28 17:24:59 -0400

    Update guild_obj.rb

--- plugins/guilds/public/guild_obj.rb
+++ plugins/guilds/public/guild_obj.rb
@@ -6,6 +6,8 @@ module AresMUSH
     attribute :name
     attribute :ranking, :type => DataType::Integer, :default => 0
     attribute :title
+    attribute :is_public, :type => DataType::Boolean, :default => true
+    
 
     index :name
   end
