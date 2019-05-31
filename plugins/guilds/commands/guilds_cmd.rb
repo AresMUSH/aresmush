d5328dea098d2da7078ca43755d1d5deec6509db
g33kcub <g33kcub@gmail.com>
2019-05-31 11:47:03 -0400

    Update guilds_cmd.rb

--- plugins/guilds/commands/guilds_cmd.rb
+++ plugins/guilds/commands/guilds_cmd.rb
@@ -4,7 +4,7 @@ module AresMUSH
       include CommandHandler
 
       def handle
-        guilds = Guilds.all_guilds.sort
+        guilds = Guilds.all_guilds.sort_by { |key| key }
         template = GuildsListTemplate.new guilds
         client.emit template.render
       end
