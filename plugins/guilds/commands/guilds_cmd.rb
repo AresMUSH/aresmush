d6d633975750de26eaf71b02154262019df702d1
g33kcub <g33kcub@gmail.com>
2019-05-31 11:56:16 -0400

    Update guilds_cmd.rb

--- plugins/guilds/commands/guilds_cmd.rb
+++ plugins/guilds/commands/guilds_cmd.rb
@@ -4,7 +4,13 @@ module AresMUSH
       include CommandHandler
 
       def handle
-        guilds = Guilds.all_guilds.sort_by { |key| key['name'] }
+        guilds = []
+        list = Guilds.all_guilds.sort_by { |key| key['name'] }
+        list.each do |a|
+          if a['is_public'] == true || enactor.is_admin?
+            guilds << a
+          end
+        end
         template = GuildsListTemplate.new guilds
         client.emit template.render
       end
