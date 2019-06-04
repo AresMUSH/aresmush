21adefa48a5cb76fba5521d1b18e52d20e1d5343
g33kcub <g33kcub@gmail.com>
2019-06-04 11:37:16 -0400

    Update power.rb

--- plugins/superconsole/helpers/power.rb
+++ plugins/superconsole/helpers/power.rb
@@ -1,16 +1,16 @@
 module AresMUSH
   module SuperConsole
-   def power_abil(char)
+   def self.power_abil(char)
      0
    end
-   def power_attr(char)
+   def self.power_attr(char)
      0
    end
-   def power_level(char)
+   def self.power_level(char)
      lvl = char.level
      lvl * 1000
    end
-   def power_gear(char)
+   def self.power_gear(char)
      0
    end
   end
