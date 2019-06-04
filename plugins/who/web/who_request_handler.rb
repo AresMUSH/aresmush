abac3e191c96283b9f4907032757f8ad7a1cc4d3
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-06-03 21:12:10 -0400

    Update who_request_handler.rb

--- plugins/who/web/who_request_handler.rb
+++ plugins/who/web/who_request_handler.rb
@@ -3,25 +3,25 @@ module AresMUSH
     class WhoRequestHandler
       def handle(request)
         who = {}
-      
+
         Global.client_monitor.logged_in.each do |client, char|
           who[char.name] = build_who_data(char)
         end
-      
+
         Global.client_monitor.web_clients.each do |client|
           char = Character[client.web_char_id]
           next if !char
           next if who.has_key?(char.name)
-        
+
           who[char.name] = build_who_data(char)
         end
-      
+
         {
           who_count: who.count,
           who: who.values.sort_by { |v| v[:name] }
         }
       end
-    
+
       def build_who_data(char)
         {
           name: char.name,
@@ -32,5 +32,3 @@ module AresMUSH
     end
   end
 end
-
-
