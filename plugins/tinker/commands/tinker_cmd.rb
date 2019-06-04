07ff168af9f8851d7da4b114a776917a5b0f7124
Gimmisky <50979911+Gimmisky@users.noreply.github.com>
2019-06-03 23:56:45 -0300

    Testing this Github deal! Try #1

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -2,14 +2,14 @@ module AresMUSH
   module Tinker
     class TinkerCmd
       include CommandHandler
-      
+
       def check_can_manage
         return t('dispatcher.not_allowed') if !enactor.has_permission?("tinker")
         return nil
       end
-      
+
       def handle
-        client.emit_success "Done!"
+        client.emit_success "REALLY Done!"
       end
 
     end
