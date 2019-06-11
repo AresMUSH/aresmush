9b6986987ed0fefef324784caa3ea7f9ac551657
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-06-11 15:16:06 -0400

    Update add_txt_handler.rb

--- plugins/txt/web/add_txt_handler.rb
+++ plugins/txt/web/add_txt_handler.rb
@@ -37,20 +37,21 @@ module AresMUSH
 
                 if pose.start_with?("=")
                     names = enactor.txt_last_scene
-                    names_raw = Txt.format_recipient_indicator(names)
+                    # recipient_names = Txt.format_recipient_indicator(names)
                     message = pose.after("=")
                 elsif pose.include?("=")
                     if (!pose.rest("=").blank? && (pose.first("=").include?("http://") || pose.first("=").include?("https://") ) )
                         names = enactor.txt_last_scene
                         message = pose
                     else
-                        names_raw = pose.first("=")
+                        # recipient_names = pose.first("=")
                         names = pose.first("=") ? pose.first("=").split(" ") : nil
                         message = pose.rest("=")
                     end
                 else
                     names = enactor.txt_last_scene
-                    names_raw = Txt.format_recipient_indicator(names)
+
+                    # recipient_names = Txt.format_recipient_indicator(names)
                     message = pose
                 end
 
@@ -58,21 +59,19 @@ module AresMUSH
                     { error: t('txt.txt_target_missing') }
                 end
 
-                names_raw = InputFormatter.titlecase_arg(names_raw)
+                # recipient_names = InputFormatter.titlecase_arg(recipient_names)
                 #Making 'Names Raw' take actual character names instead of aliases.
-                names_array = names_raw.split(/ /)
-                names_raw = []
-                names_array.each do |name|
+                # names_array = recipient_names.split(/ /)
+                recipient_names = []
+                names.each do |name|
                   char = Character.named(name)
                   if !char
                     return { error: t('txt.no_such_character') }
                   else
-                    names_raw.concat [char.name]
+                    recipient_names.concat [char.name]
                   end
                 end
-                names_raw = names_raw.join(" ")
-
-
+                recipient_names = recipient_names.join(" ")
 
                 scene_room = scene.room
 
@@ -105,33 +104,30 @@ module AresMUSH
                             end
                         end
                     end
-
+                    #Emit to online players
                     names_plus = recipients << enactor.name
 
                     names_plus.each do |name|
-                        result = OnlineCharFinder.find(name)
+                      recipient = Character.named(name)
 
-                        if (!result.found?)
-                            nil
-                        else
-                            recipient = result.target.char
-
+                        if Login.is_online?(recipient)
                             recipient_txt = t('txt.txt_to_recipient_with_scene',
-                            :txt => Txt.format_txt_indicator(enactor, names_raw),
+                            :txt => Txt.format_txt_indicator(enactor, recipient_names),
                             :sender => enactor.name,
-                            # :recipients => names_raw,
+                            # :recipients => recipient_names,
                             :message => message,
                             :scene_id => scene_id)
 
                             if (recipient.page_do_not_disturb)
                               nil
                             elsif ( scene_id && ( recipient.room.scene_id != scene_id ) )
-                                result.target.client.emit recipient_txt
+                              client = Login.find_client(recipient)
+                              client.emit recipient_txt
                             else
                               nil
                             end
 
-                            txt_received = "#{names_raw}" + " #{enactor.name}"
+                            txt_received = "#{recipient_names}" + " #{enactor.name}"
                             txt_received.slice! "#{recipient.name}"
 
                             recipient.update(txt_received: (txt_received.squish))
@@ -142,19 +138,19 @@ module AresMUSH
 
                 if names.empty?
                     self.scene_txt = t('txt.txt_to_scene_no_recipient',
-                    :txt => Txt.format_txt_indicator(enactor, names_raw),
+                    :txt => Txt.format_txt_indicator(enactor, recipient_names),
                     :sender => enactor.name,
                     :message => message )
                 else
                     self.scene_txt = t('txt.txt_to_scene_with_recipient',
-                    :txt => Txt.format_txt_indicator(enactor, names_raw),
+                    :txt => Txt.format_txt_indicator(enactor, recipient_names),
                     :sender => enactor.name,
-                    # :recipients => names_raw,
+                    # :recipients => recipient_names,
                     :message => message )
                 end
 
                 room_txt = t('txt.txt_to_recipient_with_scene',
-                :txt => Txt.format_txt_indicator(enactor, names_raw),
+                :txt => Txt.format_txt_indicator(enactor, recipient_names),
                 :sender => enactor.name,
                 :message => message,
                 :scene_id => scene_id)
@@ -162,7 +158,6 @@ module AresMUSH
                 Rooms.emit_ooc_to_room scene_room,room_txt
 
                 recipients.delete(enactor.name)
-
                 enactor.update(txt_last_scene: recipients)
 
                 Scenes.add_to_scene(scene, self.scene_txt, enactor)
