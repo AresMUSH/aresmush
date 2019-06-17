54f80573a114b5938651ed2ebbe03cfb59fc2415
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-06-17 10:15:32 -0400

    Update jobs_char.rb

--- plugins/jobs/public/jobs_char.rb
+++ plugins/jobs/public/jobs_char.rb
@@ -1,41 +1,42 @@
 module AresMUSH
   class Character
-    
+
     attribute :jobs_filter, :default => "ACTIVE"
     attribute :read_jobs, :type => DataType::Array, :default => []
 
     collection :jobs, "AresMUSH::Job", :author
-    
+
     before_delete :delete_job_participation
-    
+
     def has_unread_jobs?
       !unread_jobs.empty?
     end
-  
+
     def has_unread_requests?
       !unread_requests.empty?
-    end  
-    
+    end
+
     def assigned_jobs
       Job.find(assigned_to_id: self.id)
     end
-    
+
     def unread_jobs
+      return []
       return [] if !Jobs.can_access_jobs?(self)
       staff_jobs = Job.all.select { |j| !Jobs.check_job_access(self, j) && j.is_unread?(self) }
       their_jobs = self.unread_requests
-      
+
       return staff_jobs.concat(their_jobs).uniq
     end
-    
+
     def unread_requests
       self.requests.select { |r| r.is_unread?(self) }
     end
-    
+
     def requests
       self.jobs.to_a.concat(Job.all.select { |j| j.participants.include?(self) })
     end
-    
+
     def delete_job_participation
       Job.all.each do |j|
         Database.remove_from_set j.participants, self
