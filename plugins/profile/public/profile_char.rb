bc7af160a397e12faad02b3800635bb7fda18f0b
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-06-20 16:08:05 -0400

    not played

--- plugins/profile/public/profile_char.rb
+++ plugins/profile/public/profile_char.rb
@@ -1,6 +1,6 @@
 module AresMUSH
   class Character
-    attribute :profile, :type => DataType::Hash, :default => {}   
+    attribute :profile, :type => DataType::Hash, :default => {}
     attribute :relationships, :type => DataType::Hash, :default => {}
     attribute :relationships_category_order, :type => DataType::Array, :default => []
     attribute :profile_image
@@ -8,19 +8,20 @@ module AresMUSH
     attribute :profile_gallery, :type => DataType::Array, :default => []
     attribute :profile_last_edited, :type => DataType::Time
     attribute :profile_tags, :type => DataType::Array, :default => []
-    
+    attribute :unplayed, :type => DataType::Boolean, :default => false
+
     collection :profile_versions, "AresMUSH::ProfileVersion"
-    
+
     before_delete :delete_versions
-    
+
     def delete_versions
       self.profile_versions.each { |v| v.delete }
     end
-    
+
     def last_profile_version
       self.profile_versions.to_a.sort_by { |p| p.created_at }.reverse.first
     end
-    
+
     def build_profile_version
       profile_text = ""
       self.demographics.each { |k, v| profile_text << "\n#{k}: #{v}"}
@@ -29,7 +30,7 @@ module AresMUSH
       self.relationships.each { |k, v| profile_text << "\n\n#{k}: #{v['category']}\n------\n#{v['relationship']}"}
       profile_text
     end
-    
+
     def set_profile(new_profile, enactor)
       self.update(profile: new_profile)
       self.update(profile_last_edited: Time.now)
@@ -37,5 +38,5 @@ module AresMUSH
       Website.add_to_recent_changes('char', version.id, char.name)
     end
   end
-  
+
 end
