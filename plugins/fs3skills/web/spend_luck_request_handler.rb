6650ed9a9fa17c5cdc1979588c0d04b0447c1d3a
Linda Naughton <lynn@wordsmyth.org>
2019-06-22 23:08:57 -0400

    Luck from web.

---
+++ plugins/fs3skills/web/spend_luck_request_handler.rb
@@ -0,0 +1,34 @@
+module AresMUSH
+  module FS3Skills
+    class SpendLuckRequestHandler
+      def handle(request)
+        scene = Scene[request.args[:scene_id]]
+        enactor = request.enactor
+        reason = request.args[:reason]
+        
+        if (!scene)
+          return { error: t('webportal.not_found') }
+        end
+        
+        error = Website.check_login(request)
+        return error if error
+
+        if (!Scenes.can_read_scene?(enactor, scene))
+          return { error: t('scenes.access_not_allowed') }
+        end
+        
+        if (scene.completed)
+          return { error: t('scenes.scene_already_completed') }
+        end
+        
+        if (enactor.luck < 1)
+          return { error: t('fs3skills.not_enough_points') }
+        end
+        FS3Skills.spend_luck(enactor, reason, scene)
+       
+        {
+        }
+      end
+    end
+  end
+end
