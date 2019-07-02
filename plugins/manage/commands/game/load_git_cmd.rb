f5c0c79987f7ab95e499932ce28235de0b95e90d
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-07-02 15:36:28 -0400

    Update load_git_cmd.rb

--- plugins/manage/commands/game/load_git_cmd.rb
+++ plugins/manage/commands/game/load_git_cmd.rb
@@ -2,14 +2,14 @@ module AresMUSH
   module Manage
     class LoadGitCmd
       include CommandHandler
-      
+
       def check_can_manage
         return t('dispatcher.not_allowed') if !Manage.can_manage_game?(enactor)
         return nil
       end
-      
+
       def handle
-        output = `git load 2>&1`
+        output = `git pull 2>&1`
         client.emit_success t('manage.git_output', :output => output)
         client.emit_ooc t('manage.load_all')
         error = Manage.load_all
@@ -19,7 +19,7 @@ module AresMUSH
           client.emit_success t('manage.load_all_complete')
         end
       end
-      
+
     end
   end
 end
