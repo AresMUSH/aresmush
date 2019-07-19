1529b11e206291494754d9d5d29d8a5b9f7dee62
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-07-19 10:32:43 -0400

    Add ST to lore hook prefs

--- plugins/lorehooks/commands/lore_hook_pref_cmd.rb
+++ plugins/lorehooks/commands/lore_hook_pref_cmd.rb
@@ -14,22 +14,14 @@ module AresMUSH
       end
 
       def handle
+        preferences = ["None", "Item", "Pet", "Ancestry", "Storyteller"]
         if self.target
           client.emit_success "#{self.target.name}'s Lore Hook Preference: #{self.target.lore_hook_pref}"
-        elsif self.preference == "None"
-          enactor.update(lore_hook_pref: self.preference)
-          client.emit_success "You have set your Lore Hook preference to #{self.preference}"
-        elsif self.preference == "Item"
-          enactor.update(lore_hook_pref: self.preference)
-          client.emit_success "You have set your Lore Hook preference to #{self.preference}"
-        elsif self.preference == "Pet"
-          enactor.update(lore_hook_pref: self.preference)
-          client.emit_success "You have set your Lore Hook preference to #{self.preference}"
-        elsif self.preference == "Ancestry"
+        elsif preferences.include?(self.preference)
           enactor.update(lore_hook_pref: self.preference)
           client.emit_success "You have set your Lore Hook preference to #{self.preference}"
         else
-          client.emit_failure "That is not a Lore Hook preference. Use none, item, pet, or ancestry."
+          client.emit_failure "That is not a Lore Hook preference. Use one of the followig: #{preferences.join(", ")}."
         end
       end
 
