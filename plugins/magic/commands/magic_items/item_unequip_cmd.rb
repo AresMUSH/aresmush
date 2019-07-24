669b344091811d545239f07ff3dfee45c8654e7d
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-07-24 16:03:29 -0400

    Update item_unequip_cmd.rb

--- plugins/magic/commands/magic_items/item_unequip_cmd.rb
+++ plugins/magic/commands/magic_items/item_unequip_cmd.rb
@@ -3,20 +3,20 @@ module AresMUSH
     class ItemUnequipCmd
     #item/equip <item>
       include CommandHandler
-      attr_accessor :caster, :caster_name, :item_name
+      attr_accessor  :item_name
 
       def parse_args
         self.item_name = titlecase_arg(cmd.args)
       end
 
       def check_errors
-        return t('magic.item_not_equipped') if item_name != caster.magic_item_equipped
+        return t('magic.item_not_equipped') if item_name != enactor.magic_item_equipped
       end
 
       def handle
-        caster.update(magic_item_equipped: "None")
+        enactor.update(magic_item_equipped: "None")
         client.emit_success t('magic.item_unequipped', :item => self.item_name)
-        enactor_room.emit t('magic.has_unequipped_item', :name => self.caster.name, :item => self.item_name)
+        enactor_room.emit t('magic.has_unequipped_item', :name => self.enactor.name, :item => self.item_name)
       end
 
 
