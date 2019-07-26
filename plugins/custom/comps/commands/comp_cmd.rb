32ddd520f4480a4384e0ac07d13214785774ecf1
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-07-26 17:02:36 -0400

    Comp scenes

--- plugins/custom/comps/commands/comp_cmd.rb
+++ plugins/custom/comps/commands/comp_cmd.rb
@@ -3,33 +3,58 @@ module AresMUSH
     class CompGiveCmd
       #comp <name>=<text>
       include CommandHandler
-      attr_accessor :targets, :comp, :target_names
+      attr_accessor :targets, :comp, :target_names, :scene_id, :scene
 
       def parse_args
        args = cmd.parse_args(ArgParser.arg1_equals_arg2)
-       self.target_names = args.arg1.split(" ").map { |n| InputFormatter.titlecase_arg(n) }
 
-       self.targets = []
-        self.target_names.each do |name|
-         target = Character.named(name)
-         return  t('custom.invalid_name') if !target
-         self.targets << target
+       if args.arg1.is_integer?
+         self.scene_id = args.arg1
+         client.emit "Number"
+       else
+        self.target_names = args.arg1.split(" ").map { |n| InputFormatter.titlecase_arg(n) }
+        self.targets = []
+         self.target_names.each do |name|
+          target = Character.named(name)
+          return  t('custom.invalid_name') if !target
+          self.targets << target
+        end
        end
+
+
        #
        # self.target = Character.named(args.arg1)
        self.comp = args.arg2
       end
 
+      def check_errors
+        self.scene = Scene[scene_id]
+        return "That is not a scene number." if (self.scene_id && !scene)
+      end
+
       def handle
         date = Time.now.strftime("%Y-%m-%d")
-        targets.each do |target|
-          Comps.create(date: date, character: target, comp_msg: self.comp, from: enactor.name)
-          FS3Skills.modify_luck(target, 0.05)
-          message = t('custom.has_left_comp', :from => enactor.name)
-          Login.emit_if_logged_in target, message
+        if self.scene_id
+          self.target_names = []
+          self.scene.participants.each do |target|
+            Comps.create(date: date, character: target, comp_msg: self.comp, from: enactor.name)
+            FS3Skills.modify_luck(target, 0.05)
+            message = t('custom.has_left_comp', :from => enactor.name)
+            Login.emit_if_logged_in target, message
+            self.target_names << target.name
+          end
+          client.emit_success t('custom.left_comp', :name =>  self.target_names.join(", "))
+        else
+          targets.each do |target|
+            Comps.create(date: date, character: target, comp_msg: self.comp, from: enactor.name)
+            FS3Skills.modify_luck(target, 0.05)
+            message = t('custom.has_left_comp', :from => enactor.name)
+            Login.emit_if_logged_in target, message
+          end
+          client.emit_success t('custom.left_comp', :name =>  self.target_names.join(", "))
         end
 
-        client.emit_success t('custom.left_comp', :name =>  self.target_names.join(", "))
+
 
 
 
