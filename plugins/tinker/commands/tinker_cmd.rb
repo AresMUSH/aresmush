8092cd13c652eee38d4907e532d030cd3e44c953
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-08-12 15:12:02 -0400

    Update tinker_cmd.rb

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -2,14 +2,19 @@ module AresMUSH
   module Tinker
     class TinkerCmd
       include CommandHandler
-      
+
       def check_can_manage
         return t('dispatcher.not_allowed') if !enactor.has_permission?("tinker")
         return nil
       end
-      
+
       def handle
-        client.emit_success "Done!"
+        client.emit "Lore Hooks All Characters"
+        chars = Chargen.approved_chars
+        chars.each do |c|
+
+          client.emit "#{c.name}: #{c.lore_hook_type} - #{c.lore_hook_name} %R#{c.lore_hook_desc}"
+        end
       end
 
     end
