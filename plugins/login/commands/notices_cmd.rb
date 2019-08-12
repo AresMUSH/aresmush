5ae40848af592dac9fe48d50bf0c539570825613
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-08-12 09:37:01 -0400

    Update notices_cmd.rb

--- plugins/login/commands/notices_cmd.rb
+++ plugins/login/commands/notices_cmd.rb
@@ -4,18 +4,18 @@ module AresMUSH
       include CommandHandler
 
       def handle
-        
-        paginator = Paginator.paginate(enactor.login_notices.to_a.sort_by { |n| [ n.is_unread ? 1 : 0, n.created_at] }.reverse, cmd.page, 20)
+
+        paginator = Paginator.paginate(enactor.login_notices.to_a.sort_by { |n| [ n.is_unread ? 1 : 0, n.created_at] }.reverse, cmd.page, 10)
         if (paginator.out_of_bounds?)
           client.emit_failure paginator.out_of_bounds_msg
           return
         end
-        
+
         template = NoticesTemplate.new(enactor, paginator)
         client.emit template.render
-        
+
         #enactor.unread_notifications.each { |n| n.update(is_unread: false)}
-        
+
       end
     end
   end
