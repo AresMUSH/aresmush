cf2073f3a054cc0780621360f7e7f2001166b6ef
Linda Naughton <lynn@wordsmyth.org>
2019-08-17 19:57:03 -0400

    Config defaults.

--- install/migrations/0043_beta61_update.rb
+++ install/migrations/0043_beta61_update.rb
@@ -11,6 +11,12 @@ module AresMUSH
         config = DatabaseMigrator.read_config_file("idle.yml")
         config['idle']['roster_arrival_msg'] = "%{name} has been claimed off the roster.\n\nPosition: %{position}\n\nRP Hooks:\n%{rp_hooks}"
         config = DatabaseMigrator.write_config_file("idle.yml", config)
+        
+        Global.logger.debug "Two word demo shortcuts."
+        config = DatabaseMigrator.read_config_file("demographics.yml")
+        config['demographics']['shortcuts']['fullname'] = 'demographic/set full name='
+        config['demographics']['shortcuts']['playedby'] = 'demographic/set played by='          
+        config = DatabaseMigrator.write_config_file("demographics.yml", config)
       end 
     end
   end
