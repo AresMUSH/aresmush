710ad0f012bb93c58348e9dc7e020fa1ca0e0e8a
Obsidian Reverie <unconfigured@null.spigotmc.org>
2019-08-30 23:35:22 -0400

    Update fs3_attribute.rb

--- plugins/fs3skills/public/fs3_attribute.rb
+++ plugins/fs3skills/public/fs3_attribute.rb
@@ -20,9 +20,9 @@ module AresMUSH
       when 3
         return "%xg@%xy@%xr@%xn"
       when 4
-        return "%xg@%xy@%xy@%xy@%xn"
+        return "%xy@%xy@%xy@%xy@%xn"
 	  when 5
-        return "%xg@%xb@%xb@%xb@%xb@%xn"
+        return "%xb@%xb@%xb@%xb@%xb@%xn"
       end
     end
     
