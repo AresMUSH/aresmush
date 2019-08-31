85e543c67f9c8d5fd6203d444b1dba8941ff01fb
Linda Naughton <lynn@wordsmyth.org>
2019-08-31 18:51:53 -0400

    Notifications when event signup added or removed.

--- plugins/events/helpers.rb
+++ plugins/events/helpers.rb
@@ -48,6 +48,12 @@ module AresMUSH
         signup.update(comment: comment)
       else
         EventSignup.create(event: event, character: char, comment: comment)
+        organizer = event.character
+        if (organizer)
+          message = t('events.signup_added', :title => event.title, :name => char.name)
+          Login.notify(organizer, :event, message, event.id)
+          Login.emit_ooc_if_logged_in organizer, message
+        end
       end
     end
     
@@ -128,7 +134,15 @@ module AresMUSH
         return t('events.not_signed_up', :name => name)
       end
       
+      organizer = event.character
+      if (organizer)
+        message = t('events.signup_removed', :title => event.title, :name => signup.character.name)
+        Login.notify(organizer, :event, message, event.id)
+        Login.emit_ooc_if_logged_in organizer, message
+      end
+      
       signup.delete
+      
       return nil
     end
   end
