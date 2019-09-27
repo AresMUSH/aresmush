2e64f63b0e8fcb0ec661d221d2caceedfdf778bb
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-09-27 09:46:12 -0400

    Weather

--- plugins/openweather/helpers.rb
+++ plugins/openweather/helpers.rb
@@ -1,17 +1,17 @@
 module AresMUSH
     module Openweather
       mattr_accessor :current_weather
-  
+
       def self.can_change_weather?(actor)
         actor.has_permission?("manage_weather")
       end
-  
+
       def self.load_weather_if_needed
         if (Openweather.current_weather == {})
           Openweather.change_all_weathers
         end
       end
-  
+
       def self.change_all_weathers
         # Set an initial weather for each area and the default one
         Openweather.current_weather = {}
@@ -23,17 +23,17 @@ module AresMUSH
             end
           end
       end
-  
+
       def self.change_weather(area)
         # Figure out the climate for this area
         climate = Openweather.climate_for_area(area)
-  
+
         # Save no weather if the weather is disabled for this area.
         if (climate == "none")
           Openweather.current_weather[area] = ""
           return
         end
-  
+
         oweather = Weather.new
         oweather.api_key = Global.read_config('secrets', 'openweather_api_key')
         oweather.units = Global.read_config("openweather", "units")
@@ -47,11 +47,20 @@ module AresMUSH
         end
 
         season = ICTime.season(area)
-  
+        if season == "eary-fall"
+          season = "fall"
+        elsif season == "early-winter"
+          season = "winter"
+        elsif season == "early-spring"
+          season = "spring"
+        elsif season == "early-summer"
+          season = "summer"
+        end
+
         if (!response)
           raise "Could not call API climate for #{climate}."
         end
-        
+
         # Save the weather!
         Openweather.current_weather[area.titlecase] = response.weather
       end
@@ -61,7 +70,7 @@ module AresMUSH
 
         climate = area_climates.select { |k, v| k.downcase == area_name.downcase }.values.first
         return climate if climate
-  
+
         area = Area.find_one_by_name(area_name)
         if (area && area.parent)
           return Openweather.climate_for_area(area.parent.name)
