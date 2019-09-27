f9197e5c58567a38d6fb947f9ad5bf183751e828
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-09-27 09:13:46 -0400

    Adding open weather

--- plugins/describe/room_desc_builder.rb
+++ plugins/describe/room_desc_builder.rb
@@ -9,25 +9,25 @@ module AresMUSH
           desc << " "
           desc << room.vistas[time_of_day]
         end
-        
+
         season = ICTime.season(room.area_name).titleize
         if (room.vistas && room.vistas.has_key?(season))
           desc << " "
           desc << room.vistas[season]
         end
-        
+
         weather = weather(room)
         if (weather)
           desc << " "
           desc << weather
         end
-        
+
         desc
       end
-    
+
       def self.weather(room)
-        return nil if !AresMUSH::Weather.is_enabled? 
-        w = Weather.weather_for_area(room.area_name)
+        return nil if !AresMUSH::Openweather.is_enabled?
+        w = Openweather.weather_for_area(room.area_name)
         w ? "%R%R#{w}" : nil
       end
     end
