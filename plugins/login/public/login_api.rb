2b2b4874794480b4a0ec2c31258423d9059db52d
Linda Naughton <lynn@wordsmyth.org>
2019-10-13 14:27:40 -0400

    Remvoe tos used flag.

--- plugins/login/public/login_api.rb
+++ plugins/login/public/login_api.rb
@@ -2,17 +2,14 @@ module AresMUSH
   module Login
 
     def self.terms_of_service
-      use_tos = Global.read_config("login", "use_terms_of_service") 
-      return nil if !use_tos
-      
       begin
         tos_filename = "game/text/tos.txt"
         tos_text = File.read(tos_filename, :encoding => "UTF-8")
       rescue Exception => ex
         Global.logger.warn "Can't read terms of service file: #{ex}"
-        tos_text = t('login.cant_read_tos_text')
+        tos_text = ""
       end
-      return tos_text
+      return tos_text.blank? ? nil : tos_text
     end
     
     # Checks to see if either the IP or hostname is a match with the specified string.
