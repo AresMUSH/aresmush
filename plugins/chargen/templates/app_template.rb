225f121009a8b7ddfc4e5886ddcc766734fa9919
Linda Naughton <lynn@wordsmyth.org>
2019-10-21 22:46:16 -0400

    Integrate app review for custom cg systems.

--- plugins/chargen/templates/app_template.rb
+++ plugins/chargen/templates/app_template.rb
@@ -15,8 +15,31 @@ module AresMUSH
         "%x!%xh#{title}%xH%xn%r"
       end
       
+      def show_abilities
+        ability_system = Global.read_config("chargen", "ability_system")
+        return false if ability_system.blank?
+        return FS3Skills.is_enabled? if ability_system == 'fs3'
+        return true
+      end
+      
       def abilities
-        FS3Skills.app_review(@char)
+        ability_system = Global.read_config("chargen", "ability_system")
+        return nil if ability_system.blank?
+        
+        case ability_system
+        when "fs3"
+          FS3Skills.app_review(@char)
+        else
+          plugin_module = Global.plugin_manager.plugins.select { |p| "#{p}".upcase == "ARESMUSH::#{ability_system.upcase}"}.first
+          if (!plugin_module)
+            raise "Invalid ability system configured.  No plugin found for #{ability_system}."
+          end
+          plugin_module.app_review(@char)
+        end
+      end
+      
+      def abilities_header
+        Global.read_config("chargen", "ability_system_app_review_header")
       end
       
       def demographics
