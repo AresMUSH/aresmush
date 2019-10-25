61ad30e753f8a6c3fb2eb034e908bc4adefeb592
gaslighter <56846642+gaslighter@users.noreply.github.com>
2019-10-25 15:58:42 -0700

    Update custom.rb

--- plugins/custom/custom.rb
+++ plugins/custom/custom.rb
@@ -10,18 +10,17 @@ module AresMUSH
       Global.read_config("custom", "shortcuts")
     end
  
-    def self.get_cmd_handler(client, cmd, enactor) 
-    case cmd.root
-    when "fatigue"
-      case cmd.switch
-      when "lower"
-        return FatigueLowerCmd    
-      when "add"
-        return FatigueAddCmd     
-      else
-        return FatigueCmd
-      end
+  def self.get_cmd_handler(client, cmd, enactor)
+  case cmd.root
+  when "fatigue"
+    case cmd.switch
+    when "lower"
+      return FatigueLowerCmd
+    when "add"
+      return FatigueAddCmd
+    else
+      return FatigueCmd
     end
   end
-  nil
+  return nil
 end
