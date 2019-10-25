2cd36de724d5f786e7ba87030da2113520ba2e9d
gaslighter <56846642+gaslighter@users.noreply.github.com>
2019-10-25 15:47:14 -0700

    Create fatigue_lower.rb

---
+++ plugins/custom/fatigue_lower.rb
@@ -0,0 +1,30 @@
+module AresMUSH
+  module Custom
+    class FatigueAddCmd
+      include CommandHandler
+
+      attr_accessor :name
+
+      def parse_args
+        self.name = !cmd.args ? enactor : Character.find_one_by_name(cmd.args)
+      end
+      
+      def required_args
+        [ self.name ]
+      end
+     
+      def handle
+        if (!enactor.is_admin?)
+          client.emit_failure("Submit a request to staff.")
+          return nil
+        end
+
+        current_fatigue = name.fatigue
+        new_fatigue = current_fatigue.to_i + 1
+        name.update(fatigue: new_fatigue)
+        client.emit_ooc("Added 1 to #{name.name}'s Fatigue.  Now at: #{new_fatigue} / 7.")
+        Login.emit_ooc_if_logged_in(name, "#{enactor.name} added one to your fatigue level.  Now at: #{new_fatigue} / 7.")
+      end
+    end
+  end
+end
