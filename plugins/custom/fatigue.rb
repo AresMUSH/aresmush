78cf581043dabf3e479485de11cfc860f93649cc
gaslighter <56846642+gaslighter@users.noreply.github.com>
2019-10-25 15:38:40 -0700

    Create fatigue.rb

---
+++ plugins/custom/fatigue.rb
@@ -0,0 +1,22 @@
+module AresMUSH
+  module Custom
+    class FatigueCmd
+      include CommandHandler
+
+      attr_accessor :name
+
+      def parse_args
+        self.name = !cmd.args ? enactor : Character.find_one_by_name(cmd.args)
+      end
+      
+      def required_args
+        [ self.name ]
+      end
+     
+      def handle
+        fatigue = name.fatigue
+        client.emit_ooc("#{name.name}'s Fatigue: #{fatigue} / 7")
+      end
+    end
+  end
+end
