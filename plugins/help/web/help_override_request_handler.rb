fd42e29a9b512cd9a121b0b2f7aea3c955b3acf3
Linda Naughton <lynn@wordsmyth.org>
2019-11-01 20:18:09 -0400

    Customizable help files.

---
+++ plugins/help/web/help_override_request_handler.rb
@@ -0,0 +1,31 @@
+module AresMUSH
+  module Help
+    class HelpOverrideRequestHandler
+      def handle(request)
+        topic_id = request.args[:topic]
+        contents = request.args[:contents]
+        
+        enactor = request.enactor
+        
+        error = Website.check_login(request)
+        return error if error
+
+        if (!Manage.can_manage_game?(enactor))
+          return { error: t('dispatcher.not_allowed') }
+        end
+      
+        help_dir = File.join(AresMUSH.game_path, "help", "en")
+        FileUtils.mkpath(help_dir) unless File.exists?(help_dir)
+        path =  File.join(help_dir, "#{topic_id}.md")
+        
+        
+        File.open(path, "wb") { |f| f.write(contents) }
+        Help.reload_help
+        
+        {
+          topic: topic_id
+        }
+      end
+    end
+  end
+end
