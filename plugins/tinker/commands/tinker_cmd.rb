574da181f3ae0f68d5b64dbce65be40f285c9148
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-11-15 11:42:33 -0500

    Update tinker_cmd.rb

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -3,27 +3,19 @@ module AresMUSH
     class TinkerCmd
       include CommandHandler
 
+
       def check_can_manage
         return t('dispatcher.not_allowed') if !enactor.has_permission?("tinker")
         return nil
       end
-      
+
       def handle
         Character.all.each do |c|
-          c.achievements.each do |achievement|
-            # if (achievement.name =~ /has_died_/)
-            #   count = achievement.name.split('_').last.to_i
-            #   Achievements.award_achievement(c, 'has_died', count)
-            #   achievement.delete
-            # end
-
-            if (achievement.name =~ /gave_comps_/)
-              count = achievement.name.split('_').last.to_i
-              Achievements.award_achievement(c, 'gave_comps', count)
-              achievement.delete
-            end
-          end
+          puts "Starting XP: #{c.xp}"
+          FS3Skills.modify_xp(c, 1)
+          puts "Ending XP: #{c.xp}"
         end
+
       end
 
     end
