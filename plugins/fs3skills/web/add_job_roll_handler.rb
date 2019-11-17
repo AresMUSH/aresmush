f5334b9ae97e6270e5ec0060bd9d6b38a46ac87c
Linda Naughton <lynn@wordsmyth.org>
2019-11-17 12:49:09 -0500

    Privacy policy and help tweaks.

---
+++ plugins/fs3skills/web/add_job_roll_handler.rb
@@ -0,0 +1,34 @@
+module AresMUSH
+  module FS3Skills
+    class AddJobRollRequestHandler
+      def handle(request)
+        job = Job[request.args[:id]]
+        enactor = request.enactor
+        
+        if (!job)
+          return { error: t('webportal.not_found') }
+        end
+        
+        error = Website.check_login(request)
+        return error if error
+
+        if (!Jobs.can_access_job?(enactor, job, true))
+          return { error: t('jobs.cant_view_job') }
+        end
+        
+        if (!job.is_open?)
+          return { error: t('jobs.job_already_closed') }
+        end
+        
+        result = FS3Skills.determine_web_roll_result(request, enactor)
+        
+        return result if result[:error]
+
+        Jobs.comment(job, enactor, result[:message], false)
+        
+        {
+        }
+      end
+    end
+  end
+end
