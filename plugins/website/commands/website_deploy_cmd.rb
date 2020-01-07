2fffec2303f1f35931f8572e4ae7b0e2cd16eaa7
Linda Naughton <lynn@wordsmyth.org>
2020-01-06 21:07:01 -0500

    Wikidot link didn't handle italics.  Individual lines in log view.  Website deploy permissions fixed.

--- plugins/website/commands/website_deploy_cmd.rb
+++ plugins/website/commands/website_deploy_cmd.rb
@@ -2,7 +2,12 @@ module AresMUSH
   module Website
     class WebsiteDeployCmd
       include CommandHandler
-            
+           
+      def check_can_manage
+        return t('dispatcher.not_allowed') if !Manage.can_manage_game?(enactor)
+        return nil
+      end
+                  
       def handle
         client.emit_ooc t('webportal.redeploying_website')
         Website.redeploy_portal(enactor, false)
