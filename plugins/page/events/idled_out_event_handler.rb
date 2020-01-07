0e3d37118c92b2cda94ca3545a22e781d539d5a0
Linda Naughton <lynn@wordsmyth.org>
2020-01-06 22:38:32 -0500

    Clear mail on idle out.

--- plugins/page/events/idled_out_event_handler.rb
+++ plugins/page/events/idled_out_event_handler.rb
@@ -2,6 +2,9 @@ module AresMUSH
   module Page
     class CharIdledOutEventHandler
       def on_event(event)
+        # No need to reset if they're getting destroyed.
+        return if event.is_destroyed?
+
         char = Character[event.char_id]
         char.delete_pages
       end
