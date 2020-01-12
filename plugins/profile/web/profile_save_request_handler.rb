459fcca8bda44c7cedd9e94ef99236ec61458326
Linda Naughton <lynn@wordsmyth.org>
2020-01-12 13:39:56 -0500

    More custom scaffolding.

--- plugins/profile/web/profile_save_request_handler.rb
+++ plugins/profile/web/profile_save_request_handler.rb
@@ -56,7 +56,7 @@ module AresMUSH
         relation_category_order = (request.args[:relationships_category_order] || "").split(',')
         char.update(relationships_category_order: relation_category_order)
         
-        CustomCharFields.save_fields_from_profile_edit(char, enactor, request.args)
+        CustomCharFields.save_fields_from_profile_edit(char, request.args)
         
         ## DO PROFILE LAST SO IT TRIGGERS THE SOURCE HISTORY UPDATE
         profile = {}
