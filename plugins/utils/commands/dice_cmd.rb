e8330b711b53b849d3113d7381f0350d4c65d9a0
Linda Naughton <lynn@wordsmyth.org>
2020-01-18 16:05:28 -0500

    Dice added to scene.  Dice web request.  forum unhide fix.  FS3Skills NPC roll.

--- plugins/utils/commands/dice_cmd.rb
+++ plugins/utils/commands/dice_cmd.rb
@@ -11,32 +11,26 @@ module AresMUSH
         self.num = args.num.to_i 
         self.sides = args.sides.to_i
         self.private_roll = cmd.switch_is?("private")
-        
-        if (self.num == 0)
-          self.num = 1
-        end
       end
       
       def required_args
         [ self.num, self.sides ]
       end
       
-      def check_too_many_dice
-        return t('dice.too_many_dice') if self.num > 10
-        return t('dice.no_zero_dice') if self.num <= 0 || self.sides == 0
-        return nil
-      end
-      
       def handle
-        results = self.num.times.collect { |d| rand(self.sides) + 1 }
-        message	= t('dice.rolls_dice', :name => enactor_name,
-        :dice => "#{num}d#{sides}",
-        :results => results)
-
+        message = Utils.roll_dice(enactor_name, self.num, self.sides)
+        if (!message)
+          client.emit_failure t('dice.invalid_dice_string')
+          return
+        end
+        
         if (self.private_roll)
           client.emit_ooc message
         else
           enactor_room.emit_ooc message
+          if (enactor_room.scene)
+            Scenes.add_to_scene(enactor_room.scene, message, Game.master.system_character, false, true)
+          end
         end
       end
       
