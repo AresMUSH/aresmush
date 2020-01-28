0e9c1ec097c821e6ba5a2e6a77b039b7e27053cb
spiritlake <32268357+spiritlake@users.noreply.github.com>
2020-01-28 14:58:26 -0500

    Update tinker_cmd.rb

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -10,11 +10,23 @@ module AresMUSH
       end
 
       def handle
-        Character.all.each do |c|
-          puts "Starting XP: #{c.xp}"
-          FS3Skills.modify_xp(c, 1)
-          puts "Ending XP: #{c.xp}"
-        end
+      #   Character.all.each do |c|
+      #     puts "Starting XP: #{c.xp}"
+      #     FS3Skills.modify_xp(c, 1)
+      #     puts "Ending XP: #{c.xp}"
+      #   end
+
+      enactor = enactor
+
+      all_chars = AresCentral.alts_of(enactor)
+      puts "CHARS: #{all_chars}"
+
+      my_scenes = []
+      all_chars.each do |char|
+        char_scenes = Scene.all.select { |s| !s.completed && Scenes.is_watching?(s, char) }.sort_by { |s| s.id }
+        my_scenes.concat char_scenes
+      end
+      puts "SCENES #{my_scenes}"
 
       end
 
