651f7d127502511845435ec49e09d12f029562c5
Linda Naughton <lynn@wordsmyth.org>
2020-02-20 08:19:54 -0500

    Roster improvements.

--- plugins/idle/web/claim_roster_request_handler.rb
+++ plugins/idle/web/claim_roster_request_handler.rb
@@ -3,13 +3,19 @@ module AresMUSH
     class ClaimRosterRequestHandler
       def handle(request)
                 
+        enactor = request.enactor
+
+        error = Website.check_login(request, true)
+        return error if error
+                        
         char = Character[request.args[:id]]
+        app = Website.format_input_for_mush((request.args[:app] || ""))
         
         if (!char)
           return { error: t('webportal.not_found') }
         end
         
-        Idle.claim_roster(char)        
+        Idle.claim_roster(char, enactor, app)    
       end
     end
   end
