4eccfc98b1daa7766ce1da665dafb4bdbe6881d6
spiritlake <32268357+spiritlake@users.noreply.github.com>
2020-02-21 09:26:08 -0500

    Update idle_set_cmd.rb

--- plugins/idle/commands/idle_set_cmd.rb
+++ plugins/idle/commands/idle_set_cmd.rb
@@ -3,33 +3,33 @@ module AresMUSH
   module Idle
     class IdleSetCmd
       include CommandHandler
-      
+
       attr_accessor :name, :status
-      
+
       def parse_args
         args = cmd.parse_args(ArgParser.arg1_equals_arg2)
         self.name = titlecase_arg(args.arg1)
         self.status = titlecase_arg(args.arg2)
       end
-      
+
       def required_args
         [ self.name, self.status ]
       end
-      
-      
+
+
       def check_can_manage
         return nil if Idle.can_idle_sweep?(enactor)
         return t('dispatcher.not_allowed')
       end
-      
+
       def check_action
-        actions = [ 'None', 'Dead', 'Gone', 'Npc' ]
+        actions = [ 'None', 'Dead', 'Gone', 'Npc', 'Unplayed' ]
         return t('idle.use_roster_cmd') if self.status == 'Roster'
-        
+
         return t('idle.invalid_action', :actions => actions.join(" ")) if !actions.include?(self.status)
         return nil
       end
-            
+
       def handle
         ClassTargetFinder.with_a_character(self.name, client, enactor) do |model|
 
@@ -41,7 +41,7 @@ module AresMUSH
             client.emit_failure t('idle.use_npc_cmd')
             return
           end
-          
+
           if (self.status == 'None')
             model.update(idle_state: nil)
           elsif (self.status == 'Npc')
