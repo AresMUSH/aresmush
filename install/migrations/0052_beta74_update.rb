bfa22db32cb43d3cebd1495c08edd4d18940f02d
Linda Naughton <lynn@wordsmyth.org>
2020-02-29 13:05:06 -0500

    Migration updates.

--- install/migrations/0052_beta74_update.rb
+++ install/migrations/0052_beta74_update.rb
@@ -9,10 +9,12 @@ module AresMUSH
       def migrate
         Global.logger.debug "Add discord secret config."
         config = DatabaseMigrator.read_config_file("secrets.yml")
-        config['secrets']['discord'] = {
-          'api_token' =>  '',
-          'webhooks' => []
-        }
+        if (!config['secrets']['discord'])
+          config['secrets']['discord'] = {
+            'api_token' =>  '',
+            'webhooks' => []
+          }
+        end
         DatabaseMigrator.write_config_file("secrets.yml", config)
         
         Global.logger.debug "Add discord options."
@@ -31,6 +33,8 @@ module AresMUSH
         config['idle']['roster_app_template'] = "Tell us why you want to play this character:\n\n\nIf you're not submitting this from an existing character, provide an email where we can get back to you:"
         config['idle']['roster_app_category'] = 'APP'
         config['idle']['restrict_roster'] = false
+        config['idle']['monthly_reminder_cron']['day'] = config['idle']['monthly_reminder_cron']['date']
+        config['idle']['monthly_reminder_cron'].delete 'date'
         DatabaseMigrator.write_config_file("idle.yml", config)
         
         Global.logger.debug "Add job status filter config."
@@ -38,6 +42,12 @@ module AresMUSH
         config['jobs']['status_filters'] = {}
         DatabaseMigrator.write_config_file("jobs.yml", config)
         
+        Global.logger.debug "Set target defense skill."
+        config = DatabaseMigrator.read_config_file("fs3combat_misc.yml")
+        if (config['fs3combat']['combatant_types']['Target'])
+          config['fs3combat']['combatant_types']['Target']['defense_skill'] = 1
+        end
+        DatabaseMigrator.write_config_file("fs3combat_misc.yml", config)
         
       end 
     end
