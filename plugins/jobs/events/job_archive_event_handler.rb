1246d8124983493be893deb4d455a750602cee74
Linda Naughton <lynn@wordsmyth.org>
2020-04-22 21:17:26 -0400

    Job archive does closed status config properly.

--- plugins/jobs/events/job_archive_event_handler.rb
+++ plugins/jobs/events/job_archive_event_handler.rb
@@ -8,10 +8,16 @@ module AresMUSH
         Global.logger.debug "Archiving jobs."
         
         archive_job_days = Global.read_config('jobs', 'archive_job_days') || 10
-        old_jobs = Job.find(status: 'DONE').each do |j|
-          time_since_close = Time.now - (j.date_closed || Time.new(2018))
-          if (time_since_close > (86400 * archive_job_days)) 
-            j.update(status: 'ARCHIVED')
+        archived_status = (Global.read_config('jobs', 'archived_status') || "ARCHIVED").upcase
+        
+        (Global.read_config('jobs', 'closed_statuses') || []).each do |status|
+          next if status == archived_status
+          
+          old_jobs = Job.find(status: status).each do |j|
+            time_since_close = Time.now - (j.date_closed || Time.new(2018))
+            if (time_since_close > (86400 * archive_job_days)) 
+              j.update(status: archived_status)
+            end
           end
         end
       end
