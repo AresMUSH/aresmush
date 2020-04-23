7878a1cf4339424e344eddf426f937680036891f
spiritlake <32268357+spiritlake@users.noreply.github.com>
2020-04-23 16:23:17 -0400

    Update helpers.rb

--- plugins/profile/helpers.rb
+++ plugins/profile/helpers.rb
@@ -4,33 +4,35 @@ module AresMUSH
       relations = char.relationships.group_by { |name, data| data['category'] }
       relations.sort_by { |category, relations| Profile.category_order(char, category) }
     end
-    
+
     def self.category_order(char, category)
       char.relationships_category_order.map { |r| r.upcase }.index(category.upcase) || (category[0] || "1").ord
     end
-    
+
     def self.can_manage_char_profile?(actor, char)
       return false if !actor
       return true if actor.is_admin?
       return true if actor == char
-      
+
       return AresCentral.is_alt?(actor, char)
     end
-    
+
     def self.character_page_folder(char)
       Website::FilenameSanitizer.sanitize(char.name.downcase)
     end
-    
+
     def self.character_page_files(char)
       name = Profile.character_page_folder(char)
       Dir[File.join(AresMUSH.website_uploads_path, "#{name}/**")].sort
     end
-    
+
     def self.get_profile_status_message(char)
       case char.idle_state
       when "Roster"
         return t('profile.char_on_roster')
       when "Gone"
+        return t('profile.char_gone')
+      when "Unplayed"
         return t('profile.char_gone')
       when "Dead"
         return t('profile.char_dead')
