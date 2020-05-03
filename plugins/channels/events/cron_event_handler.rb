f076d24b9719be788e18481e6f4e9668867fb3dd
Linda Naughton <lynn@wordsmyth.org>
2020-05-03 18:08:13 -0400

    Channel chat looks more like scenes.

--- plugins/channels/events/cron_event_handler.rb
+++ plugins/channels/events/cron_event_handler.rb
@@ -7,12 +7,8 @@ module AresMUSH
         
         Global.logger.debug "Clearing channel history."
         
-        timeout_secs = (Global.read_config("channels", "recall_message_timeout_days") || 1) * 86400
-        Channel.all.each do |c|
-          messages = c.messages.select { |m| m['timestamp'] && (Time.now - Time.parse(m['timestamp']) < timeout_secs ) }
-          c.update(messages: messages)
-        end
-        
+        timeout_secs = (Global.read_config("channels", "recall_timeout_days") || 1) * 86400
+        ChannelMessage.all.select { |c| Time.now - c.created_at > timeout_secs }.each { |c| c.delete }
       end
     end
   end
