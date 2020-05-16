52194a0625ccba50bc46f5be3b1f7deee621a98a
Linda Naughton <lynn@wordsmyth.org>
2020-05-16 13:55:24 -0400

    Placeholder migration to force restart.

---
+++ install/migrations/0056_beta78_update.rb
@@ -0,0 +1,13 @@
+module AresMUSH  
+
+  module Migrations
+    class MigrationBeta78Update
+      def require_restart
+        true
+      end
+      
+      def migrate
+      end
+    end    
+  end
+end
