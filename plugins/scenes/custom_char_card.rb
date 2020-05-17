24d9925aa8d0a16cd8fd887596caadf576a10d7b
spiritlake <spiritlakemu@gmail.com>
2020-05-17 12:01:11 -0400

    Character cards

--- plugins/scenes/custom_char_card.rb
+++ plugins/scenes/custom_char_card.rb
@@ -1,11 +1,13 @@
 module AresMUSH
   module Scenes
-    
+
     def self.custom_char_card_fields(char, viewer)
-      
+
+      schools = char.groups.map { |k, v| { school_type: k, school_name: v } }
+
       # Return nil if you don't need any custom fields.
-      return nil
-      
+      # return nil
+
       # Otherwise return a hash of data.  For example, if you want to show traits you could do:
       # {
       #   traits: char.traits.map { |k, v| { name: k, description: v } }
