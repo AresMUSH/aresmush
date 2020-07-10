0335924b5e5de11058f0ffae1daf6aa25d0a1dd2
Linda Naughton <lynn@wordsmyth.org>
2020-07-09 21:15:16 -0400

    Unlink handle on idle.

--- plugins/idle/helpers.rb
+++ plugins/idle/helpers.rb
@@ -36,11 +36,10 @@ module AresMUSH
     
     def self.idle_cleanup(char, idle_status)
       Global.logger.debug "Starting idle cleanup for #{char.name}"
-      # Remove their handle.              
-      if (char.handle)
-        char.handle.delete
-      end
       Login.set_random_password(char)
+      if (char.handle)
+        AresCentral.unlink_handle(char)
+      end
       Global.dispatcher.queue_event CharIdledOutEvent.new(char.id, idle_status)
     end
     
