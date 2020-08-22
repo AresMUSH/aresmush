f36e7545ce04316edc66a42e057953d7d7cc875c
Linda Naughton <lynn@wordsmyth.org>
2020-08-22 17:42:59 -0400

    Change name account. Folder size.

--- plugins/arescentral/web/handle_link_handler.rb
+++ plugins/arescentral/web/handle_link_handler.rb
@@ -5,12 +5,17 @@ module AresMUSH
         enactor = request.enactor
         handle_name = (request.args[:handle_name] || "").titlecase
         link_code = request.args[:link_code]
+        pw = request.args[:confirm_password]
 
         error = Website.check_login(request)
         return error if error
         
         if enactor.handle
           return { error: t('arescentral.character_already_linked') }
+        end
+        
+        if (!enactor.compare_password(pw))
+          return { error: t('login.invalid_password') }
         end
         
         error = AresCentral.link_handle(enactor, handle_name, link_code)
