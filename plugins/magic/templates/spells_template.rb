3c8ae98d9bb8ce163b0ea2c1bc1318529b18026d
spiritlake <32268357+spiritlake@users.noreply.github.com>
2020-08-26 21:59:55 -0400

    Update spells_template.rb

--- plugins/magic/templates/spells_template.rb
+++ plugins/magic/templates/spells_template.rb
@@ -58,6 +58,7 @@ module AresMUSH
       end
 
       def progress(spell)
+        Global.logger.debug "SPELL: #{spell.name}"
         total_xp_needed = Magic.spell_xp_needed(spell.name) + 1
         xp = (total_xp_needed - spell.xp_needed)
         ProgressBarFormatter.format(xp, total_xp_needed)
