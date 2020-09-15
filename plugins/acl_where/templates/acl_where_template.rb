c572718abda8fb81b8d569176cd9d3b8ca2b8578
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-09-14 18:23:38 -1000

    Update acl_where_template.rb

--- plugins/acl_where/templates/acl_where_template.rb
+++ plugins/acl_where/templates/acl_where_template.rb
@@ -29,7 +29,7 @@ module AresMUSH
 	  
 	  def acl_list_rooms(area)
 		objects = Room.all.select { |r| r.name_upcase =~ /#{self.name.upcase}/ }
-        objects = objects.map { |r| format_name(r) }
+        objects.map { |r| format_name(r) }
 	  end
         
         case (Global.read_config("who", "where_style"))
