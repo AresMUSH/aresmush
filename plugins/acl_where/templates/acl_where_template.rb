6b8d57ef90539353ede74e2afd4aa72ba23a9f7b
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-09-14 20:54:12 -1000

    Update acl_where_template.rb

--- plugins/acl_where/templates/acl_where_template.rb
+++ plugins/acl_where/templates/acl_where_template.rb
@@ -28,7 +28,7 @@ module AresMUSH
       end
 	  
 	  def acl_list_rooms(area, indent_str)
-	    areaname = find_any_by_id(a.area_id)
+	    areaname = find_any_by_id(area.id)
 		#objects = Room.alexl.select { |a| a.area_id = area }
 		#objects = area.sorted_children
 		new_indent = "  #{indent_str}**"
