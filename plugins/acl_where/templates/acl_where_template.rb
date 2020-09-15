5c40495260bc69b891f1844fc8744d25577d0ca8
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-09-14 22:55:45 -1000

    Update acl_where_template.rb

--- plugins/acl_where/templates/acl_where_template.rb
+++ plugins/acl_where/templates/acl_where_template.rb
@@ -23,7 +23,9 @@ module AresMUSH
           return nil
         else
           new_indent = "  #{indent_str}"
-          kids.map { |a| "%R#{indent_str}- #{a.id}#{children(a, new_indent)}"}.join("")
+		  openbrace = " ("
+		  closebrace = ") "
+          kids.map { |a| "%R#{indent_str}- #{a.name}#{openbrace}#{a.id}#{closebrace}#{children(a, new_indent)}"}.join("")
         end
       end
 	  
