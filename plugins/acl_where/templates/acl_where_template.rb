00d900d966d6eac2a16d9520b084e99343542462
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-09-14 18:34:13 -1000

    Update acl_where_template.rb

--- plugins/acl_where/templates/acl_where_template.rb
+++ plugins/acl_where/templates/acl_where_template.rb
@@ -27,10 +27,11 @@ module AresMUSH
         end
       end
 	  
-	  def acl_list_rooms(area indent_str)
+	  def acl_list_rooms(area, indent_str)
 		objects = Room.all.select { |r| r.name_upcase =~ /#{self.name.upcase}/ }
 		new_indent = "  #{indent_str}"
 		objects.map { |a| "%R#{indent_str}- #{a.name}#{acl_list_rooms(a, new_indent)}"}.join("")
+        #objects.map { |a| format_name(a) }
 	  end
         
         case (Global.read_config("who", "where_style"))
