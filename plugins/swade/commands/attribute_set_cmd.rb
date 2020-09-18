517eddf2459b4780609d17e4399588628e394761
Bladesurfer <70926392+Bladesurfer@users.noreply.github.com>
2020-09-17 19:36:17 -1000

    testing some shite (#8)
    * fix callouts. add ref to sheet cmd
    
    * fixing syntax
    
    * testing hardset command
    
    * testing some shite

--- plugins/swade/commands/attribute_set_cmd.rb
+++ plugins/swade/commands/attribute_set_cmd.rb
@@ -3,7 +3,7 @@ module AresMUSH
     class AttributeSetCmd
       include CommandHandler
       
-      attr_accessor :target_name, :attribute_name, :die_step
+      attr_accessor :target_name, :attribute_name, :die_step, :aaaa
       
       def parse_args
         # Admin version
@@ -14,8 +14,8 @@ module AresMUSH
           self.die_step = downcase_arg(args.arg3)
         # Self version 
 		else
-          self.aaaaa = "Just Testing"
-		  args = cmd.parse_args(ArgParser.arg1_equals_arg2)
+          self.aaaaa = "Testing some shite"
+          args = cmd.parse_args(ArgParser.arg1_equals_arg2)
           self.target_name = enactor_name
           self.attribute_name = titlecase_arg(args.arg1)
           self.die_step = downcase_arg(args.arg2)
