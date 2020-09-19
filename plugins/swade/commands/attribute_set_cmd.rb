8d4c1c0ff53f724e463d3447bf026f9ebf54370d
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-09-18 20:03:29 -1000

    Update attribute_set_cmd.rb

--- plugins/swade/commands/attribute_set_cmd.rb
+++ plugins/swade/commands/attribute_set_cmd.rb
@@ -72,7 +72,7 @@ module AresMUSH
           
           if (attr)
             attr.update(die_step: self.die_step)
-			newacltest = " #{self.acltest}%r%r#{attr}%r%r#{model}"			
+			newacltest = " #{self.acltest}%r%r#{attr}%r%r#{model}%r%R#{self.die_step}"
 			template = BorderedDisplayTemplate.new newacltest, "Attr set?"
 			client.emit template.render
           else
