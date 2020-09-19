aaf8bd3d36d4e6e4134a415ff356a850711cdcef
Cyriael <cyriael@gmail.com>
2020-09-19 13:54:35 -1000

    debuggin

--- plugins/swade/commands/iconicf_set_cmd.rb
+++ plugins/swade/commands/iconicf_set_cmd.rb
@@ -10,15 +10,20 @@ module AresMUSH
 			end
       
 			def check_valid_iconicf
-				return t('swade.invalid_iconicf_name') if !Swade.is_valid_iconicf_name?(self.iconicf_name)
+				return t('swade.iconicf_valid_name') if Swade.is_valid_iconicf_name?(self.iconicf_name)
 				return nil
 			end
+			
+			# def check_valid_iconicf
+				# return t('swade.iconicf_invalid_name') if !Swade.is_valid_iconicf_name?(self.iconicf_name)
+				# return nil
+			# end
 		  
-			def check_can_set
-				return nil if enactor_name == self.target_name
-				return nil if Swade.can_manage_abilities?(enactor)
-				return t('dispatcher.not_allowed')
-			end     
+			# def check_can_set
+				# return nil if enactor_name == self.target_name
+				# return nil if Swade.can_manage_abilities?(enactor)
+				# return t('dispatcher.not_allowed')
+			# end     
 		  
 			# def check_chargen_locked
 				# return nil if Swade.can_manage_abilities?(enactor)
@@ -32,33 +37,33 @@ module AresMUSH
 				# client.emit_success t('swade.iconicf_set')
 			# end
 			
-			def handle   
-				iconicf = Swade.get_iconicf(self.iconicf_name)
+			# def handle   
+				# iconicf = Swade.get_iconicf(self.iconicf_name)
         
-				if (!iconicf)
-				client.emit_failure t('swade.iconicf_invalid_type')
-				return
-				end
+				# if (!iconicf)
+				# client.emit_failure t('swade.iconicf_invalid_type')
+				# return
+				# end
         
-				values = iconicf['values']
-				if (self.value && values)
-					self.value = values.keys.find { |v| v.downcase == self.value.downcase }
-					if (!self.value)
-						client.emit_failure t('swade.iconicf_invalid', :iconicf => self.iconicf_name)
-						return
-					end
-				end
+				# values = iconicf['values']
+				# if (self.value && values)
+					# self.value = values.keys.find { |v| v.downcase == self.value.downcase }
+					# if (!self.value)
+						# client.emit_failure t('swade.iconicf_invalid', :iconicf => self.iconicf_name)
+						# return
+					# end
+				# end
         
-				ClassTargetFinder.with_a_character(self.name, client, enactor) do |model|
-					Swade.set_iconicf(model, self.iconicf_name, self.value)
+				# ClassTargetFinder.with_a_character(self.name, client, enactor) do |model|
+					# Swade.set_iconicf(model, self.iconicf_name, self.value)
                     
-					if (!self.value)
-						client.emit_success t('swade.iconicf_cleared', :iconicf => self.iconicf_name)
-					else
-						client.emit_success t('swade.iconicf_set', :iconicf => self.iconicf_name, :value => self.value)
-					end
-				end
-			end
+					# if (!self.value)
+						# client.emit_success t('swade.iconicf_cleared', :iconicf => self.iconicf_name)
+					# else
+						# client.emit_success t('swade.iconicf_set', :iconicf => self.iconicf_name, :value => self.value)
+					# end
+				# end
+			# end
 		end
     end
 end
