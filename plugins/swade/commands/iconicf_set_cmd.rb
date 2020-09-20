1e321913afb5f158e02fbd891195bfdd262d8edb
Cyriael <cyriael@gmail.com>
2020-09-20 13:04:23 -1000

    Update iconicf_set_cmd.rb

--- plugins/swade/commands/iconicf_set_cmd.rb
+++ plugins/swade/commands/iconicf_set_cmd.rb
@@ -7,12 +7,12 @@ module AresMUSH
 			
 			def parse_args
 				  self.target = enactor_name #Set the character to be the current character
-				  self.iconicf_name = trim_arg(cmd.args) #Set 'iconicf_name' to be the inputted Iconic Framework
+				  self.iconicf_name = titlecase_arg(cmd.args || "") #Set 'iconicf_name' to be the inputted Iconic Framework
 			end
 
-			def required_args
-				[ self.target, self.iconicf_name ]
-			end
+			# def required_args
+				# [ self.target, self.iconicf_name ]
+			# end
 				
 				
 			def handle  
@@ -22,11 +22,21 @@ module AresMUSH
 				iconicf_exists = Swade.get_iconicf(self.enactor, self.iconicf_name)
 				client.emit (iconicf_exists)
 				client.emit ("----- Start if statement")
-				if (iconicf_exists)
-					swade_iconicf = Swade.set(enactor, self.iconicf_name)
-					client.emit (swade_iconicf)
-					client.emit (iconicf_name)
-					client.emit_success t('swade.iconicf_set', :name => self.iconicf_name.capitalize)
+
+				ClassTargetFinder.with_a_character(self.name, client, enactor) do |model|        
+				  
+					if (self.iconicf_name.blank?)
+						model.update(swade_iconicf: nil)
+						client.emit_success t('swade.iconicf_cleared')
+					else
+						error = Swade.set_swade_iconicf(model, self.iconicf_name, enactor)
+						if (error)
+							client.emit_failure error
+						else
+							client.emit_success t('swade.iconicf_set', :name => self.iconicf_name.capitalize)
+						end
+					end
+				end
 				else
 					client.emit_failure t('swade.iconicf_invalid_name', :name=> self.iconicf_name.capitalize)
 				end
