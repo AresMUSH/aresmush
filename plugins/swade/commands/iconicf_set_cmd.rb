7ec0c6a4225ec7f01c81a7dd0ea14a1fdb95ed9a
Cyriael <cyriael@gmail.com>
2020-09-19 21:55:11 -1000

    Update iconicf_set_cmd.rb

--- plugins/swade/commands/iconicf_set_cmd.rb
+++ plugins/swade/commands/iconicf_set_cmd.rb
@@ -56,7 +56,7 @@ module AresMUSH
 					#set the iconic framework on the character
 					setattribute= "swade_iconicf_name"
 					ClassTargetFinder.with_a_character(self.target, client, enactor) do |model|
-						SwadeAttributes.create(name: self.setattribute, value: self.iconicf_name, character: model)
+						SwadeAttributes.create(name: self.setattribute, setvalue: self.iconicf_name, character: model)
 						    #model.update(swade_iconicf_name: self.iconicf_name)
 							client.emit_success t('swade.iconicf_set', :name => self.iconicf_name)
 					end				
