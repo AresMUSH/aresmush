cd9fb0e6486f4b2081af9072671e78cacc7be616
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-09-19 17:45:25 -1000

    Update iconicf_set_cmd.rb

--- plugins/swade/commands/iconicf_set_cmd.rb
+++ plugins/swade/commands/iconicf_set_cmd.rb
@@ -54,7 +54,7 @@ module AresMUSH
 				if (iconicf)
 					client.emit (iconicf['attributes'])
 					iconicf_name=iconicf['name'].downcase
-					iconicf_attributes=iconicf['attributes']
+					iconicf_attributes=iconicf['attributes'].join(",")
 					client.emit (iconicf_name)
 					client.emit (iconicf_attributes)
 					enactor.update(swade_iconicfname: self.iconicf_name)
