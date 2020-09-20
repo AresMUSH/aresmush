12df0b5daa4656e89711be47a9b7c0009f6e0766
Cyriael <cyriael@gmail.com>
2020-09-19 22:14:45 -1000

    Update iconicf_set_cmd.rb

--- plugins/swade/commands/iconicf_set_cmd.rb
+++ plugins/swade/commands/iconicf_set_cmd.rb
@@ -61,7 +61,7 @@ module AresMUSH
 
 						    #model.update(swade_iconicf_name: self.iconicf_name)
 							client.emit_success t('swade.iconicf_set', :name => self.iconicf_name)
-					end				
+				#end				
 					client.emit (iconicf['attributes'])
 					iconicf_name=iconicf['name']
 					iconicf_attributes=iconicf['attributes']
