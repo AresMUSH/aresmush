c8753da0f2d053d4cd7b6b0963698b19aff64974
Cyriael <cyriael@gmail.com>
2020-09-20 14:16:52 -1000

    Update iconicf_set_cmd.rb

--- plugins/swade/commands/iconicf_set_cmd.rb
+++ plugins/swade/commands/iconicf_set_cmd.rb
@@ -3,7 +3,7 @@ module AresMUSH
 		class IconicfSetCmd
 			include CommandHandler
       
-			attr_accessor :goals
+			attr_accessor :swade_iconicf
 
 			def parse_args
 				self.goals = trim_arg(cmd.args)
