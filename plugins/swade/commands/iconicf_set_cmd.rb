a73853987ec8e173d1bac97c5ef9e057773403d7
Cyriael <cyriael@gmail.com>
2020-09-20 14:02:18 -1000

    Update iconicf_set_cmd.rb

--- plugins/swade/commands/iconicf_set_cmd.rb
+++ plugins/swade/commands/iconicf_set_cmd.rb
@@ -3,10 +3,11 @@ module AresMUSH
 		class IconicfSetCmd
 			include CommandHandler
       
-			attr_accessor :target, :iconicf_name, :swade_iconicf
+			#attr_accessor :target, :iconicf_name, :swade_iconicf
+			attr_accessor :iconicf_name
 			
 			def parse_args
-				  self.target = enactor_name #Set the character to be the current character
+				  #self.target = enactor_name #Set the character to be the current character
 				  self.iconicf_name = titlecase_arg(cmd.args || "") #Set 'iconicf_name' to be the inputted Iconic Framework
 			end
 
@@ -21,21 +22,24 @@ module AresMUSH
 				client.emit ("----- check to see if the Iconic Framework Exists")
 				iconicf_exists = Swade.get_iconicf(self.enactor, self.iconicf_name)
 				client.emit (iconicf_exists)
-				client.emit ("----- Start TargetFinder")
+				client.emit ("----- ")
+				enactor.update(swade_iconicf: self.iconicf_name)
+				client.emit_success t('swade.iconicf_set', :name => self.iconicf_name.capitalize)
+				
 
-				ClassTargetFinder.with_a_character(self.target, client, enactor) do |model|        
+#				ClassTargetFinder.with_a_character(self.target, client, enactor) do |model|        
 				  
-					if (self.iconicf_name.blank?)
-						enactor.update(swade_iconicf: nil)
-						client.emit_success t('swade.iconicf_cleared', :name => self.iconicf_name.capitalize)
-					else
-						client.emit (model)
-						client.emit (enactor)
-						enactor.update(swade_iconicf: self.iconicf_name)
-						client.emit_success t('swade.iconicf_set', :name => self.iconicf_name.capitalize)
-					end
+					# if (self.iconicf_name.blank?)
+						# enactor.update(swade_iconicf: nil)
+						# client.emit_success t('swade.iconicf_cleared', :name => self.iconicf_name.capitalize)
+					# else
+						# client.emit (model)
+						# client.emit (enactor)
+						# enactor.update(swade_iconicf: self.iconicf_name)
+						# client.emit_success t('swade.iconicf_set', :name => self.iconicf_name.capitalize)
+					# end
 					
-				end
+				# end
 				# else
 					# client.emit_failure t('swade.iconicf_invalid_name', :name=> self.iconicf_name.capitalize)
 				# end
