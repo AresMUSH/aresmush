cc98eca8ffd6f89a91725c06a603f9dba45b9415
Cyriael <cyriael@gmail.com>
2020-09-20 14:13:13 -1000

    Update iconicf_set_cmd.rb

--- plugins/swade/commands/iconicf_set_cmd.rb
+++ plugins/swade/commands/iconicf_set_cmd.rb
@@ -3,15 +3,15 @@ module AresMUSH
 		class IconicfSetCmd
 			include CommandHandler
       
-			attr_accessor :iconicf_name
+			attr_accessor :goals
 
 			def parse_args
-				self.iconicf_name = trim_arg(cmd.args)
+				self.goals = trim_arg(cmd.args)
 			end
 
 			def handle
-				enactor.update(swade_iconicf: self.iconicf_name)
-				client.emit_success "Iconic Framework set!"
+				enactor.update(goals: self.goals)
+			client.emit_success "Goals set!"
 			end
 		end
 	end
