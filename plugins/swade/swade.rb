dcd768ca51fbcc5182ae66ec86c7e0e23028215a
Cyriael <cyriael@gmail.com>
2020-09-21 21:57:08 -1000

    Update swade.rb

--- plugins/swade/swade.rb
+++ plugins/swade/swade.rb
@@ -31,54 +31,64 @@ module AresMUSH
 								# return RaceCmd
 							# else
 								# return RaceSetCmd
+							# end
 						# when "hj"
 							# if (!cmd.args)							 
 								# return HjCmd
 							# else
 								# return HjSetCmd
+							# end
 						# when "fandg"
 							# if (!cmd.args)							 
 								# return FandgCmd
 							# else
 								# return FandgSetCmd
+							# end
 						when "stats"
 							if (!cmd.args)							 
 								return StatsCmd
 							else
 								# return StatsSetCmd
 								return client.emit ("Command Pending")
+							end
 						when "skill"
 							if (!cmd.args)							 
 								return SkillCmd
 							else
 								# return SkillSetCmd
 								return client.emit ("Command Pending")
+							end
 						when "hind"
 							if (!cmd.args)							 
 								return HindCmd
 							else
 								# return HindSetCmd
 								return client.emit ("Command Pending")
+							end
 						# when "edge"
 							# if (!cmd.args)							 
 								# return EdgeCmd
 							# else
 								# return EdgeSetCmd
+							# end
 						# when "ppower"
 							# if (!cmd.args)							 
 								# return PpowerCmd
 							# else
 								# return PpowerSetCmd
+							# end
 						# when "mpower"
 							# if (!cmd.args)							 
 								# return MpowerSetCmd
 							# else
 								# return MpowerSetCmd
+							# end
 						# when "cyber"
 							# if (!cmd.args)							 
 								# return CyberSetCmd
 							# else
 								#return CyberSetCmd
+							# end
 						else
 							client.emit ("Error")
 						end
