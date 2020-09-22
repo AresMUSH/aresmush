09929b785fad61424acbb037e594e1d292a50f70
Cyriael <cyriael@gmail.com>
2020-09-21 15:16:29 -1000

    tweak

--- plugins/swade/commands/stats_cmd.rb
+++ plugins/swade/commands/stats_cmd.rb
@@ -1,14 +1,14 @@
 module AresMUSH    
-  module Swade
-    class StatsCmd
-      include CommandHandler
-  
-      def handle
-        types = Global.read_config("swade", "stats_lib")
-        list = types.sort_by { |a| a['name']}
-        template = StatsTemplate.new list
-        client.emit template.render
-      end
-    end
-  end
+	module Swade
+		class StatsCmd
+			include CommandHandler
+		  
+			def handle
+				types = Global.read_config("swade", "stats")
+				list = types.sort_by { |a| a['name']}
+				template = StatsTemplate.new list
+				client.emit template.render
+			end
+		end
+	end
 end
