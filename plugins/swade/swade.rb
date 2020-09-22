6570c10e75f966b624c92dea8d4bc8c6aa37bbf4
Cyriael <cyriael@gmail.com>
2020-09-21 15:02:11 -1000

    Update swade.rb

--- plugins/swade/swade.rb
+++ plugins/swade/swade.rb
@@ -24,40 +24,40 @@ module AresMUSH
 								end
 						when "reset"
 							return ResetCmd
-						end
-						when "chargen"
-							return ChargenCmd
-						end
-						when "race"
-							return RaceCmd
-						end
-						when "hj"
-							return HjCmd
-						end
-						when "fandg"
-							return FandgCmd
-						end
+						# end
+						# when "chargen"
+							# return ChargenCmd
+						# end
+						# when "race"
+							# return RaceCmd
+						# end
+						# when "hj"
+							# return HjCmd
+						# end
+						# when "fandg"
+							# return FandgCmd
+						# end
 						when "stat"
 							return StatCmd
 						end
-						when "skill"
-							return SkillCmd
-						end
-						when "hind"
-							return HindCmd
-						end
-						when "edge"
-							return EdgeCmd
-						end
-						when "ppower"
-							return PpowerCmd
-						end
-						when "mpower"
-							return MpowerCmd
-						end
-						when "cyber"
-							return CyberCmd
-						end
+						# when "skill"
+							# return SkillCmd
+						# end
+						# when "hind"
+							# return HindCmd
+						# end
+						# when "edge"
+							# return EdgeCmd
+						# end
+						# when "ppower"
+							# return PpowerCmd
+						# end
+						# when "mpower"
+							# return MpowerCmd
+						# end
+						# when "cyber"
+							# return CyberCmd
+						# end
 				when "sheet"
 					return SheetCmd
 				end
