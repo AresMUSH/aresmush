e0c0cbe6dd2cce2faeb5ce302bfc8dba90b6525a
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-09-23 08:10:22 -1000

    Changes to web stuff

---
+++ plugins/swade/public/swade_api.rb
@@ -0,0 +1,19 @@
+module AresMUSH
+  module Swade
+    def self.get_abilities_for_web_viewing(char, viewer)
+		skills = "Hello World" 
+        return {
+
+          skills: skills
+          # Copied from the FS3 stuff.
+
+          # attrs: attrs,
+          # action_skills: action_skills,
+          # backgrounds: backgrounds,
+          # languages: languages,
+          # advantages: advantages,
+          # use_advantages: FS3Skills.use_advantages?
+        } 
+	end
+  end
+end
