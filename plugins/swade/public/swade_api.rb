76c22a756b0a30f2407a177b62be6c1bf2d066c5
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-09-23 18:36:05 -1000

    f

--- plugins/swade/public/swade_api.rb
+++ plugins/swade/public/swade_api.rb
@@ -1,5 +1,23 @@
 module AresMUSH
   module Swade
+  
+    def die_rating (rating)
+      case rating
+      when 0
+        return "No die"
+      when 1
+        return "<i class='fad fa-dice-d4'></i>"
+      when 2
+        return "<i class='fad fa-dice-d6'></i>"
+      when 3
+        return "<i class='fad fa-dice-d8'></i>"
+      when 4
+        return "<i class='fad fa-dice-d10'></i>"
+      when 5
+        return "<i class='fad fa-dice-d12'></i>" 
+      end
+    end
+	
     def self.get_abilities_for_web_viewing(char, viewer)
 	
 	    # Format skill table
@@ -29,7 +47,7 @@ module AresMUSH
 				cellopenid='<div class="col-sm-4">'
 				cellcloseid='</div>'
 				title = "#{ a.name }".capitalize
-				rating = a.rating
+				rating = die_rating(a.rating)
 				"#{rowopenid}#{cellopenid}#{title}: #{rating}#{cellcloseid}#{linebreak}#{rowcloseid}"
 			end
 	end	
