1e04a26710c34bb4957dd443831175b364ede10d
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-09-25 07:32:44 -1000

    Update swrifts_api.rb

--- plugins/swrifts/public/swrifts_api.rb
+++ plugins/swrifts/public/swrifts_api.rb
@@ -51,7 +51,7 @@ module AresMUSH
 				pp swskills
 				swsclass2 = swskills.class
 				swdesc2 = ''
-				swdesc = swskills['description']
+				#swdesc = swskills['description']
 				rowopenid = i == 0 ? "<div class='skilltable'><div class='container-fluid skillstable'><div class='row no-gutters'>" : ""
 				rowcloseid = i == skills.count ? "</div></div></div>" : ""
 				#linebreak = i % 3 == 0 ? " <div class='w-100'></div> " : ""
