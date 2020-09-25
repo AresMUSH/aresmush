bde88621d50f2434ed3059318617beba1b085c46
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-09-24 16:35:02 -1000

    Update swrifts_api.rb

--- plugins/swrifts/public/swrifts_api.rb
+++ plugins/swrifts/public/swrifts_api.rb
@@ -43,13 +43,12 @@ module AresMUSH
 				correcttitle = "#{a.name}".titleize
 				downsizetitle = "#{a.name}".downsize
 				rating = die_rating(correcttitle,a.rating)
-				
 				# sets 'iconicf' to the Iconic Framework 'name' of our game\config\swrifts_skills.yml file
 				#swriftskills = Global.read_config('swrifts', 'swrifts_skills')				
 				#select the skill from the list.
 				#swskills = swriftskills.select { |ss| ss['name'].downcase == downsizetitle }.first
 				#ssdesc = "#{swskills.description}"
-				ssdesc = 'hellow world'
+				#ssdesc = 'hellow world'
 				rowopenid = i == 0 ? "<div class='skilltable'><div class='container-fluid skillstable'><div class='row no-gutters'>" : ""
 				rowcloseid = i == skills.count ? "</div></div></div>" : ""
 				#linebreak = i % 3 == 0 ? " <div class='w-100'></div> " : ""
@@ -59,7 +58,7 @@ module AresMUSH
 				colautoopenid="<div class='col-sm-9 heading #{cssclass}'>"
 				colsmallopenid="<div class='col-sm-3 rating #{cssclass}'>"
 				cellcloseid='</div>'
-				title = "<span title='#{correcttitle} - #{ssdesc}'>#{correcttitle}</span>"
+				title = "<span title='#{correcttitle}'>#{correcttitle}</span>"
 				"#{rowopenid}#{cellopenid}#{colautoopenid}#{title}: #{cellcloseid}#{colsmallopenid}#{rating}#{cellcloseid}#{cellcloseid}#{linebreak}#{rowcloseid}"
 			end
 	end	
