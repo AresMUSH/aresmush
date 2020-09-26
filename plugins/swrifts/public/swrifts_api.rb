90e157a8d6786a6f6a8a4b0673c9bf1414c95220
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-09-25 20:01:14 -1000

    Update swrifts_api.rb

--- plugins/swrifts/public/swrifts_api.rb
+++ plugins/swrifts/public/swrifts_api.rb
@@ -151,11 +151,11 @@ module AresMUSH
 	
 	
 	#### CHARGEN ####
-	
+
 	def self.get_abilities_for_chargen(char)
 		# Format Iconic Framework table
 		swrifts_iconicf = Global.read_config('swrifts', 'iconicf')
-		iconicf = returniconicforcg(swrifts_iconicf)
+		#iconicf = returniconicforcg(swrifts_iconicf)
 		swrifts_iconicf='hellow world'
 		#iconicf = iconicf.join(" ") #removes the comma's that seperates the entries		
 
