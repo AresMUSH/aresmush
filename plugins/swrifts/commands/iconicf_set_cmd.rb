ed302c7d2bea02f528bf14d13f52575de20a1a10
Cyriael <cyriael@gmail.com>
2020-09-25 22:10:41 -1000

    Update iconicf_set_cmd.rb

--- plugins/swrifts/commands/iconicf_set_cmd.rb
+++ plugins/swrifts/commands/iconicf_set_cmd.rb
@@ -68,7 +68,7 @@ module AresMUSH
 						setrating = "#{rating}"
 						ClassTargetFinder.with_a_character(self.target, client, enactor) do |model|
 							# add to char obj
-							model.update(self.setthing: self.rating)
+							model.update(self.setthing":" self.rating)
 						end
 					end
 					client.emit_success ("Chargen Stats Set.")
