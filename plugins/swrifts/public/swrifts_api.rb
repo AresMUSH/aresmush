07ca3e4726127125d5ec6467ae904b3a1715d166
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-09-26 15:11:22 -1000

    Update swrifts_api.rb

--- plugins/swrifts/public/swrifts_api.rb
+++ plugins/swrifts/public/swrifts_api.rb
@@ -178,7 +178,7 @@ module AresMUSH
 	
 	def self.returniconicforcg(model)
 	    modelclass = model.class
-		modelclass = "Model Class: #{model.class}"
+		modelclass = "Model Class: #{modelclass}"
 		#Used for debugging - need to delete when complete
         list = model.sort_by { |a| a['name']}
 			.each_with_index
