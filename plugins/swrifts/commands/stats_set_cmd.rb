c7ad9a3e52b0655c3635d30e24e6a239af0fa65a
Cyriael <cyriael@gmail.com>
2020-09-28 22:33:31 -1000

    Update stats_set_cmd.rb

--- plugins/swrifts/commands/stats_set_cmd.rb
+++ plugins/swrifts/commands/stats_set_cmd.rb
@@ -3,7 +3,7 @@ module AresMUSH
 		class StatsSetCmd
 			include CommandHandler
 			  
-			attr_accessor :target_name, :stat_name, :rating
+			attr_accessor :target_name, :stat_name, :mod
 			
 			#command syntax: swrifts/stat	<stat_name>=<new_rating>
 
@@ -11,41 +11,32 @@ module AresMUSH
 				args = cmd.parse_args(ArgParser.arg1_equals_arg2) #break <args> into arg1 and arg2
 			    self.target_name = enactor_name #set target to enactor
 			    self.stat_name = titlecase_arg(args.arg1) #set stat_name to arg1
-			    self.rating = integer_arg(args.arg2) #set raing to arg2
+			    self.mod = integer_arg(args.arg2) #set raing to arg2
+				self.points_name = stats_points
 			end
 
 			def required_args
-				[self.target_name, self.stat_name, self.rating]
+				[self.target_name, self.stat_name, self.mod]
 			end
 			
 			def check_valid_stat
 				return t('swrifts.invalid_stat_name') if !Swrifts.is_valid_stat_name?(self.stat_name)
 				return nil
-			end			
-			  
-			# def stat_current
-				# stat = "stat_current"
-				# return_current_stat stat
-			# end
+			end
 			
-			# add check for stat cap max
-			
-			# def return_current_stat(statname)
-			# statname = traitname.downcase
-			# swriftsstats = @char.swrifts_stats
-			# swriftsstats.to_a.sort_by { |a| a.name }
-				# .each_with_index
-					# .map do |a, i| 
-					# if a.name.downcase == "#{statname}"
-						# return a.rating
-					# end
-				# end	
-			# end
+			def check_stats_points
+				current_stats_points = Swrifts.chargen_points(enactor, self.points_name)
+				if mod > current_stats_points
+					return t('swrifts.invalid_points')
+				else
+					return nil
+				end
+			end
 			  
 			def handle
 				current_rating = Swrifts.stat_rating(enactor, self.stat_name)
 				client.emit ( current_rating )
-				mod = self.rating
+				mod = self.mod
 				client.emit ( mod )
 				new_rating = current_rating + mod
 				client.emit ( new_rating )
@@ -54,20 +45,7 @@ module AresMUSH
 					stat = Swrifts.find_stat(model, self.stat_name)				
 					stat.update(rating: new_rating)
 				end
-				
-				
-				# error = Swrifts.check_rating(self.name, new_rating)
-				# if (error)
-				  # client.emit_failure error
-				  # return
-				# end
-			  
-				# error = Swrifts.set_stat(enactor, self.name, new_rating)
-				# if (error)
-				  # client.emit_failure error
-				# else
-				  # client.emit_success Swrifts.stat_raised_text(enactor, self.name)
-				# end
+
 			end
 		end
 	end
