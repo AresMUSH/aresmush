5b0016dfa58910f1434a3ad468d259a7992375f5
Cyriael <cyriael@gmail.com>
2020-10-01 17:43:31 -1000

    Update init_char_cmd.rb

--- plugins/swrifts/commands/init_char_cmd.rb
+++ plugins/swrifts/commands/init_char_cmd.rb
@@ -3,7 +3,7 @@ module AresMUSH
 		class InitCharCmd
 			include CommandHandler
 			      
-			attr_accessor :target
+			attr_accessor :target, :init
 			
 			def parse_args
 				self.target = enactor_name #Set the character to be the current character
