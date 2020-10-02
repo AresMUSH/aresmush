56cd656fb6d494aaa66c646504e0dbd59711ffcb
Cyriael <cyriael@gmail.com>
2020-10-02 01:23:01 -1000

    Update iconicf_set_cmd.rb

--- plugins/swrifts/commands/iconicf_set_cmd.rb
+++ plugins/swrifts/commands/iconicf_set_cmd.rb
@@ -154,18 +154,18 @@ module AresMUSH
 					client.emit_failure ("This Iconic Framework has no Counters")
 				end
 
-				## ----- This sets the default Hinderances on the Character -----	
-				if (iconicf_hinderances) 
-					iconicf_hinderances.each do |key|
-						setthing = "#{key}".downcase
-						ClassTargetFinder.with_a_character(self.target, client, enactor) do |model|
-							SwriftsHinderances.create(name: setthing, character: model)
-						end
-					end
-					client.emit_success t('swrifts.iconichinderances_set')
-				else 
-					client.emit_failure ("This Iconic Framework has no Hinderances")
-				end
+				# ----- This sets the default Hinderances on the Character -----	
+				# if (iconicf_hinderances) 
+					# iconicf_hinderances.each do |key|
+						# setthing = "#{key}".downcase
+						# ClassTargetFinder.with_a_character(self.target, client, enactor) do |model|
+							# SwriftsHinderances.create(name: setthing, character: model)
+						# end
+					# end
+					# client.emit_success t('swrifts.iconichinderances_set')
+				# else 
+					# client.emit_failure ("This Iconic Framework has no Hinderances")
+				# end
 				
 				## ----- This sets the default Edges on the Character -----				
 				if (iconicf_edges)
