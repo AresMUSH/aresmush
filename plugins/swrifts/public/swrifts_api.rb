931902790801694728e9a68e67f0b59b8b1c1717
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-02 19:44:20 -1000

    Update swrifts_api.rb

--- plugins/swrifts/public/swrifts_api.rb
+++ plugins/swrifts/public/swrifts_api.rb
@@ -258,7 +258,7 @@ module AresMUSH
 				cgpointsarray[c.name] = cgrating
 				#cgpointsarray = {name: cgname, rating: cgrating}
 		end
-		return ("#{cgpointsarray}") 
+		return ({cgpointsarray}) 
 		#return (cgp) 
 	end		
 	
