d609f2127bdfb5a8fc43e5259994dbceb2105341
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-03 11:41:16 -1000

    Update iconicf_set_cmd.rb

--- plugins/swrifts/commands/iconicf_set_cmd.rb
+++ plugins/swrifts/commands/iconicf_set_cmd.rb
@@ -20,10 +20,13 @@ module AresMUSH
 			def check_valid_iconicf
 				if !Swrifts.is_valid_iconicf_name?(self.iconicf_name) #Is the Iconic Framework in the list
 					return t('swrifts.iconicf_invalid_name', :name => self.iconicf_name.capitalize) 
-				elsif !Swrifts.is_valid_cat?(model,"traits") #Are there any traits set - i.e INIT completed.
-					return t('swrifts.iconicf_invalid_init', :name => "Traits")
-				elsif Swrifts.trait_set?(model,"iconicf") #Is the iconic framework already set
-					return t('swrifts.trait_already_set',:name => "Iconic Framework")
+				else
+				ClassTargetFinder.with_a_character(self.target_name, client, enactor) do |model|
+					if !Swrifts.is_valid_cat?(model,"traits") #Are there any traits set - i.e INIT completed.
+						return t('swrifts.iconicf_invalid_init', :name => "Traits")
+					elsif Swrifts.trait_set?(model,"iconicf") #Is the iconic framework already set
+						return t('swrifts.trait_already_set',:name => "Iconic Framework")
+					end
 				end
 				return nil
 			end
