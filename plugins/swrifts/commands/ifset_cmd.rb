a4c29e815df3b791c72b919bd146d046012fb5ca
Cyriael <cyriael@gmail.com>
2020-10-03 14:39:37 -1000

    Update ifset_cmd.rb

--- plugins/swrifts/commands/ifset_cmd.rb
+++ plugins/swrifts/commands/ifset_cmd.rb
@@ -25,9 +25,9 @@ module AresMUSH
 					iconicf_hinderances.each do |key|
 						setthing = "#{key}".downcase
 						client.emit ( "#{setthing}" )
-						# ClassTargetFinder.with_a_character(self.target_name, client, enactor) do |model|
-							# SwriftsHinderances.create(name: setthing, character: model)
-						# end
+						ClassTargetFinder.with_a_character(self.target_name, client, enactor) do |model|
+							SwriftsHinderances.create(name: setthing, character: model)
+						end
 					end
 					client.emit_success t('swrifts.iconichinderances_set')
 				else 
