d06f5e9da6b737a0f26c2f3e8cca3a5cdf41d853
Cyriael <cyriael@gmail.com>
2020-10-03 22:41:19 -1000

    Update ifset_cmd.rb

--- plugins/swrifts/commands/ifset_cmd.rb
+++ plugins/swrifts/commands/ifset_cmd.rb
@@ -3,23 +3,22 @@ module AresMUSH
 		class IfSetCmd
 			include CommandHandler
 			      
-			attr_accessor :target_name, :iconicf_name
+			attr_accessor :target_name
 			
 			def parse_args
 				self.target_name = enactor_name #Set the character to be the current character
-				self.iconicf_name = trim_arg(cmd.args) #Set 'iconicf_name' to be the inputted Iconic Framework
 
 			end
 
 			def required_args
-				[ self.target_name, self.iconicf_name ]
+				[ self.target_name]
 			end
 
 			
 
 			def self.get_cmd_handler(client, cmd, enactor)
 				ClassTargetFinder.with_a_character(self.target_name, client, enactor) do |model|
-					Swrifts.check_features_mod(model, SwriftsHinderances, "Hinderances", hinderance_name)
+					Swrifts.check_features_mod(model, SwriftsHinderances, "Hinderances", Elderly)
 				end
 			end
 			
