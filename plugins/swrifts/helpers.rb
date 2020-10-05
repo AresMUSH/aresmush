1e36f4e19c252ad0a6519c64c60fb8f53a7699b5
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-04 18:08:20 -1000

    Update helpers.rb

--- plugins/swrifts/helpers.rb
+++ plugins/swrifts/helpers.rb
@@ -13,10 +13,11 @@ module AresMUSH
 		def self.add_feature(model, collection, featuretype, feature_name)
 			collection.create(name: feature_name, character: model)
 			featuretype = featuretype.downcase
-			return (featuretype)
+			#return (featuretype)
 			feature_name = feature_name.gsub("*", "")	 #remove the * that appear in the feature name		
 			feature_name = feature_name.gsub("^", "")	 #remove the ^ that appear in the feature name		
 			featurehash = Global.read_config('swrifts', featuretype)
+			return (featurehash.inspect)
 			fg = featurehash.select { |a| a['name'].downcase == feature_name.downcase }.first
 			
 			if (fg['stats'])
