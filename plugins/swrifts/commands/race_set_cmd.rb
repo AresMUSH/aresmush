15b6427735494477176cb786f21fa24e2ace79b0
Cyriael <cyriael@gmail.com>
2020-10-06 22:15:01 -1000

    Update race_set_cmd.rb

--- plugins/swrifts/commands/race_set_cmd.rb
+++ plugins/swrifts/commands/race_set_cmd.rb
@@ -49,7 +49,7 @@ module AresMUSH
 					icf = ( "#{e_iconicf}" )
 				end
 				
-				return IconicSetCmd
+				return IconicfSetCmd
 
 				## ----- Update Race Framework
 				ClassTargetFinder.with_a_character(self.target_name, client, enactor) do |model|
