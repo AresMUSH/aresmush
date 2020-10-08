cbffb8e4026f36582ed3904902d67f51e04dec2d
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-07 21:34:20 -1000

    Update custom_char_fields.rb

--- plugins/profile/custom_char_fields.rb
+++ plugins/profile/custom_char_fields.rb
@@ -31,7 +31,7 @@ module AresMUSH
       # Note Custom fields will be in chargen_data[:custom]
       # Example: char.update(goals: chargen_data[:custom][:goals])
       def self.save_fields_from_chargen(char, chargen_data)
-		Swrifts.save_abilities_for_chargen(char, chargen_data)
+		#Swrifts.save_abilities_for_chargen(char, chargen_data)
       end
     end
   end
