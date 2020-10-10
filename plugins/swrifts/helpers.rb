5c30e8e2e29aae3869f1b4c8d7d53b00cd97b2b3
Cyriael <cyriael@gmail.com>
2020-10-09 21:08:42 -1000

    Update helpers.rb

--- plugins/swrifts/helpers.rb
+++ plugins/swrifts/helpers.rb
@@ -13,7 +13,7 @@ module AresMUSH
 				numbers = [1, 2, 3, 4]
 				numbers.each do |key|
 					setthing = "hj#{key}}"
-					setrating = rand[1..20]
+					setrating = 1
 					SwriftsHeroesj.create(name: setthing, rating: setrating, character: model)
 				end
 			end			
