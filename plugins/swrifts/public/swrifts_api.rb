0187ed54d217c262f73f9c7e11e404551e59f3fe
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-10 14:26:05 -1000

    Update swrifts_api.rb

--- plugins/swrifts/public/swrifts_api.rb
+++ plugins/swrifts/public/swrifts_api.rb
@@ -393,8 +393,7 @@ module AresMUSH
 				tt = Swrifts.run_init(char, init)				
 				# tt = SwriftsTraits.create(name: 'iconicf', rating: name_downcase, character: char)
 				iconicf = Global.read_config('swrifts', 'iconicf')
-				iconicf.select { |a| a['name'].downcase == name_downcase }.first				
-				tt1 = iconicf
+				tt1 = iconicf.select { |a| a['name'].downcase == name_downcase }.first				
 				#Swrifts.run_iconicf(char, iconicf)
 				tt2 = 'we got here'
 			else
