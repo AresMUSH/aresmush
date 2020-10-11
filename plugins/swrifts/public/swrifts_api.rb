2fc93b4788902fe92684da83fef51f1417874118
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-10 14:28:57 -1000

    Update swrifts_api.rb

--- plugins/swrifts/public/swrifts_api.rb
+++ plugins/swrifts/public/swrifts_api.rb
@@ -409,7 +409,7 @@ module AresMUSH
 		 
 		# trait = char.swrifts_traits.inspect
 	
-		return ["Trait: #{swriftstraits}, #{tt}, #{tt1}, #{tt2}, #{tt3}"]
+		return ["Trait: #{swriftstraits}, #{tt}, #{tt1}, #{tt2}, #{name_downcase}"]
 		
         #return ["Iconfic Framework Set to: #{chopped_iconicf}", "Race set to: #{chopped_race}"]	
 	end
