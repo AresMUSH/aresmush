4db3c4279c5bf9747666ab4086bcf1009a310618
Cyriael <cyriael@gmail.com>
2020-10-13 21:35:53 -1000

    Update helpers.rb

--- plugins/swrifts/helpers.rb
+++ plugins/swrifts/helpers.rb
@@ -591,19 +591,19 @@ module AresMUSH
 			name_downcase = trait_title.downcase
 			model.swrifts_traits.select { |a| a.name.downcase == name_downcase }.first
 		end
-		
-		# def self.return_element_value(model, elementname, set)
-		# elementname = elementname.downcase
-		# ss = "swrifts_#{set}"
-		# swriftselement = model.ss
-		# swriftselement.to_a.sort_by { |a| a.name }
-			# .each_with_index
-				# .map do |a, i| 
-				# if a.name.downcase == "#{elementname}"
-					# return ("#{a.rating}")
-				# end
-			# end	
-		# end
+				
+		def self.return_element_value(model, element_title, set)
+		element_title = element_title.downcase
+		ss = "swrifts_#{set}"
+		swriftselement = model.ss
+		swriftselement.to_a.sort_by { |a| a.name }
+			.each_with_index
+				.map do |a, i| 
+				if a.name.downcase == "#{element_title}"
+					return ("#{a.rating}")
+				end
+			end	
+		end
 
 		
 		## ----- Die Step
