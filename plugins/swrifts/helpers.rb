c0dbc99041d3cb2d26334f8f5ec2b6784778201c
Cyriael <cyriael@gmail.com>
2020-10-14 23:09:24 -1000

    Update helpers.rb

--- plugins/swrifts/helpers.rb
+++ plugins/swrifts/helpers.rb
@@ -312,7 +312,7 @@ module AresMUSH
 		def self.race_check(model, race, race_name, icf_name)
 			carray = race.include? 'complications'
 			
-			if icf_name == "None"
+			if icf_name == "none"
 				return false
 			else
 				iconicf = Swrifts.find_iconicf_config(icf_name) #get the Iconic Framework entry we're working with from the yml
