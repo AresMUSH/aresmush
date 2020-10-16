3bcf673dc864e9e2412061704c6874008647f3fa
spiritlake <32268357+spiritlake@users.noreply.github.com>
2020-10-16 09:58:56 -0400

    Update motd_set_cmd.rb

--- plugins/login/commands/motd_set_cmd.rb
+++ plugins/login/commands/motd_set_cmd.rb
@@ -4,16 +4,16 @@ module AresMUSH
       include CommandHandler
 
       attr_accessor :notice
-      
+
       def parse_args
         self.notice = cmd.args
       end
-      
+
       def check_can_set
-        return t('dispatcher.not_allowed')  if !Login.can_manage_login?(enactor)
+        return t('dispatcher.not_allowed')  if !Login.can_view_bgs?(enactor)
         return nil
       end
-      
+
       def handle
         Game.master.update(login_motd: self.notice)
         client.emit_success t('login.motd_set')
