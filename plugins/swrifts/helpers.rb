4d64c6d222cf205e070e70d59373b6797703e835
Cyriael <cyriael@gmail.com>
2020-10-16 20:39:42 -1000

    Update helpers.rb

--- plugins/swrifts/helpers.rb
+++ plugins/swrifts/helpers.rb
@@ -361,6 +361,7 @@ module AresMUSH
 				
 				if nsb_check == true
 					pajock = edgecheck.select{ |a| a.name == "power armor jock*" }.first
+					return (pajock.inspect)
 					if (pajock)
 						return true
 					else #continue
