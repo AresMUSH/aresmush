c5572e4895adcf9ebe1e41dd8337d8d2b249fd96
Cyriael <cyriael@gmail.com>
2020-10-17 21:15:00 -1000

    Update ifset_cmd.rb

--- plugins/swrifts/commands/ifset_cmd.rb
+++ plugins/swrifts/commands/ifset_cmd.rb
@@ -23,50 +23,56 @@ module AresMUSH
 				system=iconicf
 				model=enactor
 				
+				rand_num_check = model.swrifts_randnum
+				client.emit (rand_num_check.inspect)
+				
+				return
+				
+				
 				systest = system['hj1_options']
 				
 				hjcheck = model.swrifts_heroesj.empty?
 				client.emit !hjcheck
 			
 			
-				if (system['hj1_options']) && hjcheck == true #See if there are any HJ slots (iconicf command) outlined AND they haven't been set already
+				# if (system['hj1_options']) && hjcheck == true #See if there are any HJ slots (iconicf command) outlined AND they haven't been set already
 					
-					counter = 0
-					system.each do | title, value |
-						title = title.slice(0,2)
-						if title == "hj" 
-							counter = counter + 1
-						else
-						end						
-					end
-					counter = *(1..counter)
-					counter.each do |key|
-						setthing = "hj#{key}"
-						setrating = rand(1..10)
-						settable = "None"
-						setdesc = "None"
-						SwriftsHeroesj.create(name: setthing, table: settable, rating: setrating, description: setdesc, character: model)
-					end
-				elsif (system['hj1_options']) && !hjcheck == true #See if there are any HJ slots (iconicf command) outlined AND if they already have HJs set up
-					counter = 0
-					system.each do | title, value |
-						title = title.slice(0,2)
-						if title == "hj" 
-							counter = counter + 1
-						else
-						end						
-					end
-					counter = *(1..counter)
-					counter.each do |key|
-						setthing = "hj#{key}"
-						test = model.swrifts_heroesj
-						hj_set = model.swrifts_heroesj.select { |a| a.name.downcase == setthing }.first	
-						settable = "None"
-						setdesc = "None"
-						hj_set.update(table: settable, description: setdesc, character: model)
-					end
-				else
-				end
+					# counter = 0
+					# system.each do | title, value |
+						# title = title.slice(0,2)
+						# if title == "hj" 
+							# counter = counter + 1
+						# else
+						# end						
+					# end
+					# counter = *(1..counter)
+					# counter.each do |key|
+						# setthing = "hj#{key}"
+						# setrating = rand(1..10)
+						# settable = "None"
+						# setdesc = "None"
+						# SwriftsHeroesj.create(name: setthing, table: settable, rating: setrating, description: setdesc, character: model)
+					# end
+				# elsif (system['hj1_options']) && !hjcheck == true #See if there are any HJ slots (iconicf command) outlined AND if they already have HJs set up
+					# counter = 0
+					# system.each do | title, value |
+						# title = title.slice(0,2)
+						# if title == "hj" 
+							# counter = counter + 1
+						# else
+						# end						
+					# end
+					# counter = *(1..counter)
+					# counter.each do |key|
+						# setthing = "hj#{key}"
+						# test = model.swrifts_heroesj
+						# hj_set = model.swrifts_heroesj.select { |a| a.name.downcase == setthing }.first	
+						# settable = "None"
+						# setdesc = "None"
+						# hj_set.update(table: settable, description: setdesc, character: model)
+					# end
+				# else
+				# end
 				
 			end
 			
