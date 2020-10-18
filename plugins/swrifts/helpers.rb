f11a9ec23bbe5a0600c1410146be98b4baed178e
Cyriael <cyriael@gmail.com>
2020-10-17 21:18:13 -1000

    Update helpers.rb

--- plugins/swrifts/helpers.rb
+++ plugins/swrifts/helpers.rb
@@ -18,6 +18,7 @@ module AresMUSH
 					setthing = "hj#{key}"
 					setrating = rand(1..10)
 					SwriftsRandnum.create(name: setthing, rating: setrating, character: model)
+				end
 				fandg_nums.each do |key|
 					setthing = "fandg#{key}"
 					setrating = rand(1..10)
