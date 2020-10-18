d9ec4582427b430940384518bd3f54770b4db49d
Cyriael <cyriael@gmail.com>
2020-10-17 15:50:31 -1000

    Update hj_set_cmd.rb

--- plugins/swrifts/commands/hj_set_cmd.rb
+++ plugins/swrifts/commands/hj_set_cmd.rb
@@ -61,6 +61,7 @@ module AresMUSH
 			# client.emit (hj_rolls.inspect)
 			
 			hj_rolls.each do | roll, desc |
+				array[roll]
 				if roll===icfhj_roll.to_i
 					client.emit (roll.inspect)
 					client.emit ("#{desc}")
