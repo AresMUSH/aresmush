9400ccab5bc080a06953e931778ebb2388ea7d4f
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-20 22:14:14 -1000

    Update swrifts_api.rb

--- plugins/swrifts/public/swrifts_api.rb
+++ plugins/swrifts/public/swrifts_api.rb
@@ -313,7 +313,7 @@ module AresMUSH
 				ifstring << ")"
 			end		
 			ifdisabled=false # Will need better logic here.
-			return ("#{rawcharrace.inspect}, #{rawcharrace.length}")
+			return ("#{rawcharrace.inspect}")
 			if ( rawcharrace.length > 0 )				
 				swrifts_race = Swrifts.find_race_config(rawcharrace) #get the Race entry we're working with from the yml
 				# Is there a character race selected?
