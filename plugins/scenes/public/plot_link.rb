0a78b4e7ccb2ece98fa92b0e0b22484595224b36
Linda Naughton <lynn@wordsmyth.org>
2020-10-27 02:26:53 -0400

    Fixing plot indexing.

--- plugins/scenes/public/plot_link.rb
+++ plugins/scenes/public/plot_link.rb
@@ -7,15 +7,18 @@ module AresMUSH
     index :plot
     
     def self.find_link(plot, scene)
-      PlotLink.all.select { |l| l.plot == plot && l.scene == scene }.first
+      #PlotLink.all.select { |l| l.plot == plot && l.scene == scene }.first
+      PlotLink.find(plot_id: plot.id).combine(scene_id: scene.id).first
     end
     
     def self.find_by_plot(plot)
-      PlotLink.all.select { |l| l.plot == plot }
+      #PlotLink.all.select { |l| l.plot == plot }
+      PlotLink.find(plot_id: plot.id)
     end
 
     def self.find_by_scene(scene)
-      PlotLink.all.select { |l| l.scene == scene }
+      #PlotLink.all.select { |l| l.scene == scene }
+      PlotLink.find(scene_id: scene.id)
     end
   end
 end
