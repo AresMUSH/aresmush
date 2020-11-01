3b2bf13fcf52b9585e3db206ce78d7cf08f914f9
Cyriael <cyriael@gmail.com>
2020-10-31 16:22:18 -1000

    add hj cmd

---
+++ plugins/swrifts/templates/hj_template.rb
@@ -0,0 +1,18 @@
+module AresMUSH    
+  module Swrifts
+    class IconicfTemplate < ErbTemplateRenderer
+
+      attr_accessor :types
+      
+      def initialize(types)
+        self.types = types
+        super File.dirname(__FILE__) + "/hj.erb"
+      end
+      
+	  def description(type)
+		type['description']
+      end 
+
+    end
+  end
+end
