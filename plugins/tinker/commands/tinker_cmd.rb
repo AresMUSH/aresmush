6a493b5753427b96addcda04f5f537d22f0ccefe
spiritlake <32268357+spiritlake@users.noreply.github.com>
2020-11-04 09:25:10 -0500

    Update tinker_cmd.rb

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -9,31 +9,15 @@ module AresMUSH
         return nil
       end
 
-     
+      def parse_args
+        spell = cmd.args
+      end
+
+
       def handle
-      #   Character.all.each do |c|
-      #     puts "Starting XP: #{c.xp}"
-      #     FS3Skills.modify_xp(c, 1)
-      #     puts "Ending XP: #{c.xp}"
-      #   end
-
-      enactor = enactor
-      if (enactor.handle)
-        all_chars = AresCentral.alts(enactor)
-      else
-        all_chars = enactor
-      end
-
-
-      client.emit "CHARS: #{all_chars}"
-
-      my_scenes = []
-      all_chars.each do |char|
-        char_scenes = Scene.all.select { |s| !s.completed && Scenes.is_watching?(s, char) }.sort_by { |s| s.id }
-        my_scenes.concat char_scenes
-      end
-
-      client.emit "SCENES #{my_scenes}"
+        if Chargen.approved_chars.select { |c| Magic.knows_spell?(c, spell) }
+          client.emit c.name
+        end
 
       end
 
