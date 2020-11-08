83cdbcd61191b76938eb8e7a909c935e78073fdc
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-11-08 13:18:21 -1000

    Update swrifts_api.rb

--- plugins/swrifts/public/swrifts_api.rb
+++ plugins/swrifts/public/swrifts_api.rb
@@ -587,7 +587,7 @@ module AresMUSH
 				# end
 			# end
 		end
-		return ("#{cifstring.object}");
+		return ("#{charhiicf.inspect}, #{charhjicf.class}");
 	end
 
 	def self.acl_return_traits(st,traitname)
