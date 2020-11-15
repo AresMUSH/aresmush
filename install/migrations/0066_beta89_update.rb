f0c0bba3a88a258abb2efff352e90dbc4448d7cd
Linda Naughton <lynn@wordsmyth.org>
2020-11-15 00:34:11 -0500

    Premade job comments, scene commands.

--- install/migrations/0066_beta89_update.rb
+++ install/migrations/0066_beta89_update.rb
@@ -12,6 +12,15 @@ module AresMUSH
         if (role)
           role.update(is_restricted: true)
         end
+        
+        Global.logger.debug "Add job responses."
+        config = DatabaseMigrator.read_config_file('jobs.yml')
+        if (!config['jobs']['responses']) 
+          config['jobs']['responses'] = [
+            { 'name' => "Received", 'text' => "Your job has been received. We'll get to it soon." }
+          ]
+          DatabaseMigrator.write_config_file('jobs.yml', config)
+        end
       end
     end    
   end
---
+++ plugins/scenes/custom_scene_commands.rb
@@ -0,0 +1,10 @@
+module AresMUSH
+  module Scenes
+    class CustomSceneCommands
+      def handle(enactor, char, scene, command, args)
+        
+        return nil
+      end
+    end
+  end
+end
