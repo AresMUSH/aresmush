0deff2ab2ea194ab2354960ceda8132b39d4d00b
spiritlake <32268357+spiritlake@users.noreply.github.com>
2020-11-30 09:42:01 -0500

    Update gear_special_info.rb

--- plugins/fs3combat/helpers/gear_special_info.rb
+++ plugins/fs3combat/helpers/gear_special_info.rb
@@ -2,12 +2,12 @@ module AresMUSH
   module FS3Combat
     class GearSpecialInfo
       attr_accessor :gear_type, :list
-      
+
       def initialize(list, gear_type)
         self.gear_type = gear_type.to_s
         self.list = list
       end
-      
+
       def specials
         if (self.gear_type == "weapon" || self.gear_type == "armor")
           allowed_specials = []
@@ -16,10 +16,11 @@ module AresMUSH
               allowed_specials = fields[1]
             end
           end
-          
+
           specials = {}
           all_specials = self.gear_type == "weapon" ? FS3Combat.weapon_specials : FS3Combat.armor_specials
           all_specials.each do |k, v|
+            Global.logger.debug "Allowed Specials #{allowed_specials} K #{k}"
             if (allowed_specials.include?(k))
               specials[k] = special_effects(k, v)
             end
@@ -29,7 +30,7 @@ module AresMUSH
           {}
         end
       end
-      
+
       def special_effects(name, data)
         effects = []
         data.each do |k, v|
