7d15f1e4f8c3043495f6418c8df047833ecfd468
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-12-07 07:55:29 -1000

    Update swrifts_api.rb

--- plugins/swrifts/public/swrifts_api.rb
+++ plugins/swrifts/public/swrifts_api.rb
@@ -225,7 +225,8 @@ module AresMUSH
 		hjslots = acl_get_hj_slots(swrifts_iconicf, rawcharicf) #swrifts_icf is the system icf's, charicf is the one selected by the player		
 		
 		# hjtables = acl_get_hj_slots(swrifts_iconicf, rawcharicf)
-		hjtables = cghjtables.inspect
+		# hjtables = cghjtables.inspect
+		hjtables = "#{cghjtables.inspect}"
 
 		return {
 		  iconicf: iconicf, #System iconic frameworks
