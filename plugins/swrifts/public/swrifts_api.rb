7985a8c0b6b369c809357495d8dc185c54b9a34c
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-12-07 23:02:14 -1000

    Update swrifts_api.rb

--- plugins/swrifts/public/swrifts_api.rb
+++ plugins/swrifts/public/swrifts_api.rb
@@ -226,7 +226,7 @@ module AresMUSH
 		
 		hjtables = acl_get_hj_tables(cghjtables, rawcharicf)
 		# hjtables = cghjtables.inspect
-		hjtables = "#{hjtables.inspect}"
+		hjtables = hjtables.inspect
 
 		return {
 		  iconicf: iconicf, #System iconic frameworks
