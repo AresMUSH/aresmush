a163cdc782d82f40a6b5b7f56366cdb9d680f881
spiritlake <spiritlakemu@gmail.com>
2020-12-16 09:56:11 -0500

    Player list

--- plugins/custom/commands/last_rp_cmd.rb
+++ plugins/custom/commands/last_rp_cmd.rb
@@ -8,11 +8,13 @@ module AresMUSH
         client.emit "Last RP Scenes for All Characters"
         chars = Chargen.approved_chars
         chars.each do |c|
-          last_scene = c.scenes_starring.sort_by { |s| s.created_at }.reverse[0] || "NONE"
-          scene_created = last_scene.created_at || "N/A"
+          last_scene = c.scenes_starring.sort_by { |s| s.created_at }.reverse[0]
           scene_count = c.scenes_starring.count
-          scene_title = last_scene.title || "N/A"
-          client.emit "#{scene_created} - #{c.name} - #{scene_title}. #{scene_count} scenes total."
+          if last_scene
+            client.emit "#{last_scene.created_at} - #{c.name} - #{last_scene.title}. #{scene_count} scenes total."
+          else
+            client.emit "#{c.name}: %xrNONE%xn"
+          end
         end
       end
 
