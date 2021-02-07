1ddaaaadbf52f1c9a2d969e304fcfdc68cd240d3
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-02-07 12:41:46 -1000

    Update swrifts_api.rb

--- plugins/swrifts/public/swrifts_api.rb
+++ plugins/swrifts/public/swrifts_api.rb
@@ -660,7 +660,7 @@ module AresMUSH
 		
 		
 		if (chargen_data[:custom][:race])
-			return (chargen_data[:custom][:race])
+			return (chargen_data[:custom][:race][:class])
 		else
 			return ("hello")
 		end
