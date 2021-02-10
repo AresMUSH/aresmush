a078ec1ddbc15e84ebfbb2a84f4f6b8cf93a2793
spiritlake <spiritlakemu@gmail.com>
2021-02-10 09:54:24 -0500

    Update tinker_cmd.rb

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -17,14 +17,37 @@ module AresMUSH
 
 
       def handle
-
+        client.emit "---------------CREATURES----------"
+        client.emit "---------------Major School----------"
         Creature.all.each do |p|
-          # client.emit p.major_school
-          client.emit "#{p.name}- #{ p.major_school['name']}"
-
-          # p.update(primary_school: "#{p.primary_school['name']}")
-          # client.emit "DONE: #{p.primary_school}"
+          client.emit "#{p.name}: #{p.major_school['name']}"
+          # p.update(major_school: "#{p.major_school['name']}")
         end
+        client.emit "---------------Minor School----------"
+        Creature.all.each do |p|
+          client.emit "#{p.name}: #{p.minor_school['name']}"
+          # p.update(minor_school: "#{p.minor_school['name']}")
+        end
+
+        client.emit "---------------PORTALS----------"
+        client.emit "---------------Primary School----------"
+        Portal.all.each do |p|
+          client.emit "#{p.name}: #{p.primary_school['name']}"
+          # p.update(primary_school: "#{p.primary_school['name']}")
+        end
+        client.emit "--------------All Schools----------"
+        Portal.all.each do |p|
+          schools = p.all_schools
+          all_schools = []
+          schools.each do |s|
+            all_schools.concat [s['name']]
+          end
+          client.emit "#{p.name}: #{all_schools}"
+          # p.update(all_schools: all_schools)
+
+        end
+
+
       end
 
 
