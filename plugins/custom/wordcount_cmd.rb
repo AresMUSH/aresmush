6c1d7946fa76f8a87d48dee440c360c05b463a31
Clockwork EJD <4930532+ClockworkEJD@users.noreply.github.com>
2021-02-18 17:50:17 -0500

    Update wordcount_cmd.rb

--- plugins/custom/wordcount_cmd.rb
+++ plugins/custom/wordcount_cmd.rb
@@ -11,8 +11,11 @@ module AresMUSH
             
             def handle
                 ClassTargetFinder.with_a_character(self.name, client, enactor) do |model|
-                  word_count = "#{model.name}'s Word Count:", model.pose_word_count
-                  msg = word_count.join(" ")
+                  word_count = model.pose_word_count
+                  scene_count = model.scenes_participated_in.size
+                  words_per_scene = word_count / scene_count
+                  total_count = "#{model.name} has written", word_count, "words in", scene_count, "scenes for an average of", words_per_scene, "per scene."
+                  msg = total_count.join(" ")
                   client.emit_success msg
                 end
             end
