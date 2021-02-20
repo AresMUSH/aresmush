b828da7f7733aa3951b077b25ba2063fb63b58fd
Linda Naughton <lynn@wordsmyth.org>
2021-02-20 01:55:40 -0500

    Enumerated content warnings.

--- install/migrations/0070_beta93_update.rb
+++ install/migrations/0070_beta93_update.rb
@@ -12,6 +12,19 @@ module AresMUSH
         config = DatabaseMigrator.read_config_file("channels.yml")
         config['channels']['debug_discord'] = false
         DatabaseMigrator.write_config_file("channels.yml", config)
+        
+        Global.logger.debug "Adding content warnings."
+        config = DatabaseMigrator.read_config_file("scenes.yml")
+        config['scenes']['content_warnings'] = [
+          "graphic violence",
+          "sexual assault",
+          "nsfw",
+          "self-harm",
+          "suicide",
+          "bigotry",
+          "torture"
+        ]
+        DatabaseMigrator.write_config_file("scenes.yml", config)
       end
     end    
   end
