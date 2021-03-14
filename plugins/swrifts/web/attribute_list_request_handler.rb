9d352e63ca0182ee6fd8dee4f36cae55c511ce26
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-03-14 13:28:51 -1000

    Adding hinderances pages to site

--- plugins/swrifts/web/attribute_list_request_handler.rb
+++ plugins/swrifts/web/attribute_list_request_handler.rb
@@ -4,7 +4,7 @@ module AresMUSH
       def handle(request)
         {
 		  edges: build_list(Swrifts.swrifts_edges),
-		  # hinderances: build_list(Swrifts.swrifts_hinderances)
+		  hinderances: build_list(Swrifts.swrifts_hinderances)
         } 
       end
       
