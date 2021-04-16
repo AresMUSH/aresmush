839b32cc4b77f1b3205bdd0bdc424e37cc9478d9
spiritlake <spiritlakemu@gmail.com>
2021-04-16 17:07:44 -0400

    Update custom_char_card.rb

--- plugins/scenes/custom_char_card.rb
+++ plugins/scenes/custom_char_card.rb
@@ -3,7 +3,16 @@ module AresMUSH
 
     def self.custom_char_card_fields(char, viewer)
 
-      schools = char.groups.map { |k, v| { school_type: k, school_name: v } }
+
+
+      {
+        schools:char.groups.map { |k, v| { school_type: k.titlecase, school_name: v } },
+        abilites: self.char_fs3_abilities(char)
+      }
+
+
+
+
 
       # Return nil if you don't need any custom fields.
       # return nil
@@ -13,5 +22,16 @@ module AresMUSH
       #   traits: char.traits.map { |k, v| { name: k, description: v } }
       # }
     end
+
+    def self.char_fs3_abilities(char)
+      abilities = []
+      [ char.fs3_attributes, char.fs3_action_skills, char.fs3_background_skills, char.fs3_languages, char.fs3_advantages ].each do |list|
+        list.each do |a|
+          abilities << a.name
+        end
+        return abilities
+      end
+    end
+
   end
 end
