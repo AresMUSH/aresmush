82eeae7a257b396b75f5323039b9e12cd20c059e
spiritlake <spiritlakemu@gmail.com>
2021-04-23 15:08:16 -0400

    Update desc_rooms.rb

--- plugins/describe/public/desc_rooms.rb
+++ plugins/describe/public/desc_rooms.rb
@@ -4,15 +4,19 @@ module AresMUSH
     attribute :shortdesc
     attribute :details, :type => DataType::Hash, :default => {}
     attribute :vistas, :type => DataType::Hash, :default => {}
-    
+
     def expanded_desc
       Describe::RoomDescBuilder.build(self)
     end
+
+    def expanded_web_desc
+      Describe::RoomWebDescBuilder.build(self) 
+    end
   end
-  
+
   class Exit
     attribute :description
     attribute :shortdesc
     attribute :details, :type => DataType::Hash, :default => {}
-  end    
+  end
 end
