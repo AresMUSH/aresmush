3fcf28bdbf4697aa8b5e5dadf31c0df730e10bbb
spiritlake <spiritlakemu@gmail.com>
2021-06-23 17:09:18 -0400

    Spell fatigue in spells command

--- plugins/magic/templates/spells_template.rb
+++ plugins/magic/templates/spells_template.rb
@@ -12,6 +12,25 @@ module AresMUSH
         Magic.count_spells_total(char)
       end
 
+      def magic_energy
+        ProgressBarFormatter.format(char.magic_energy, char.total_magic_energy)
+      end
+
+      def magic_energy_percent
+        current = char.magic_energy
+        total = char.total_magic_energy
+        puts "Current: #{current} Total: #{total}"
+        (current.to_f/total.to_f)*100
+      end
+
+      def spell_fatigue_degree
+        Magic.get_fatigue_level(char)[:degree]
+      end
+
+      def spell_fatigue_msg
+        Magic.get_fatigue_level(char)[:msg]
+      end
+
       def spells_learned
         @char.spells_learned.to_a
       end
