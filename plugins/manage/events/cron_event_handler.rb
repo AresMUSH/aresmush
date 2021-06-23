5d813c87a9720933a30bf7d2fdfeee6149694041
Linda Naughton <lynn@wordsmyth.org>
2021-06-22 23:38:38 -0400

    Auto wiki update.

--- plugins/manage/events/cron_event_handler.rb
+++ plugins/manage/events/cron_event_handler.rb
@@ -7,8 +7,13 @@ module AresMUSH
         
         Global.logger.debug "Starting backup."
         
-	# Note: The spawn is inside the backup method.
-        Manage.perform_backup(nil)
+	      # Note: The spawn is inside the backup method.
+        Manage.perform_backup(nil)  
+        
+	      # Note: The spawn is inside the export method.
+        if (Global.read_config("website", "auto_wiki_export"))      
+          Website.export_wiki
+        end
       end
     end    
   end
