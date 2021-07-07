c54cbdc93318fa63ac3e5b7fc06d88227dbcd4b5
Linda Naughton <lynn@wordsmyth.org>
2021-07-07 02:41:17 -0400

    Unified chat.

--- plugins/channels/web/leave_channel_request_handler.rb
+++ plugins/channels/web/leave_channel_request_handler.rb
@@ -5,19 +5,25 @@ module AresMUSH
         enactor = request.enactor
         channel_name = request.args[:channel]
         channel = Channel.find_one_by_name(channel_name)
+        char = Character.named(request.args[:char])
         
         error = Website.check_login(request)
         return error if error
                 
         request.log_request
         
-        if (!channel)
+        if (!channel || !char)
           return { error: t('webportal.not_found') }
         end
         
-        Channels.leave_channel(enactor, channel)
-                         
+        if (!AresCentral.is_alt?(char, enactor))
+          return { error: t('dispatcher.not_allowed') }
+        end
+        
+        Channels.leave_channel(char, channel)
+        
         {
+          enabled: AresCentral.alts(enactor).any? { |a| Channels.is_on_channel?(a, channel) }
         }
       end
     end
