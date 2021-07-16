cefe18f18700a527cab873692ba93ac14226f696
spiritlake <spiritlakemu@gmail.com>
2021-07-16 16:40:59 -0400

    Fixing crons

--- plugins/website/events/web_cron_event_handler.rb
+++ plugins/website/events/web_cron_event_handler.rb
@@ -1,26 +1,27 @@
 module AresMUSH
   module Website
     class WebCronEventHandler
+
       def on_event(event)
         config = Global.read_config("website", "sitemap_update_cron")
         if Cron.is_cron_match?(config, event.time)
           update_sitemap
         end
-        
+
         config = Global.read_config("website", "wiki_export_cron")
         if Cron.is_cron_match?(config, event.time)
           export_wiki
         end
-        
+
       end
-      
-      def updte_sitemap
+
+      def update_sitemap
         Global.logger.debug "Updating sitemap."
         path = File.join(AresMUSH.game_path, 'sitemap.xml')
         File.open(path, 'w') do |file|
           file.puts '<?xml version="1.0" encoding="UTF-8"?>'
           file.puts '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
-          
+
           topics = Website.build_sitemap
           topics.each do |t|
             file.puts " <url>"
@@ -31,14 +32,16 @@ module AresMUSH
           end
           file.puts '</urlset>'
         end
-        
-        def export_wiki
-  	      # Note: The spawn is inside the export method.
-          if (Global.read_config("website", "auto_wiki_export"))      
-            Website.export_wiki
-          end
+      end
+
+      def export_wiki
+	      # Note: The spawn is inside the export method.
+        if (Global.read_config("website", "auto_wiki_export"))
+          Website.export_wiki
         end
       end
+
+
     end
   end
 end
