661d8a4475abbce22bf4d680efc8da488196bb40
spiritlake <spiritlakemu@gmail.com>
2021-08-05 17:36:15 -0400

    Cleaning up newturn messages
    Mods now return on one line, expirations are colored.

--- plugins/magic/helpers/spell_combat_newturn_helper.rb
+++ plugins/magic/helpers/spell_combat_newturn_helper.rb
@@ -4,21 +4,15 @@ module AresMUSH
     def self.spell_newturn(combatant)
 
       Magic.shield_newturn_countdown(combatant)
-
-      if (combatant.magic_init_mod_counter == 0 && combatant.magic_init_mod > 0)
-        FS3Combat.emit_to_combat combatant.combat, t('magic.mod_wore_off', :name => combatant.name, :type => "initiative", :mod => combatant.magic_init_mod), nil, true
-        combatant.update(magic_init_mod: 0)
-        combatant.log "#{combatant.name} resetting initiative mod to #{combatant.magic_init_mod}."
-      elsif (combatant.magic_init_mod_counter > 0 && combatant.magic_init_mod > 0)
-        combatant.update(magic_init_mod_counter: combatant.magic_init_mod_counter - 1)
-      end
+      mods = []
+      messages = []
 
       if (combatant.magic_stance_counter == 0 && combatant.magic_stance_spell)
         stance = Global.read_config("spells", combatant.magic_stance_spell, "stance")
         if stance == "cover"
           stance = "in cover"
         end
-        FS3Combat.emit_to_combat combatant.combat, t('magic.stance_wore_off', :name => combatant.name, :spell => combatant.magic_stance_spell, :stance => stance), nil, true
+        messages.concat [t('magic.stance_wore_off', :name => combatant.name, :spell => combatant.magic_stance_spell, :stance => stance)]
         combatant.log "#{combatant.name} #{combatant.magic_stance_spell} wore off."
         combatant.update(magic_stance_spell: nil)
         combatant.update(stance: "Normal")
@@ -27,42 +21,50 @@ module AresMUSH
       end
 
       if (combatant.magic_stun_counter == 0 && combatant.magic_stun)
-        FS3Combat.emit_to_combat combatant.combat, t('fs3combat.stun_wore_off', :name => combatant.name), nil, true
+        messages.concat [t('fs3combat.stun_wore_off', :name => combatant.name)]
         combatant.update(magic_stun: false)
         combatant.update(magic_stun_spell: nil)
         combatant.log "#{combatant.name} is no longer magically stunned."
       elsif (combatant.magic_stun_counter > 0 && combatant.magic_stun)
         subduer = combatant.subdued_by
-        FS3Combat.emit_to_combat combatant.combat, t('fs3combat.still_stunned', :name => combatant.name, :stunned_by => subduer.name, :rounds => combatant.magic_stun_counter), nil, true
+        messages.concat [t('fs3combat.still_stunned', :name => combatant.name, :stunned_by => subduer.name, :rounds => combatant.magic_stun_counter)]
         combatant.update(magic_stun_counter: combatant.magic_stun_counter - 1)
       end
 
-      if combatant.magic_lethal_mod_counter == 0 && combatant.magic_lethal_mod != 0
-        FS3Combat.emit_to_combat combatant.combat, t('magic.mod_wore_off', :name => combatant.name, :type => "lethality", :mod => combatant.magic_lethal_mod), nil, true
-        combatant.update(magic_lethal_mod: 0)
-        combatant.log "#{combatant.name} resetting lethality mod to #{combatant.magic_lethal_mod}."
-      else
-        combatant.update(magic_lethal_mod_counter: combatant.magic_lethal_mod_counter - 1)
-      end
-
-      if combatant.magic_defense_mod_counter == 0 && combatant.magic_defense_mod != 0
-        FS3Combat.emit_to_combat combatant.combat, t('magic.mod_wore_off', :name => combatant.name, :type => "defense", :mod => combatant.magic_defense_mod), nil, true
-        combatant.update(magic_defense_mod: 0)
-        combatant.log "#{combatant.name} resetting defense mod to #{combatant.magic_defense_mod}."
-      else
-        combatant.update(magic_defense_mod_counter: combatant.magic_defense_mod_counter - 1)
-      end
-
       if combatant.magic_attack_mod_counter == 0 && combatant.magic_attack_mod != 0
-        FS3Combat.emit_to_combat combatant.combat, t('magic.mod_wore_off', :name => combatant.name, :type => "attack", :mod => combatant.magic_attack_mod), nil, true
+        mods.concat ["#{combatant.magic_attack_mod} attack"]
         combatant.update(magic_attack_mod: 0)
         combatant.log "#{combatant.name} resetting attack mod to #{combatant.magic_attack_mod}."
       else
         combatant.update(magic_attack_mod_counter: combatant.magic_attack_mod_counter - 1)
       end
 
+      if combatant.magic_defense_mod_counter == 0 && combatant.magic_defense_mod != 0
+        mods.concat ["#{combatant.magic_defense_mod} defense"]
+        combatant.update(magic_defense_mod: 0)
+        combatant.log "#{combatant.name} resetting defense mod to #{combatant.magic_defense_mod}."
+      else
+        combatant.update(magic_defense_mod_counter: combatant.magic_defense_mod_counter - 1)
+      end
+
+      if (combatant.magic_init_mod_counter == 0 && combatant.magic_init_mod > 0)
+        mods.concat ["#{combatant.magic_init_mod} initiative"]
+        combatant.update(magic_init_mod: 0)
+        combatant.log "#{combatant.name} resetting initiative mod to #{combatant.magic_init_mod}."
+      elsif (combatant.magic_init_mod_counter > 0 && combatant.magic_init_mod > 0)
+        combatant.update(magic_init_mod_counter: combatant.magic_init_mod_counter - 1)
+      end
+
+      if combatant.magic_lethal_mod_counter == 0 && combatant.magic_lethal_mod != 0
+        mods.concat ["#{combatant.magic_lethal_mod_counter} lethality"]
+        combatant.update(magic_lethal_mod: 0)
+        combatant.log "#{combatant.name} resetting lethality mod to #{combatant.magic_lethal_mod}."
+      else
+        combatant.update(magic_lethal_mod_counter: combatant.magic_lethal_mod_counter - 1)
+      end
+
       if combatant.spell_mod_counter == 0 && combatant.spell_mod != 0
-        FS3Combat.emit_to_combat combatant.combat, t('magic.mod_wore_off', :name => combatant.name, :type => "spell", :mod => combatant.spell_mod), nil, true
+        mods.concat ["#{combatant.spell_mod} spell"]
         combatant.update(spell_mod: 0)
         combatant.log "#{combatant.name} resetting spell mod to #{combatant.spell_mod}."
       else
@@ -73,11 +75,23 @@ module AresMUSH
         auto_revive_spell = combatant.associated_model.auto_revive?
         combatant.update(action_klass: "AresMUSH::FS3Combat::SpellAction")
         combatant.update(action_args: "#{auto_revive_spell}/#{combatant.name}")
-        FS3Combat.emit_to_combat combatant.combat,t('magic.spell_action_msg_long', :name => combatant.name, :spell => auto_revive_spell), nil, true
+        messages.cocat [t('magic.spell_action_msg_long', :name => combatant.name, :spell => auto_revive_spell)]
+      end
+
+      if !mods.empty?
+        if mods.count > 1
+          mods = mods.join(", ")
+          messages.concat  [t('magic.mods_wore_off', :name => combatant.name, :mods => mods)]
+        else
+          mods = mods.join()
+          messages.concat  [t('magic.mod_wore_off', :name => combatant.name, :mods => mods)]
+      end
+
+      messages.uniq.each do |msg|
+        FS3Combat.emit_to_combat(combatant.combat, msg, nil, true)
       end
 
     end
-
 
 
 
