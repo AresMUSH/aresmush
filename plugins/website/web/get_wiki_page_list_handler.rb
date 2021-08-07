22a218343ded1eea2e712405466f00e4a9d8e3f2
Linda Naughton <lynn@wordsmyth.org>
2021-08-07 11:20:06 -0400

    Wiki all pages improvements, locations in export.

--- plugins/website/web/get_wiki_page_list_handler.rb
+++ plugins/website/web/get_wiki_page_list_handler.rb
@@ -2,12 +2,28 @@ module AresMUSH
   module Website
     class GetWikiPageListRequestHandler
       def handle(request)
-        WikiPage.all.to_a
-        .sort_by { |p| p.heading }
-        .map{ |p| {
-          heading: p.heading,
-          id: p.name
-        }}
+        all_pages = WikiPage.all.to_a
+          .sort_by { |p| p.heading }
+          .map{ |p| {
+            heading: p.heading,
+            id: p.name
+          }}
+        category_pages = WikiPage.all
+          .group_by { |p| p.category }
+          .select { |category, pages| !category.blank? }
+          .map { |category, pages | 
+          {
+            name: category,
+            pages: pages.sort_by { |p| p.heading }.map { |p| {
+              heading: p.heading,
+              id: p.name
+            }}
+          }}
+        
+        {
+          all: all_pages,
+          categories: category_pages
+        }
       end
     end
   end
