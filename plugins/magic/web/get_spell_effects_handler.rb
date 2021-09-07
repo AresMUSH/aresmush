22183b6bb5219321fe9a52b66b9c92bfb6b1a74f
spiritlake <spiritlakemu@gmail.com>
2021-09-07 16:15:56 -0400

    Remove unused code

--- plugins/magic/web/get_spell_effects_handler.rb
+++ plugins/magic/web/get_spell_effects_handler.rb
@@ -11,32 +11,7 @@ module AresMUSH
         end
         effects = effects.uniq.sort
         return effects
-
-        # damage = []
-        # spells.each do |s|
-        #    damage.concat [s[:damage_type]]
-        # end
-        #
-        # damage = damage.uniq
-
-
-          # {
-          #   effects: effects,
-          #   damage: damage
-          # }
-
-
-        # effects = []
-        # all_spells = Global.read_config("spells")
-        # spells = Magic.build_spell_list(all_spells)
-        # spells.each do |s|
-        #   effects.concat [Global.read_config("spells", s[:name], "effect")]
-        # end
-        # effects = effects.uniq
-
-
       end
-
 
     end
   end
