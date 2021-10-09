677049e4d4d792154d15519757bd86e139178a16
Linda Naughton <lynn@wordsmyth.org>
2021-10-09 11:03:53 -0400

    Better quickref.

--- plugins/help/commands/quickref_cmd.rb
+++ plugins/help/commands/quickref_cmd.rb
@@ -25,37 +25,39 @@ module AresMUSH
         self.topic = Help.strip_prefix(self.topic)
         search_topic = self.topic.gsub('/', ' ').split(' ').first
         
-        topics = Help.find_quickref(search_topic)
+        topic_keys = Help.find_quickref(search_topic)
 
-        if (topics.empty?)
+        if (topic_keys.empty?)
           client.emit_failure t('help.not_found', :topic => self.topic)          
           return
         end
         
         list = []
-        formatter = MarkdownFormatter.new
-        
-        topic_keys = topics.values.uniq
-        
+        manage_list = []
+                
         topic_keys.each do |found_topic|
+          meta = Help.topic_index[found_topic]
           md_contents = Help.topic_contents(found_topic)
-          lines = md_contents.split("\n")            
+          lines = md_contents.split("\n")
           matching_lines = lines.select { |l| line_is_match?(l) }
           
           if (!matching_lines.empty?)
-            list.concat matching_lines
-          end    
+            if (found_topic.start_with?("manage") || meta["toc"].start_with?("~admin~"))
+              manage_list.concat matching_lines          
+            else
+              list.concat matching_lines
+            end    
+          end
         end
         
-        if (list.empty?)
+        if (list.empty? && manage_list.empty?)
           client.emit_failure t('help.not_found', :topic => self.topic)          
           return
         end
         
         help_url = Help.topic_url('')
-        text = formatter.to_mush(list.sort.join("\n"))
-        client.emit "%lh#{text}%lf"
-        
+        template = QuickrefTemplate.new(list, manage_list)
+        client.emit template.render        
       end
       
       def line_is_match?(line)
