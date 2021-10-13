0ed06181b969ed3dad36ba9d2e743e39a2815881
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-10-12 18:01:32 -1000

    Update swrifts_api.rb

--- plugins/swrifts/public/swrifts_api.rb
+++ plugins/swrifts/public/swrifts_api.rb
@@ -413,7 +413,7 @@ module AresMUSH
 			
 			iconicfarray << {name: ifname, disabled: ifdisabled, desc: desc, trexcludes: trexcludes}
 		end
-		return ({"iconicfarray"})
+		return ("#{iconicfarray}")
 	end	
 	
 	def self.returninitcgforcg(model)
