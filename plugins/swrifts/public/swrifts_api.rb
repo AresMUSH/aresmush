836d0389aa3f6e297571e92c1092ebfb11e48ed4
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-10-13 17:57:58 -1000

    Update swrifts_api.rb

--- plugins/swrifts/public/swrifts_api.rb
+++ plugins/swrifts/public/swrifts_api.rb
@@ -368,6 +368,7 @@ module AresMUSH
 			dname = dname.gsub("^", "")
 			trexlarray = cgsys.select { |ss| ss['name'].downcase.start_with?"#{dname}" }.first #Filter the trait's to find the one that's been selected	
 
+			return ( "#{trexlarray}" )
 			if ( traittype == 'hind' ) 
 				trex = trexlarray['excludes']
 			else 
