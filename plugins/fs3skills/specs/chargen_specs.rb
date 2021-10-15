52ea4a94da02f18ca9563fbeca10126572e2da4c
sisterkettle <dani.kay332@gmail.com>
2021-10-15 11:36:39 -0700

    updated chargen_specs.rb - incapable true

--- plugins/fs3skills/specs/chargen_specs.rb
+++ plugins/fs3skills/specs/chargen_specs.rb
@@ -9,10 +9,10 @@ module AresMUSH
         allow(FS3Skills).to receive(:action_skill_names) { [ "Firearms", "Demolitions" ] }
         allow(FS3Skills).to receive(:advantage_names) { [ "Rank" ] }
         allow(FS3Skills).to receive(:language_names) { [ "English", "Spanish" ] }
-        allow(Global).to receive(:read_config).with("fs3skills", "allow_incapable_action_skills") { false }
+        allow(Global).to receive(:read_config).with("fs3skills", "allow_incapable_action_skills") { true }
         stub_translate_for_testing
       end
-      
+
       describe :check_ability_name do
         it "should not allow funky chars" do
           # Because it will mess up the +/- modifier parsing
@@ -20,20 +20,20 @@ module AresMUSH
           expect(FS3Skills.check_ability_name("X-Y")).to eq "fs3skills.no_special_characters"
           expect(FS3Skills.check_ability_name("X=Y")).to eq "fs3skills.no_special_characters"
           expect(FS3Skills.check_ability_name("X,Y")).to eq "fs3skills.no_special_characters"
-          
+
           # For aesthetic reasons
           expect(FS3Skills.check_ability_name("X:.[]|Y")).to eq "fs3skills.no_special_characters"
-          
+
           # Because folks on older clients can't see them properly.
           expect(FS3Skills.check_ability_name("XñY")).to eq "fs3skills.no_special_characters"
         end
-        
+
         it "should allow spaces and underlines" do
           expect(FS3Skills.check_ability_name("X Y")).to be_nil
           expect(FS3Skills.check_ability_name("X_Y")).to be_nil
         end
       end
-      
+
       describe :check_rating do
         it "should error if below min ratings" do
           expect(FS3Skills.check_rating("Brawn", 0)).to eq "fs3skills.min_rating_is"
@@ -41,12 +41,12 @@ module AresMUSH
           expect(FS3Skills.check_rating("English", -1)).to eq "fs3skills.min_rating_is"
           expect(FS3Skills.check_rating("Basketweaving", -1)).to eq "fs3skills.min_rating_is"
         end
-        
+
         it "should allow incapable for min rating if configured" do
           allow(Global).to receive(:read_config).with("fs3skills", "allow_incapable_action_skills") { true }
           expect(FS3Skills.check_rating("Brawn", 0)).to eq "fs3skills.min_rating_is"
         end
-        
+
         it "should allow min ratings" do
           expect(FS3Skills.check_rating("Brawn", 1)).to be_nil
           expect(FS3Skills.check_rating("Firearms", 1)).to be_nil
@@ -60,35 +60,35 @@ module AresMUSH
           expect(FS3Skills.check_rating("English", 3)).to be_nil
           expect(FS3Skills.check_rating("Basketweaving", 3)).to be_nil
         end
-        
+
         it "should error if above max ratings" do
           expect(FS3Skills.check_rating("Brawn", 5)).to eq "fs3skills.max_rating_is"
           expect(FS3Skills.check_rating("Firearms", 8)).to eq "fs3skills.max_rating_is"
           expect(FS3Skills.check_rating("English", 4)).to eq "fs3skills.max_rating_is"
           expect(FS3Skills.check_rating("Basketweaving", 4)).to eq "fs3skills.max_rating_is"
         end
-        
+
         it "should allow max action skill rating to be configurable" do
           allow(Global).to receive(:read_config).with("fs3skills", "max_skill_rating") { 5 }
           expect(FS3Skills.check_rating("Firearms", 5)).to be_nil
           expect(FS3Skills.check_rating("Firearms", 6)).to eq "fs3skills.max_rating_is"
         end
       end
-      
-      describe :set_ability do 
+
+      describe :set_ability do
         before do
           allow(FS3Skills).to receive(:check_rating) { nil }
           allow(FS3Skills).to receive(:check_ability_name) { nil }
-          
+
           @char = double
         end
-          
-        it "should error if abiliy name invalid" do 
+
+        it "should error if abiliy name invalid" do
           allow(FS3Skills).to receive(:check_ability_name) { "an error" }
           error = FS3Skills.set_ability(@char, "Firearms", 4)
           expect(error).to eq("an error")
         end
-        
+
         context "success" do
           before do
             @ability = double
@@ -98,7 +98,7 @@ module AresMUSH
             allow(@char).to receive(:name) { "Bob" }
             allow(@ability).to receive(:rating_name) { "X" }
           end
-        
+
           it "should update an existing ability" do
             expect(@ability).to receive(:update).with(rating: 4)
             error = FS3Skills.set_ability(@char, "Firearms", 4)
@@ -116,4 +116,3 @@ module AresMUSH
     end
   end
 end
-        
