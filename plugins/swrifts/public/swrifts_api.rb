04fed01132bfc05e15bd5ca94e260d62fdb2146f
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-10-14 21:28:07 -1000

    Update swrifts_api.rb

--- plugins/swrifts/public/swrifts_api.rb
+++ plugins/swrifts/public/swrifts_api.rb
@@ -363,10 +363,8 @@ module AresMUSH
 		
 		iconicfarray = []	
 		ttss = []
-		
-		return ("ICF: #{charicf}, Race: #{charrace}")
-		
-		# return("#{cgsys}")
+				
+		return("#{cgsys}")
 		# Create an array of the excluded traits for the ones that are already set on the character.
 		cg.each do |d|
 			dname = d.name.downcase
