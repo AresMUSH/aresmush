3a4ca31e7e0f9f838481fd69df255bbe4ff1ec3c
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-10-16 12:06:45 -1000

    Update swrifts_api.rb

--- plugins/swrifts/public/swrifts_api.rb
+++ plugins/swrifts/public/swrifts_api.rb
@@ -391,7 +391,8 @@ module AresMUSH
 									# return("#{t['iconicf']} #{charicf[:class]}")									
 									# If the ICF chosen by the player doesn't match t['iconicfc'] then remove this trait from the main array (cg).																		
 									if ( charicf[:class].downcase != t['iconicf'].downcase ) 
-										return ( "#{t['iconicf']}" )
+										d.delete(d.name)
+										return ( "#{cg}" )
 									end
 								end
 							end
