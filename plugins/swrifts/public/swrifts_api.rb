5a2368a770b1613c1341c053c8483b00709f0c29
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-10-17 14:44:29 -1000

    Update swrifts_api.rb

--- plugins/swrifts/public/swrifts_api.rb
+++ plugins/swrifts/public/swrifts_api.rb
@@ -366,11 +366,7 @@ module AresMUSH
 		whatsthis = []
 		ttsl = []
 		
-		return (traittype)
 
-		if (traittype == 'hind')
-			return ("CG: #{cg}")
-		end
 		# Create an array of the excluded traits for the ones that are already set on the character.
 		cg.each do |d|
 			ttsl << { name: "#{d.name}" }
