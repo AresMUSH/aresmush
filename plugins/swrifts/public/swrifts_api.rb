ca3e00a582c596b7bb6ac4ad0841e0f516c9c7f8
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-10-31 13:44:53 -1000

    dd

--- plugins/swrifts/public/swrifts_api.rb
+++ plugins/swrifts/public/swrifts_api.rb
@@ -160,8 +160,8 @@ module AresMUSH
     # Set the Characters Iconic Framework
     if ( rawcharicf.length > 0 )
       mycharicf = getcharicf(rawcharicf,swrifts_iconicf)
-      each.mycharicf[:abilities].do | aa |
-        return "#{aa}"
+      mycharicf[:abilities].each do | aa |
+        return aa.inspect
       end
       #get the character abilities to list
       #abilites =
