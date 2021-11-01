cdbff2a3426abcf0c5b6f1aa0585af516b64a8a4
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-10-31 17:20:23 -1000

    fff

--- plugins/swrifts/public/swrifts_api.rb
+++ plugins/swrifts/public/swrifts_api.rb
@@ -174,7 +174,7 @@ module AresMUSH
     rawcharicf = acl_return_traits(swriftstraits,'iconicf') #Get the characters Iconic Framework from the traits
     swrifts_iconicf = Global.read_config('swrifts', 'iconicf')
     swrifts_abilities = Global.read_config('swrifts', 'abilities')
-    #return swrifts_abilities
+
     # Set the Characters Iconic Framework
     aaarray = []
     aastring = Array.new
