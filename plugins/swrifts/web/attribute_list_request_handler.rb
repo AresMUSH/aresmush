f03fe983af1fdb96150b54fe884b0a80554f6956
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-10-31 15:55:03 -1000

    d

--- plugins/swrifts/web/attribute_list_request_handler.rb
+++ plugins/swrifts/web/attribute_list_request_handler.rb
@@ -8,7 +8,7 @@ module AresMUSH
 		  iconicf: build_list(Swrifts.swrifts_icf),
 		  race: build_list(Swrifts.swrifts_races),
 		  skills: build_list(Swrifts.swrifts_skills),
-      swabilities: build_list(Swrifts.swrifts_abilities)
+      abilities: build_list(Swrifts.swrifts_abilities)
         }
       end
 
