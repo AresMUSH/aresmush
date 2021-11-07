68613a1b6393db2b765a8a260cf4eeb66cdf3373
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-11-07 11:04:32 -1000

    d

--- plugins/swrifts/public/swrifts_api.rb
+++ plugins/swrifts/public/swrifts_api.rb
@@ -802,12 +802,13 @@ module AresMUSH
 					tempcifstring[i] = []
 					hjopt = k.split("_")[0]
           hjnumber = hjopt.gsub("hj", "")
+          tablename = 'tablename'
 					v.each do |k1,v1|
 						tempcifstring[i] << {table: k1, name: hjopt}
 					end
 					cifstring[hjopt] = tempcifstring
-          cifstring[hjopt]['tablename'] = []
-          cifstring[hjopt]['tablename'] = hjnumber
+          cifstring[hjopt][tablename] = []
+          cifstring[hjopt][tablename] = hjnumber
           ++i
 				end
 			end
