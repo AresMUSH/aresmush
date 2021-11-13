c75c012312d1f160ec6addfca0a1528b90123a46
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-11-13 12:20:55 -1000

    d

--- plugins/swrifts/helpers/helpers.rb
+++ plugins/swrifts/helpers/helpers.rb
@@ -7,7 +7,7 @@ module AresMUSH
 			set.each do |key, rating|
 				# return (set)
 				element_name = "#{key}".downcase
-				# return (element_name)
+				return (element_name)
 				mod = "#{rating}".to_i
 				# return (element_name.inspect)
 				element = charhash.select { |a| a.name.downcase == element_name }.first
