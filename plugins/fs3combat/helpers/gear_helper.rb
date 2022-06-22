eb3966c72c01a67b6b6b0c9b62a7be914487d989
Aerrin99 <nicole.robinson.arnold@gmail.com>
2022-06-22 19:59:19 +0000

    set magic specials in FS3Combat.set_armor

--- plugins/fs3combat/helpers/gear_helper.rb
+++ plugins/fs3combat/helpers/gear_helper.rb
@@ -186,6 +186,8 @@ module AresMUSH
     end
     
     def self.set_armor(enactor, combatant, armor, specials = nil)
+      magic_specials = Magic.magic_armor_specials(combatant, armor)
+      magic_specials != nil ? specials = specials.concat([magic_specials]) : specials = specials
       combatant.update(armor_name: armor ? armor.titlecase : nil)
       combatant.update(armor_specials: specials ? specials.map { |s| s.titlecase }.uniq : [])
       message = t('fs3combat.armor_changed', :name => combatant.name, :armor => combatant.armor)
