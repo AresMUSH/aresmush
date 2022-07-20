d4c7ce536127da6a644041cf855f52f3e82d1e91
aerrin99 <102542459+aerrin99@users.noreply.github.com>
2022-07-20 17:18:31 -0400

    sketched spec reqs for combat/mount methods

---
+++ plugins/expandedmounts/specs/expanded_combat_mount_cmd_specs.rb
@@ -0,0 +1,47 @@
+module AresMUSH
+  module ExpandedMounts
+
+    describe "combat/mount" do
+      it "allows a character to mount any existing mount" do
+
+      end
+
+      it "gives an error if the mount doesn't exist" do
+
+      end
+
+      it "sets the character as pilot if they are bonded to the mount" do
+
+      end 
+
+      it "sets the character as the passenger if they are not bonded to the mount" do
+
+      end
+
+      it "only lets # of passengers ride the mount" do
+
+      end
+
+      it "lets characters mount by using another character name instead of mount name" do
+
+      end
+
+      it "errors if players try to mount while in a vehicle"  do
+        # May not be able to do this IDK
+      end
+
+      it "removes the character from another mount if they are already mounted" do
+
+      end
+
+      it "joints the character to the vehicle list using FS3Combat.join_vehicle" do
+
+      end
+
+
+
+    end
+
+
+  end
+end
