69fdc8cfff384b5f14d03405e486cf78af4ec8ef
spiritlake <32268357+spiritlake@users.noreply.github.com>
2022-08-03 21:45:15 -0400

    Update character_groups_request_handler.rb

--- plugins/profile/web/character_groups_request_handler.rb
+++ plugins/profile/web/character_groups_request_handler.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 module AresMUSH
   module Profile
     class CharacterGroupsRequestHandler
@@ -17,7 +18,7 @@ module AresMUSH
         char_group_names = char_groups.keys
         npc_group_names = npc_groups.keys
         group_names = char_group_names.concat(npc_group_names).uniq
-        
+        byebug
         group = Demographics.all_groups.select { |k, v| k.downcase == group_key }.values.first
         group_order = (( group["values"] || {} ).keys || []).map { |g| g.downcase }
         group_names = group_names.sort_by { |g| [group_order.find_index(g.downcase) || 99, g] }
