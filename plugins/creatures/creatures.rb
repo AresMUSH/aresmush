7274263ccf4a57b5ff46807e934c3459f1188b80
aerrin99 <102542459+aerrin99@users.noreply.github.com>
2022-08-09 13:28:46 -0400

    added creatures to web portal

---
+++ plugins/creatures/creatures.rb
@@ -0,0 +1,37 @@
+$:.unshift File.dirname(__FILE__)
+
+module AresMUSH
+     module Creatures
+
+    def self.plugin_dir
+      File.dirname(__FILE__)
+    end
+
+    def self.shortcuts
+      Global.read_config("creatures", "shortcuts")
+    end
+
+    def self.get_cmd_handler(client, cmd, enactor)
+      nil
+    end
+
+    def self.get_event_handler(event_name)
+      nil
+    end
+
+    def self.get_web_request_handler(request)
+      case request.cmd
+      when "creature"
+        return CreatureRequestHandler
+      when "creatures"
+        return CreaturesRequestHandler
+      when "creatureEdit"
+        return CreatureEditRequestHandler
+      when "creatureCreate"
+        return CreateCreatureRequestHandler
+      end
+      nil
+    end
+
+  end
+end
