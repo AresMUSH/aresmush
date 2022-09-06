3cd76248a3c0e05e44f7717cd09044ba622d2e3c
aerrin99 <102542459+aerrin99@users.noreply.github.com>
2022-09-06 16:52:11 -0400

    gear detail template takes groups for specials

--- plugins/fs3combat/templates/gear_detail_template.rb
+++ plugins/fs3combat/templates/gear_detail_template.rb
@@ -3,25 +3,45 @@ module AresMUSH
     class GearDetailTemplate < ErbTemplateRenderer
 
       attr_accessor :title, :list, :gear_type
-      
+
       def initialize(list, title, gear_type)
         @list = list.sort
         @title = title
         @gear_type = gear_type
         super File.dirname(__FILE__) + "/gear_detail.erb"
       end
-      
+
       def field(data)
         data ? FS3Combat.gear_detail(data) : "---"
       end
-      
+
       def field_name(name)
         t("fs3combat.#{gear_type}_title_#{name}")
       end
-      
+
       def specials
-        info = GearSpecialInfo.new(self.list, self.gear_type)
-        info.specials.map { |name, effects| "%xh#{left(name, 20)}%xn%R%T#{effects.join("%R%T")}"}
+        if (self.gear_type == :weapon)
+          allowed_specials = []
+          self.list.each do |fields|
+            if (fields[0] == "allowed_specials")
+              allowed_specials = fields[1]
+            end
+          end
+
+          specials = []
+          FS3Combat.weapon_specials.each do |k, v|
+            if allowed_specials
+              if (allowed_specials.include?(k))
+                special_title = left("#{k}:", 20)
+                effects = v.map { |k, v|  "#{k.titlecase}: #{v}" }.join("%R%T")
+                specials << "%xh#{special_title}%xn%R%T#{effects}"
+              end
+            end
+          end
+          specials
+        else
+          []
+        end
       end
     end
   end
