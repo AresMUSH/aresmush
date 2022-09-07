43de8040a891a574255926833277cafe57354b80
aerrin99 <102542459+aerrin99@users.noreply.github.com>
2022-09-07 11:02:01 -0400

    Don't create ghost mounts for characters who haven't input info yet.

--- plugins/expandedmounts/helpers/expanded_mounts_cg_helpers.rb
+++ plugins/expandedmounts/helpers/expanded_mounts_cg_helpers.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 module AresMUSH
   module ExpandedMounts
     def self.mount_name(char)
@@ -22,28 +23,31 @@ module AresMUSH
       if !mount_data.join.blank?
         errors.concat [t('expandedmounts.need_mount_type')] if chargen_data[:custom][:mount_type] == ""
         errors.concat [t('expandedmounts.need_mount_name')] if chargen_data[:custom][:mount_name] == ""
+        errors.concat [t('expandedmounts.already_mount_named')] if Mount.named(chargen_data[:custom][:mount_name])
         #Minor school can be set separately from the mount, so is not in mount_data
-        errors.concat [t('expandedmounts.need_school')] if chargen_data[:custom][:minor_school]  == "" 
+        errors.concat [t('expandedmounts.need_school')] if chargen_data[:custom][:minor_school]  == ""
       end
 
       return errors
     end
 
     def self.save_mount(char, chargen_data)
+      puts "HELLO?"
       error = ExpandedMounts.required_mount_info(chargen_data)
-      puts "ERROR: #{error}"
       return error if !error.empty?
 
+      mount_data = [chargen_data[:custom][:mount_type], chargen_data[:custom][:mount_name],  chargen_data[:custom][:mount_desc], chargen_data[:custom][:mount_shortdesc]]
+      return if mount_data.join.blank?
+
       if char.bonded
+        "BONDED"
         mount = char.bonded
         mount.update(expanded_mount_type: chargen_data[:custom][:mount_type])
         mount.update(name: chargen_data[:custom][:mount_name].titlecase)
         mount.update(description: chargen_data[:custom][:mount_desc])
         mount.update(shortdesc: chargen_data[:custom][:mount_shortdesc])
       else
-        mount_data = [chargen_data[:custom][:mount_type], chargen_data[:custom][:mount_name],  chargen_data[:custom][:mount_desc], chargen_data[:custom][:mount_shortdesc]]
-
-        return [t('expandedmounts.already_mount_named')] if (!mount_data.join.blank? && Mount.named(chargen_data[:custom][:mount_name]))
+        "NOT BONDED"
 
         mount = Mount.create(bonded: char, name: chargen_data[:custom][:mount_name].titlecase, expanded_mount_type: chargen_data[:custom][:mount_type], description: chargen_data[:custom][:mount_desc], shortdesc: chargen_data[:custom][:mount_shortdesc])
         char.update(bonded: mount)
