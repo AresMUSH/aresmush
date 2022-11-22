1b2450271ba89c5ae1a84ad8070eb3ce089a2c00
aerrin99 <102542459+aerrin99@users.noreply.github.com>
2022-11-22 16:02:24 -0500

    add creatures to scenes

--- plugins/scenes/web/get_scene_handler.rb
+++ plugins/scenes/web/get_scene_handler.rb
@@ -8,17 +8,17 @@ module AresMUSH
 
         error = Website.check_login(request, !edit_mode)
         return error if error
-        
+
         if (!scene)
           return { error: t('webportal.not_found') }
         end
-        
+
         if (edit_mode)
           can_edit = scene.shared ? Scenes.can_edit_scene?(enactor, scene) : Scenes.can_read_scene?(enactor, scene)
           if (!can_edit)
             return { error: t('dispatcher.not_allowed') }
           end
-        else          
+        else
           if (!scene.shared)
             return { unshared: true }
           end
@@ -27,7 +27,7 @@ module AresMUSH
             Login.mark_notices_read(enactor, :scene, scene.id)
           end
         end
-        
+
         if (edit_mode)
           log = scene.shared ? scene.scene_log.log : nil
           summary = Website.format_input_for_html(scene.summary)
@@ -35,11 +35,15 @@ module AresMUSH
           log = Website.format_markdown_for_html(scene.scene_log.log)
           summary = Website.format_markdown_for_html(scene.summary)
         end
-        
+
         participants = scene.participants.to_a
             .sort_by {|p| p.name }
             .map { |p| { name: p.name, id: p.id, icon: Website.icon_for_char(p), is_ooc: p.is_admin? || p.is_playerbit?  }}
-            
+
+        creatures = scene.creatures.to_a
+            .sort_by {|creature| creature.name }
+            .map { |creature| { name: creature.name, id: creature.id }}
+
         {
           id: scene.id,
           title: scene.title,
@@ -58,6 +62,7 @@ module AresMUSH
           scene_pacing: scene.scene_pacing,
           log: log,
           plots: scene.related_plots.map { |plot| { title: plot.title, id: plot.id } },
+          creatures: creatures,
           related_scenes: scene.related_scenes.sort_by { |r| r.date_title }.map { |r| { title: r.date_title, id: r.id }},
           can_edit: enactor && Scenes.can_edit_scene?(enactor, scene),
           can_delete: Scenes.can_delete_scene?(enactor, scene),
