e940ab4ccaaac022570d8a703d38ca0bb20c421e
aerrin99 <102542459+aerrin99@users.noreply.github.com>
2022-11-22 12:22:38 -0500

    spell fatigue cmd

---
+++ plugins/magic/templates/spell_fatigue_template.rb
@@ -0,0 +1,41 @@
+module AresMUSH
+  module Magic
+    class SpellFatigueTemplate < ErbTemplateRenderer
+      attr_accessor :char
+
+      def initialize(char)
+        @char = char
+        super File.dirname(__FILE__) + "/spell_fatigue.erb"
+      end
+
+      def magic_energy
+        ProgressBarFormatter.format(char.magic_energy, char.total_magic_energy)
+      end
+
+      def magic_energy_percent
+        current = char.magic_energy
+        total = char.total_magic_energy
+        puts "Current: #{current} Total: #{total}"
+        ((current.to_f/total.to_f)*100).to_i
+      end
+
+      def spell_fatigue_degree
+        Magic.get_fatigue_level(char)[:degree]
+      end
+
+      def spell_fatigue_msg
+        Magic.get_fatigue_level(char)[:msg]
+      end
+
+      def magic_energy_mod
+        Magic.get_magic_energy_mod(char)
+      end
+
+      def magic_energy_attack_mod
+        (magic_energy_mod.to_f / 2)
+      end
+
+
+    end
+  end
+end
