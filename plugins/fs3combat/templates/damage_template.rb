38be37fb94bbcca3e7448117b8f7d970474b39ef
aerrin99 <102542459+aerrin99@users.noreply.github.com>
2022-11-22 12:23:05 -0500

    spell fatigue in damage cmd

--- plugins/fs3combat/templates/damage_template.rb
+++ plugins/fs3combat/templates/damage_template.rb
@@ -5,23 +5,23 @@ module AresMUSH
 
 
       attr_accessor :damage, :target
-      
+
       def initialize(target)
         @target = target
         @damage = target.damage
         super File.dirname(__FILE__) + "/damage.erb"
-      end      
-      
+      end
+
       def severity(d)
         initial_sev = d.initial_severity
         current_sev = FS3Combat.display_severity(d.current_severity)
         "#{current_sev} (#{initial_sev[0..2]})"
       end
-      
+
       def treatable(d)
         d.is_treatable? ? t('global.y') : '-'
-      end   
-         
+      end
+
       def healing(d)
         total = FS3Combat.healing_points(d.current_severity)
         healed = total - d.healing_points
@@ -30,17 +30,25 @@ module AresMUSH
 
         ProgressBarFormatter.format(healed, total, 5)
       end
-            
+
       def healed_by
         return t('global.none') if @target.class != AresMUSH::Character
         docs = @target.doctors.map { |h| h.name }
         docs.count > 0 ? docs.join(", ") : t('global.none')
       end
-      
+
       def wound_mod
         FS3Combat.total_damage_mod(target)
       end
-      
+
+      def magic_energy_mod
+        Magic.get_magic_energy_mod(target)
+      end
+
+      def magic_energy_attack_mod
+        (magic_energy_mod.to_f / 2)
+      end
+
       def vehicle_notice
         combat = @target.combat
         return nil if !combat
