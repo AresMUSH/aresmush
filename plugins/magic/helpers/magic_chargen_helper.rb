7d3cfb64b30884639bfcdf26dc2a3e70e83fe971
spiritlake <32268357+spiritlake@users.noreply.github.com>
2022-12-12 20:30:07 -0500

    25 points on spells

--- plugins/magic/helpers/magic_chargen_helper.rb
+++ plugins/magic/helpers/magic_chargen_helper.rb
@@ -78,7 +78,7 @@ module AresMUSH
     end
 
     def self.check_points_on_spells(char)
-      points = 35
+      points = 20
       if Magic.points_on_spells(char) > points
         error = t('magic.too_many_points', :num => points)
       else
