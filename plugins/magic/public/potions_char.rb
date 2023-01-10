99677d7e44e8118c7bc95e89fff2159b3f039354
aerrin99 <102542459+aerrin99@users.noreply.github.com>
2023-01-10 10:59:32 -0500

    Update potions_char.rb

--- plugins/magic/public/potions_char.rb
+++ plugins/magic/public/potions_char.rb
@@ -1,7 +1,7 @@
 module AresMUSH
   class Character < Ohm::Model
-    collection :potions_has, "AresMUSH::PotionsHas" || []
-    collection :potions_creating, "AresMUSH::PotionsCreating" || []
+    collection :potions_has, "AresMUSH::PotionsHas"
+    collection :potions_creating, "AresMUSH::PotionsCreating"
     #DO NOT USE THIS, IT IS ONLY HERE TO FIX THE ERROR KILLING NESSIE AND ALL RELATED THINGS.
     # attribute :creating_potions
     attribute :potions_used, :type => DataType::Integer
@@ -10,7 +10,6 @@ module AresMUSH
     attribute :can_create_potions
 
   end
-end
 
   class PotionsHas < Ohm::Model
     include ObjectModel
@@ -31,3 +30,6 @@ end
     index :name
 
   end
+end
+
+
