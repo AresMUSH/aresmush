a520a0c09c429c9befa2bfba4053dcd8c4d20986
Nicole <102542459+aerrin99@users.noreply.github.com>
2023-01-29 17:51:54 -0500

    mythic name in finger

--- plugins/profile/templates/profile_template.rb
+++ plugins/profile/templates/profile_template.rb
@@ -1,9 +1,9 @@
 module AresMUSH
   module Profile
     class ProfileTemplate < ErbTemplateRenderer
-      
+
       attr_accessor :char
-      
+
       def initialize(enactor, char)
         @char = char
         @enactor = enactor
@@ -13,11 +13,11 @@ module AresMUSH
       def approval_status
         Profile.get_profile_status_message(@char)
       end
-      
+
       def visible_demographics
         Demographics.visible_demographics(@char, @enactor).select { |d| d != 'birthdate' }
       end
-      
+
       def demographic(d)
         @char.demographic(d)
       end
@@ -25,22 +25,26 @@ module AresMUSH
       def name
         Demographics.name_and_nickname(@char)
       end
-      
+
       def age
         age = @char.age
         age == 0 ? "" : age
       end
-      
+
       def show_age
         Demographics.age_enabled?
       end
-      
+
       def birthdate
         @char.formatted_birthdate
       end
 
       def groups
         Demographics.all_groups.keys
+      end
+
+      def bonded_name
+        @char.bonded&.name
       end
 
       def group(g)
@@ -50,26 +54,26 @@ module AresMUSH
       def rank
         @char.rank
       end
-      
+
       def desc
         @char.description
       end
-      
+
       def status
         Chargen.approval_status(@char)
       end
-      
+
       def alts
         alt_list = AresCentral.alts(@char).select { |c| c != self }.map { |c| c.name }
         alt_list.delete(@char.name)
         alt_list.join(" ")
       end
-      
-      def hooks        
+
+      def hooks
         formatter = MarkdownFormatter.new
         formatter.to_mush @char.rp_hooks
       end
-      
+
       def last_on
         if (Login.is_online?(@char))
           t('profile.currently_connected')
@@ -77,33 +81,33 @@ module AresMUSH
           OOCTime.local_long_timestr(@enactor, @char.last_on)
         end
       end
-      
+
       def timezone
         @char.timezone
       end
-      
+
       def custom_profile
         !@char.profile.empty?
       end
-      
+
       def handle_name
         @char.handle.name
       end
-      
+
       def unread_mail
         t('profile.unread_message_count', :num => @char.num_unread_mail)
       end
-      
+
       def wiki
         game_site = Game.web_portal_url
         "#{game_site}/char/#{@char.name}"
       end
-      
+
       def handle_profile
         arescentral = Global.read_config("arescentral", "arescentral_url")
         "#{arescentral}/handle/#{@char.handle.name}"
       end
-      
+
       def profile_title
         Profile.profile_title(@char)
       end
