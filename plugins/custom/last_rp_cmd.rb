1a5c19e6bb48e53f88754bedd0fe7d2ba230119d
aerrin99 <102542459+aerrin99@users.noreply.github.com>
2023-02-14 11:06:38 -0500

    Update last_rp_cmd.rb

--- plugins/custom/last_rp_cmd.rb
+++ plugins/custom/last_rp_cmd.rb
@@ -5,13 +5,13 @@ module AresMUSH
       include CommandHandler
 
       def handle
-        client.emit "Last RP Scenes for All Characters"
+        client.emit "Date | Name | Scene Title | Total Scenes"
         chars = Chargen.approved_chars
         chars.each do |c|
           last_scene = c.scenes_starring.sort_by { |s| s.created_at }.reverse[0]
           scene_count = c.scenes_starring.count
           if last_scene
-            client.emit "#{last_scene.created_at} | #{c.name} | #{last_scene.title} | #{scene_count} scenes"
+            client.emit "#{last_scene.created_at} | #{c.name} | #{last_scene.title} | #{scene_count}"
           else
             client.emit "#{c.name}: %xrNONE%xn"
           end
