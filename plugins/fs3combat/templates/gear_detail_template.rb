bcfefe513470e2aff9002cc370ef1916282300dc
Nicole <102542459+aerrin99@users.noreply.github.com>
2023-02-15 21:37:42 -0500

    weapon and armor specials show on detail cmd

--- plugins/fs3combat/templates/gear_detail_template.rb
+++ plugins/fs3combat/templates/gear_detail_template.rb
@@ -21,20 +21,42 @@ module AresMUSH
 
       def specials
         if (self.gear_type == :weapon)
-          allowed_specials = []
-          self.list.each do |fields|
-            if (fields[0] == "allowed_specials")
-              allowed_specials = fields[1]
-            end
-          end
-
+          special_group = FS3Combat.weapon_stat(@title, "special_group")
+          allowed_specials = Global.read_config("fs3combat", "weapon special groups", special_group)
           specials = []
+
           FS3Combat.weapon_specials.each do |k, v|
             if allowed_specials
-              if (allowed_specials.include?(k))
-                special_title = left("#{k}:", 20)
-                effects = v.map { |k, v|  "#{k.titlecase}: #{v}" }.join("%R%T")
+              if (allowed_specials.include?(k.downcase))
+                desc = v.select { |k, v| k == "description"}
+                desc = desc.map { |k, v| "#{v}" }
+                special_title = left("%xh#{k}:%xn #{desc[0]}", 40)
+                effects = v.select { |k, v| k != "is_magic" && k != "description"}
+                effects = effects.map { |k, v|  "#{k.titlecase}: #{v}" }.join("%R%T")
                 specials << "%xh#{special_title}%xn%R%T#{effects}"
+              end
+            end
+          end
+          specials
+        elsif (self.gear_type == :armor)
+          special_group = FS3Combat.armor_stat(@title, "special_group")
+          allowed_specials = Global.read_config("fs3combat", "armor special groups", special_group)
+          specials = []
+
+          FS3Combat.armor_specials.each do |k, v|
+            if allowed_specials
+              if (allowed_specials.include?(k.downcase))
+                desc = v.select { |k, v| k == "description"}
+                desc = desc.map { |k, v| "#{v}" }
+                special_title = left("%xh#{k}:%xn #{desc[0]}", 40)
+                protection = []
+                v.dig('protection').each do |k, v|
+                  protection.concat ["#{k}: #{v}"]
+                end
+                protection = protection.join("; ")
+                effects = v.select { |k, v| k != "is_magic" && k != "description" && k != "protection"}
+                effects = effects.map { |k, v|  "#{k.titlecase}: #{v}" }.join("%R%T")
+                specials << "%xh#{special_title}%xn%R%TProtection: #{protection}%R%T#{effects}"
               end
             end
           end
@@ -43,6 +65,7 @@ module AresMUSH
           []
         end
       end
+
     end
   end
 end
