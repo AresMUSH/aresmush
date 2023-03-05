eea9da5d2376f77b68952badea928ed243f6d8bb
Linda Naughton <lynn@wordsmyth.org>
2023-03-05 10:35:27 -0500

    Fix bug in mail multi-delete.

--- plugins/mail/commands/manage/mail_delete_cmd.rb
+++ plugins/mail/commands/manage/mail_delete_cmd.rb
@@ -15,13 +15,14 @@ module AresMUSH
       
       def handle
         messages_to_delete = Mail.select_message_range(self.num)
+        
         if (!messages_to_delete)
           client.emit_failure t('mail.invalid_message_number')
           return
         end
         
         messages_to_delete.each do |m|
-          Mail.with_a_delivery(client, enactor, "#{num}") do |delivery|
+          Mail.with_a_delivery(client, enactor, "#{m}") do |delivery|
             Mail.move_to_trash(delivery)
             client.emit_ooc t("mail.message_deleted", :subject => delivery.subject)
           end
