ee8c3c91d1c1710d0dd9d65fe6cb75b919484a19
Nicole <102542459+aerrin99@users.noreply.github.com>
2023-03-15 22:36:33 -0400

    Update luck.rb

--- plugins/fs3skills/helpers/luck.rb
+++ plugins/fs3skills/helpers/luck.rb
@@ -3,7 +3,7 @@ module AresMUSH
     def self.can_manage_luck?(actor)
       actor && actor.has_permission?("manage_abilities")
     end
-    
+
     def self.modify_luck(char, amount)
       max_luck = Global.read_config("fs3skills", "max_luck")
       luck = char.luck + amount
@@ -11,7 +11,7 @@ module AresMUSH
       luck = [0, luck].max
       char.update(fs3_luck: luck)
     end
-    
+
     def self.spend_luck(char, reason, scene)
       char.spend_luck(1)
       message = t('fs3skills.luck_point_spent', :name => char.name, :reason => reason)
@@ -22,17 +22,17 @@ module AresMUSH
       else
         char.room.emit_ooc message
       end
-      
+
       Achievements.award_achievement(char, "fs3_luck_spent")
-      
+
       if (Global.read_config('fs3skills', 'job_on_luck_spend'))
-        category = Jobs.system_category
+        category = "Luck"
         status = Jobs.create_job(category, t('fs3skills.luck_job_title', :name => char.name), message, Game.master.system_character)
         if (status[:job])
           Jobs.close_job(Game.master.system_character, status[:job])
         end
       end
-      
+
       Global.logger.info "#{char.name} spent luck on #{reason}."
     end
   end
