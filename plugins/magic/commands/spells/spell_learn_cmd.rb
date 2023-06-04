73118bc5cfaf63376bbfb7d5fcc4097606feffe3
Nicole <102542459+aerrin99@users.noreply.github.com>
2023-06-03 21:10:46 -0400

    Can't learn spells without XP

--- plugins/magic/commands/spells/spell_learn_cmd.rb
+++ plugins/magic/commands/spells/spell_learn_cmd.rb
@@ -16,7 +16,7 @@ module AresMUSH
         return t('magic.use_school_version') if (self.spell == "Potions" || self.spell == "Familiar")
         # return t('magic.request_spell') if (self.spell == "Wild Shape" || self.spell == "Greater Wild Shape" || self.spell == "Half Shift")
         return t('magic.not_spell') if !Magic.is_spell?(self.spell)
-        # return t('fs3skills.not_enough_xp') if enactor.xp <= 0
+        return t('fs3skills.not_enough_xp') if enactor.xp <= 0
         return t('magic.too_many_spells', :spell_max => Magic.spell_max) if Magic.count_spells_total(enactor) >= Magic.spell_max
         previous = Magic.previous_level_spell?(enactor, self.spell)
         return t('magic.need_previous_level') if Magic.previous_level_spell?(enactor, self.spell) == false
