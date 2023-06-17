e7dc36630f35acad65cb3b1aa9d154f5b5838b80
Nicole <102542459+aerrin99@users.noreply.github.com>
2023-06-17 09:12:52 -0400

    Update heal_scan_cmd.rb

--- plugins/fs3combat/commands/damage/heal_scan_cmd.rb
+++ plugins/fs3combat/commands/damage/heal_scan_cmd.rb
@@ -2,7 +2,7 @@ module AresMUSH
   module FS3Combat
     class HealScanCmd
       include CommandHandler
-      
+
       def handle
         damage = {}
         Character.all.each do |c|
@@ -11,15 +11,22 @@ module AresMUSH
             damage[c.name] = damage_mod
           end
         end
-        
+
+        Mount.all.each do |c|
+          damage_mod = FS3Combat.total_damage_mod(c)
+          if (damage_mod != 0)
+            damage[c.name] = damage_mod
+          end
+        end
+
         list = damage.sort_by { |k, v| v }.map { |name, damage_mod| "#{name.ljust(30)} #{damage_mod}" }
-        
+
         template = BorderedListTemplate.new list, t('fs3combat.damage_scan_title'), nil, t('fs3combat.damage_scan_subtitle')
         client.emit template.render
       end
-      
 
-      
+
+
     end
   end
 end
