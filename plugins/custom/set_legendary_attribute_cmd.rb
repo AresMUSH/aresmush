a90ec4acf1a535f71e17fa219aed7b662a48847e
ninaws <edlund.n@gmail.com>
2025-01-03 00:51:46 -0800

    Update setlegendary command to simplify format and restrict to admins

--- plugins/custom/set_legendary_attribute_cmd.rb
+++ plugins/custom/set_legendary_attribute_cmd.rb
@@ -6,21 +6,41 @@ module AresMUSH
         attr_accessor :name, :attribute, :value
   
         def parse_args
-          # Expect command format: setlegendary <name>/<attribute>=<value>
-          args = cmd.parse_args(ArgParser.arg1_slash_arg2_equals_arg3)
+          # Expect command format: setlegendary <name>/<attribute>
+          args = cmd.parse_args(ArgParser.arg1_slash_arg2)
           self.name = titlecase_arg(args.arg1)
           self.attribute = downcase_arg(args.arg2)
-          self.value = trim_arg(args.arg3)
+          self.value = 5  # Always set the value to 5
         end
   
         def required_args
-          [self.name, self.attribute, self.value]
+          [self.name, self.attribute]
+        end
+  
+        def check_permissions
+          return t('dispatcher.not_allowed') unless enactor.is_admin?
+          nil
         end
   
         def handle
-          client.emit_failure "This command is under construction."
+          target = Character.find_one_by_name(self.name)
+          if !target
+            client.emit_failure "Character #{self.name} not found."
+            return
+          end
+  
+          if !target.fs3_attributes[self.attribute]
+            client.emit_failure "Attribute #{self.attribute} is not valid."
+            return
+          end
+  
+          target.fs3_attributes[self.attribute] = self.value
+          target.save
+          Global.dispatcher.queue_event CharUpdatedEvent.new(target.id)
+  
+          client.emit_success("#{self.name}'s #{self.attribute.capitalize} is now Legendary (5).")
         end
       end
     end
   end
-  
+    
