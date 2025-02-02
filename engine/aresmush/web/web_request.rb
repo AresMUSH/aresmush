a5c7f951d0082990ed141640b7614acb388aec9f
lynnfaraday <lynnfaraday@users.noreply.github.com>
2025-02-01 22:04:35 -0500

    Args come over in body not form data.

--- engine/aresmush/web/web_request.rb
+++ engine/aresmush/web/web_request.rb
@@ -1,44 +1,41 @@
 module AresMUSH
   class WebRequest
-    attr_accessor :json, :ip_addr, :hostname
+    attr_accessor :ip_addr, :hostname, :cmd, :args, :api_key, :auth, :json
     
     def initialize(json)
       @json = json
-    end
-    
-    def cmd
-      @json[:cmd]
-    end
-    
-    def args
-      @json[:args] || {}
-    end
-    
-    def api_key
-      @json[:api_key]
+      @cmd = json["cmd"]
+      @args = (json["args"] || {})
+      @api_key = json["api_key"]
+      @auth = (json["auth"] || {}) #.transform_keys(&:to_sym)
+      
+      #
+      #
+      # TODO !!!
+      #
+      #
+      #
+      puts @args
+      puts @auth
     end
     
     def check_api_key
-      key = @json[:api_key]
+      key = self.api_key
       return true if Website.engine_api_keys.include?(key)
       return Game.master.player_api_keys && Game.master.player_api_keys.has_key?(key)
     end
     
-    def auth
-      @json[:auth] || {}
-    end
-    
     def token
-      auth[:token]
+      @auth['token']
     end
     
     def enactor
-      id = auth[:id]
+      id = @auth['id']
       id ? Character.find_one_by_name(id) : nil
     end
     
     def log_request
-      Global.logger.debug "Web Request: #{cmd} #{auth[:id]} #{args} #{enactor ? enactor.name : "Anonymous"}"
+      Global.logger.debug "Web Request: #{cmd} #{@auth["id"]} #{args} #{enactor ? enactor.name : "Anonymous"}"
     end
   end
 end
