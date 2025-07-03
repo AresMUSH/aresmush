2091c98bdee27cba824f30cf7f8ae2876a49b6c4
lynnfaraday <lynnfaraday@users.noreply.github.com>
2025-07-02 23:39:21 -0400

    Default font and rainbow file.

---
+++ install/migrations/0093_2x5x0_update.rb
@@ -0,0 +1,22 @@
+module AresMUSH  
+
+  module Migrations
+    class Migration2x5x0Update
+      def require_restart
+        false
+      end
+      
+      def migrate
+        Global.logger.debug "Installing default rainbow colors"
+        src = File.join(AresMUSH.root_path, 'install', 'game.distr', 'styles', 'rainbow.scss')
+        dest = File.join(AresMUSH.game_path, 'styles', 'rainbow.scss')
+        FileUtils.cp src, dest
+        
+        Global.logger.debug "Installing default fonts"
+        src = File.join(AresMUSH.root_path, 'install', 'game.distr', 'styles', 'fonts.scss')
+        dest = File.join(AresMUSH.game_path, 'styles', 'fonts.scss')
+        FileUtils.cp src, dest
+      end
+    end
+  end    
+end
