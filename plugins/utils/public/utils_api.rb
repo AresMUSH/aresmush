ed2edbc0cccdd010a59f170977c159b5316c7d56
lynnfaraday <lynnfaraday@users.noreply.github.com>
2026-03-21 00:22:46 -0400

    Add notes to wiki char backup. Error trap deleting wiki backup if files are gone.

--- plugins/utils/public/utils_api.rb
+++ plugins/utils/public/utils_api.rb
@@ -4,5 +4,13 @@ module AresMUSH
       prefix = char.utils_edit_prefix ? "#{char.utils_edit_prefix} " : ""
       client.emit_raw "#{prefix}#{msg}"
     end
+    
+    def self.export_notes(char)
+      section = "player"
+      text = char.notes_section(section)
+      title = t("notes.notes_#{section}_title", :name => char.name)
+      template = BorderedDisplayTemplate.new text, title
+      return template.render
+    end
   end
 end
