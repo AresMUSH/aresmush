0c14089d60cfdad895cd1132e38e682775eddb13
lynnfaraday <lynnfaraday@users.noreply.github.com>
2026-03-20 23:46:23 -0400

    Gear blurb.

--- plugins/fs3combat/web/gear_list_request_handler.rb
+++ plugins/fs3combat/web/gear_list_request_handler.rb
@@ -2,13 +2,15 @@ module AresMUSH
   module FS3Combat
     class GearListRequestHandler
       def handle(request)
+        gear_blurb = Global.read_config("fs3combat", "gear_blurb")
         {
           weapons: build_list(FS3Combat.weapons),
           armor: build_list(FS3Combat.armors),
           vehicles: build_list(FS3Combat.vehicles),
           mounts: build_list(FS3Combat.mounts),
           allow_vehicles: FS3Combat.vehicles_allowed?,
-          allow_mounts: FS3Combat.mounts_allowed?
+          allow_mounts: FS3Combat.mounts_allowed?,
+          gear_blurb: gear_blurb.blank? ? nil : Website.format_markdown_for_html(gear_blurb)
         } 
       end
       
