063ba1ee4a040be268a53c25516278346c11d074
lynnfaraday <lynnfaraday@users.noreply.github.com>
2026-07-10 23:38:12 -0400

    Tweak instructions for custom classes.

--- plugins/website/custom_icon_classes.rb
+++ plugins/website/custom_icon_classes.rb
@@ -5,7 +5,7 @@ module AresMUSH
       # Gets custom classes to add to the big character log icons.
       #
       # @param [Character] char - The character being requested.
-      # @return [string] - A list of CSS classes to include on the icon div.
+      # @return [string] - A single string containing one or more CSS classes to include on the icon div.
       
       def self.custom_icon_classes(char)
         return ""
