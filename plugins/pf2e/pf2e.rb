ca59d8c2d6bb1297839e38fec75e05a110380adb
Andi <bodyhorrorandcupcakes@gmail.com>
2026-08-01 10:17:18 -0700

    Creating new plugins pf2e and alttracker

---
+++ plugins/pf2e/pf2e.rb
@@ -0,0 +1,27 @@
+$:.unshift File.dirname(__FILE__)
+
+module AresMUSH
+     module Pf2e
+
+    def self.plugin_dir
+      File.dirname(__FILE__)
+    end
+
+    def self.shortcuts
+      Global.read_config("pf2e", "shortcuts")
+    end
+
+    def self.get_cmd_handler(client, cmd, enactor)
+      nil
+    end
+
+    def self.get_event_handler(event_name)
+      nil
+    end
+
+    def self.get_web_request_handler(request)
+      nil
+    end
+
+  end
+end
